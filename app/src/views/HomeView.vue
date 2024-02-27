0eee0cb8d3c133abeb1085c16c51884ef91599a8
Hermann EHO <hermannleboss@gmail.com>
2024-02-27 06:09:07 +0000

    Refactor CSS styles and update HomeView.vue AboutView.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,7 +3,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="home">
+      <h1>This is an Home page</h1>
+    </div>
 </template>
