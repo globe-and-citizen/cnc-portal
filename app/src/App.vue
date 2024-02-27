b60c2bfea50550c0e6ac09d1ebdb098146949663
Hermann EHO <hermannleboss@gmail.com>
2024-02-27 06:41:55 +0000

    import Tailwind CSS

--- app/src/App.vue
+++ app/src/App.vue
@@ -7,6 +7,9 @@ import { RouterLink, RouterView } from 'vue-router'
 
     <div class="wrapper">
 
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
