5bd9ea9ddb04d780ce41ad91fb5c2af49ce0f073
farrellh1 <farrell.hauzan@gmail.com>
2024-03-20 20:37:49 +0700

    feat: Tips basic view

---
+++ app/src/views/TipsView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import MemberDetail from '@/components/MemberDetail.vue';
+import { useMembersStore } from '@/stores/member';
+
+const { members } = useMembersStore()
+</script>
+
+<template>
+  <div class="text-primary text-center my-[20px]">My teams</div>
+  <div class="flex flex-col gap-[10px] mx-[200px]">
+    <div v-for="member in members" :key="member.id" class="bg-neutral p-[6px]">
+      <MemberDetail :member="member" />
+    </div>
+  </div>
+</template>
