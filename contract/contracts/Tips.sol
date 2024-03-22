1a4bfdea4b169b73faf724c5c2e63dc78f6dad91
farrellh1 <farrell.hauzan@gmail.com>
2024-03-22 21:13:12 +0700

    fix: push and send tip should be an array of address

--- contract/contracts/Tips.sol
+++ contract/contracts/Tips.sol
@@ -4,27 +4,43 @@ pragma solidity ^0.8.24;
 contract Tips {
     mapping(address => uint256) private addressToTips;
 
-    event PushTip(address from, address to, uint256 amount);
-    event SendTip(address from, address to, uint256 amount);
-    event WithdrawTip(address to, uint256 amount);
+    event PushTip(
+        address from,
+        address[] to,
+        uint256 totalAmount,
+        uint256 amountPerAddress
+    );
+    event SendTip(
+        address from,
+        address[] to,
+        uint256 totalAmount,
+        uint256 amountPerAddress
+    );
+    event TipWithdrawal(address to, uint256 amount);
 
-    function pushTip(address _to) external payable {
-        require(msg.value > 0, "Must send a positive amount.");
-        bool tipSuccess = payable(_to).send(msg.value);
-        require(tipSuccess, "Failed to push tip.");
+    function pushTip(address[] memory _toAddresses) positiveAmountRequired external payable {
+        uint256 amountPerAddress = msg.value / _toAddresses.length;
 
-        emit PushTip(msg.sender, _to, msg.value);
+        for (uint256 i = 0; i < _toAddresses.length; i++) {
+            bool transferSuccess = payable(_toAddresses[i]).send(amountPerAddress);
+            require(transferSuccess, "Failed to send tip.");
+        }
+
+        emit PushTip(msg.sender, _toAddresses, msg.value, amountPerAddress);
     }
 
-    function sendTip(address _to) external payable {
-        require(msg.value > 0, "Must send a positive amount.");
-        addressToTips[_to] += msg.value;
+    function sendTip(address[] memory _toAddresses) positiveAmountRequired external payable {
+        uint256 amountPerAddress = msg.value / _toAddresses.length;
 
-        emit SendTip(msg.sender, _to, msg.value);
+        for (uint256 i = 0; i < _toAddresses.length; i++) {
+            addressToTips[_toAddresses[i]] += amountPerAddress;
+        }
+
+        emit SendTip(msg.sender, _toAddresses, msg.value, amountPerAddress);
     }
 
     function withdraw() external payable {
-        uint tipEarned = addressToTips[msg.sender];
+        uint256 tipEarned = addressToTips[msg.sender];
         require(tipEarned > 0, "No tips to withdraw.");
 
         bool tipSuccess = payable(msg.sender).send(tipEarned);
@@ -32,10 +48,15 @@ contract Tips {
 
         addressToTips[msg.sender] = 0;
 
-        emit WithdrawTip(msg.sender, tipEarned);
+        emit TipWithdrawal(msg.sender, tipEarned);
     }
 
     function getTips(address _address) external view returns (uint256) {
-      return addressToTips[_address];
+        return addressToTips[_address];
+    }
+
+    modifier positiveAmountRequired() {
+        require(msg.value > 0, "Must send a positive amount.");
+        _;
     }
 }
