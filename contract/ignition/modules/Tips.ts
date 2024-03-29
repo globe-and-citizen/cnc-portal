20e6e89d43f1320ea69eec9db5d57ec0c4a0a26d
farrellh1 <farrell.hauzan@gmail.com>
2024-03-29 14:45:44 +0700

    feat: create deployable contract script

---
+++ contract/ignition/modules/Tips.ts
@@ -0,0 +1,10 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const TipsModule = buildModule("TipsModule", (m) => {
+
+  const tips = m.contract("Tips");
+
+  return { tips };
+});
+
+export default TipsModule;
