0f222178b7ed8f69c2dbb988f40476504863ddc5
farrellh1 <farrell.hauzan@gmail.com>
2024-03-29 14:46:35 +0700

    feat: add global constant variable

---
+++ app/src/constant/index.ts
@@ -0,0 +1,3 @@
+export const PRIVATE_KEY = import.meta.env.VITE_APP_PRIVATE_KEY
+export const JSON_RPC_URL = import.meta.env.VITE_APP_JSON_RPC_URL
+export const TIPS_ADDRESS = import.meta.env.VITE_APP_TIPS_ADDRESS
