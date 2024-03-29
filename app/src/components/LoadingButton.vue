08ea794f78074bdd418022e2de2c4b76f1169bb3
farrellh1 <farrell.hauzan@gmail.com>
2024-03-29 13:24:43 +0700

    feat: create loading button

---
+++ app/src/components/LoadingButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="btn w-full" :class="`btn-${color}`">
+    <span class="loading loading-spinner loading-sm text-white"></span>
+  </button>
+</template>
+
+
+<script setup lang="ts">
+defineProps<{
+  color: string
+}>()
+</script>
