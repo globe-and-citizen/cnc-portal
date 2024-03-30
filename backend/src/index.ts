4a47f4d90ff6f1e3595b554421957c3e0103d298
Dasarath G <dasarathg68@gmail.com>
2024-03-30 12:02:59 +0530

    feat: add swagger-autogen for generating api-specs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,9 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 import teamRoutes from "./routes/teamRoutes";
 import memberRoutes from "./routes/memberRoutes";
-
+import { swaggerGenerate } from "./utils/swagger";
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./utils/swagger-output.json");
 const prisma = new PrismaClient();
 
 const app = express();
@@ -13,6 +15,10 @@ app.use(cors());
 app.use(teamRoutes);
 app.use(memberRoutes);
 const port = parseInt(process.env.PORT as string) || 3000;
+
+swaggerGenerate();
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
---
+++ backend/src/utils/swagger.ts
@@ -0,0 +1,16 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+  info: {
+    title: "CNC Portal",
+    description:
+      "Often, several open-source developers coalesce around a particular project and that project that becomes a very successful company or foundation. Due to the nature of these open-source projects, however, there is no direct way to concretely recognize the innumerable micro contributions of each contributor along the way. Furthermore, large open projects often struggle to get contributors acting in unison in ways that are decisive, unified, and directed. The CNC Portal is to solve these two problems by 1) creating the mechanism to financially recognize micro contributions and, 2) creating tools that promote the emergence of effective governance. The Crypto Native Corporation Portal (CNC Portal) has the potential to be an extremely large and extremely complex project.",
+  },
+};
+
+const outputFile = "./utils/swagger-output.json";
+const routes = ["./index.ts"];
+
+export const swaggerGenerate = () => {
+  swaggerAutogen(outputFile, routes, doc);
+};
