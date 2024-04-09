bfacf85a979214916e2ec78ddb4a9bb24a5b06f9
Dasarath G <dasarathg68@gmail.com>
2024-04-09 12:30:38 +0530

    revert: use the manual way of writing the specification

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,8 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 import teamRoutes from "./routes/teamRoutes";
 import memberRoutes from "./routes/memberRoutes";
-import { swaggerGenerate } from "./utils/swagger";
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./utils/swagger-output.json");
+
+import { generateDocs } from "./utils/swagger";
 const prisma = new PrismaClient();
 
 const app = express();
@@ -16,8 +15,8 @@ app.use(teamRoutes);
 app.use(memberRoutes);
 const port = parseInt(process.env.PORT as string) || 3000;
 
-swaggerGenerate();
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+generateDocs();
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
--- backend/src/utils/swagger.ts
+++ backend/src/utils/swagger.ts
@@ -1,16 +1,18 @@
-const swaggerAutogen = require("swagger-autogen")();
+const swaggerJsdoc = require("swagger-jsdoc");
 
-const doc = {
-  info: {
-    title: "CNC Portal",
-    description:
-      "Often, several open-source developers coalesce around a particular project and that project that becomes a very successful company or foundation. Due to the nature of these open-source projects, however, there is no direct way to concretely recognize the innumerable micro contributions of each contributor along the way. Furthermore, large open projects often struggle to get contributors acting in unison in ways that are decisive, unified, and directed. The CNC Portal is to solve these two problems by 1) creating the mechanism to financially recognize micro contributions and, 2) creating tools that promote the emergence of effective governance. The Crypto Native Corporation Portal (CNC Portal) has the potential to be an extremely large and extremely complex project.",
+const options = {
+  failOnErrors: true, // Whether or not to throw when parsing errors. Defaults to false.
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Hello World",
+      version: "1.0.0",
+    },
   },
+  apis: ["./src/index.ts"],
 };
 
-const outputFile = "./utils/swagger-output.json";
-const routes = ["./index.ts"];
-
-export const swaggerGenerate = () => {
-  swaggerAutogen(outputFile, routes, doc);
+export const generateDocs = () => {
+  const openapiSpecification = swaggerJsdoc(options);
+  console.log(openapiSpecification);
 };
