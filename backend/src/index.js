5d8c77a5a420ba27b3b787bb8c103e0e2344fe35
aliassan <alimkwananzi@gmail.com>
2024-04-11 11:30:14 +0200

    Removed .js files generated by running tsc

--- backend/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const serverConfig_1 = __importDefault(require("./config/serverConfig"));
-serverConfig_1.default.listen();
