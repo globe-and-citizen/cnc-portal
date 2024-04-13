70265b459215c6623a7cc20bcc7ae3062647450a
Hermann EHO <hermannleboss@gmail.com>
2024-04-13 07:49:07 +0000

    refactor: remove Team Card margin, use the new interface for team props, use a object for props instead of attribute

--- app/src/components/TeamCard.vue
+++ app/src/components/TeamCard.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="card w-80 bg-white ml-5 mt-5">
+  <div class="card bg-white">
     <div class="card-body">
-      <h1 class="card-title">{{ props.TeamName }}</h1>
-      <p class="text-sm">{{ props.TeamDescription }}</p>
+      <h1 class="card-title">{{ props.team.name }}</h1>
+      <p class="text-sm">{{ props.team.description }}</p>
       <div class="card-actions justify-between"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps(['TeamName', 'TeamDescription'])
+import type { Team } from '@/types/team';
+interface Props{
+  team: Team
+}
+const props = defineProps<Props>()
 </script>
