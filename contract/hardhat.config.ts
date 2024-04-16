16b73760dc981ebc4af8bb72079d39af1076ccda
Hermann EHO <hermannleboss@gmail.com>
2024-04-16 17:11:38 +0000

    fix: fix build error.

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -5,17 +5,16 @@ import  dotenv from 'dotenv'
 
 dotenv.config()
 if(process.env.ALCHEMY_API_KEY === undefined || process.env.PRIVATE_KEY === undefined) {
-  console.error('Please set your ALCHEMY_API_KEY and PRIVATE_KEY in a .env file')
-  throw new Error('Please set your ALCHEMY_API_KEY and PRIVATE_KEY in a .env file')
+  console.error('\x1b[33m Please set your ALCHEMY_API_KEY and PRIVATE_KEY in a .env file\x1b[0m')
 }
 const config: HardhatUserConfig = {
   solidity: '0.8.24',
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {},
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY as string]
+    sepolia:  {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY? process.env.ALCHEMY_API_KEY : ""}`,
+      accounts: [process.env.PRIVATE_KEY  ? process.env.PRIVATE_KEY : ""]
     }
   }
 }
