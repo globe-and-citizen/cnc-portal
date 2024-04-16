c87ce9f87a68e263eb80a306478cb523675870c2
Hermann EHO <hermannleboss@gmail.com>
2024-04-16 17:22:20 +0000

    chore: format documents

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,10 +1,10 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 import '@nomicfoundation/hardhat-chai-matchers'
-import  dotenv from 'dotenv'
+import dotenv from 'dotenv'
 
 dotenv.config()
-if(process.env.ALCHEMY_API_KEY === undefined || process.env.PRIVATE_KEY === undefined) {
+if (process.env.ALCHEMY_API_KEY === undefined || process.env.PRIVATE_KEY === undefined) {
   console.error('\x1b[33m Please set your ALCHEMY_API_KEY and PRIVATE_KEY in a .env file\x1b[0m')
 }
 const config: HardhatUserConfig = {
@@ -12,9 +12,9 @@ const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {},
-    sepolia:  {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY? process.env.ALCHEMY_API_KEY : ""}`,
-      accounts: [process.env.PRIVATE_KEY  ? process.env.PRIVATE_KEY : ""]
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY ? process.env.ALCHEMY_API_KEY : ''}`,
+      accounts: [process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : '']
     }
   }
 }
