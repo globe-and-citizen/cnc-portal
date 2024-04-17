de1094b44cf9bd39250599a5a3335ec498b805c9
farrellh1 <farrell.hauzan@gmail.com>
2024-04-17 19:37:46 +0700

    feat: add get balance and withdraw function

--- app/src/stores/tips.ts
+++ app/src/stores/tips.ts
@@ -12,16 +12,25 @@ export const useTipsStore = defineStore('tips', {
     sendTipLoading: false as boolean,
     pushTipLoading: false as boolean,
     isWalletConnected: false as boolean,
-    totalTipAmount: 0 as number
+    totalTipAmount: 0 as number,
+    balance: '0' as string
   }),
   actions: {
     async connectWallet() {
       if (this.isWalletConnected) return
 
+      const { show } = useToastStore()
+      if (!(window as any).ethereum) {
+        show(ToastType.Info, 'Please install Metamask')
+        return
+      }
+
       const provider = new ethers.BrowserProvider((window as any).ethereum)
       await provider.send('eth_requestAccounts', [])
       this.contract = new ethers.Contract(TIPS_ADDRESS, ABI, await provider.getSigner())
       this.isWalletConnected = true
+
+      return (await provider.getSigner()).address
     },
     async pushTip(addresses: AddressLike[]) {
       const { show } = useToastStore()
@@ -68,6 +77,33 @@ export const useTipsStore = defineStore('tips', {
       } finally {
         this.sendTipLoading = false
         this.totalTipAmount = 0
+      }
+    },
+    async getBalance() {
+      let address: AddressLike | undefined
+      if (!this.isWalletConnected) {
+        address = await this.connectWallet()
+      }
+
+      this.balance = ethers.formatEther(await this.contract!.getBalance(address))
+    },
+    async withdrawTips() {
+      if (!this.isWalletConnected) await this.connectWallet()
+
+      const { show } = useToastStore()
+
+      if (this.balance == '0') {
+        show(ToastType.Info, 'No tips to withdraw')
+        return
+      }
+
+      try {
+        await this.contract!.withdraw()
+        show(ToastType.Success, 'Tips withdrawn successfully')
+      } catch (error) {
+        show(ToastType.Error, 'Failed to withdraw tips')
+      } finally {
+        await this.getBalance()
       }
     }
   }
