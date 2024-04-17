3159e330b2f0f9e19b2d63715e860683b3c3185f
aliassan <alimkwananzi@gmail.com>
2024-04-17 14:30:17 +0200

    Removed createUser controller

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -11,63 +11,7 @@ const prisma = new PrismaClient();
  * @param res
  * @returns
  */
-const createUser = async (req: Request, res: Response) => {
-  try {
-    //Get authentication data from reguest
-    const { address } = req.params;
-    const { message, signature } = req.body
-
-    //Check to make sure authentication data is not empty
-    if (!address)
-      return errorResponse(401, "Create user error: Missing user address", res)
-
-    if (!message)
-      return errorResponse(401, "Create user error: Missing SIWE message", res)
-
-    if (!signature)
-      return errorResponse(401, "Create user error: Missing signature", res)
-
-    //Check user is owner of the address
-    const SIWEObject = new SiweMessage(message)
-    const { data } = await SIWEObject.verify({signature})
-
-    if (data.address !== address) {
-      await prisma.$disconnect()
-      return errorResponse(403, "Create user error: User verification failed", res) 
-    }
-
-    //Generate nonce
-    const nonce = generateNonce();
-
-    //Create user and register new nonce
-    await prisma.user.create({
-      data: {
-        address,
-        nonce,
-      },
-    });
-
-    await prisma.$disconnect();
-
-    //Return response
-    res.status(200).json({
-      success: true,
-      nonce,
-    });
-  } catch (error) {
-    await prisma.$disconnect();
-
-    return errorResponse(500, error, res);
-  }
-};
-
-/**
- *
- * @param req
- * @param res
- * @returns
- */
-const getNonce = async (req: Request, res: Response) => {
+export const getNonce = async (req: Request, res: Response) => {
   const { address } = req.params;
 
   try {
@@ -99,5 +43,3 @@ const getNonce = async (req: Request, res: Response) => {
     return errorResponse(500, error, res);
   }
 };
-
-export { createUser, getNonce };
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import { createUser, getNonce } from "../controllers/userController";
+import { getNonce } from "../controllers/userController";
 const userRoutes = express.Router();
 
-userRoutes.post("/create/:address", createUser);
 userRoutes.get("/nonce/:address", getNonce);
 
 export default userRoutes;
