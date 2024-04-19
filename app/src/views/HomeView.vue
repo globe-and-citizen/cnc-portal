ae88c7c2af248698422cf9af6c2af24a29d40911
Hermann EHO <hermannleboss@gmail.com>
2024-04-19 18:08:15 +0000

    chore: lint and code formating

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,8 +3,13 @@
 <template>
   <div class="mt-10">
     <h1>Welcome To the CNC portal</h1>
-    <p>Our Website is still in construction. Look into our wonderfull tip feature and try to tip your team</p>
-    
-    <button class="mt-6 btn btn-secondary"><RouterLink to="/teams">Tip your Team</RouterLink></button>
+    <p>
+      Our Website is still in construction. Look into our wonderfull tip feature and try to tip your
+      team
+    </p>
+
+    <button class="mt-6 btn btn-secondary">
+      <RouterLink to="/teams">Tip your Team</RouterLink>
+    </button>
   </div>
 </template>
