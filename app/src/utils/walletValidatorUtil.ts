97e2bd23677ffd5f254fa6a1e0c716a51fd8f70f
Dasarath G <dasarathg68@gmail.com>
2024-04-22 21:01:39 +0530

    feat: created wallet address validator util

---
+++ app/src/utils/walletValidatorUtil.ts
@@ -0,0 +1,7 @@
+export const isWalletAddressValid = (address: string) => {
+  // checks if the input string starts with "0x" and consists of exactly 40 hexadecimal characters.
+  const regex = /^(0x)?[0-9a-fA-F]{40}$/
+
+  // Check if the address matches the regex pattern
+  return regex.test(address)
+}
