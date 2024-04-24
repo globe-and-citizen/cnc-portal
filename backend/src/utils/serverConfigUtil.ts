94504b94f13f50364ba93e344d0ab2f3fb2c93bb
aliassan <alimkwananzi@gmail.com>
2024-04-24 10:03:06 +0200

    remove: remove unused error handling code

--- backend/src/utils/serverConfigUtil.ts
+++ backend/src/utils/serverConfigUtil.ts
@@ -1,32 +1,3 @@
-type Origin = string | undefined
-type StaticOrigin = boolean | string | RegExp | (boolean | string | RegExp)[]
-type CorsCallback = (err: Error | null, origin?: StaticOrigin) => void
-
-const validateOrigin = (origin: Origin, callback: CorsCallback) => {
-    const frontendUrl = process.env.FRONTEND_URL
-    console.log("[src][utils][serverConfigUtil.ts][validateOrigin] origin: ", origin)
-    if (!frontendUrl) {
-        //If the FRONTEND_URL is not set, throw an error
-        const msg = `The environment variable FRONTEND_URL is not set. 
-            Please set it to allow CORS`
-        return callback(new Error(msg), false)
-    }
-
-    if (origin !== frontendUrl) {
-        //If the origin does not match the FRONTEND_URL, throw an error
-        const msg = `The CORS policy for this site allow access from 
-            ${origin}.`
-        return callback(new Error(msg), false)
-    }
-
-    return callback(null, true)
-}
-
-export const corsOptions = {
-    origin: validateOrigin,
-    optionsSuccessStatus: 200
-}
-
 export const errorMessages = {
     frontendUrl: `
         Required environment variable 'FRONTEND_URL' not set.
