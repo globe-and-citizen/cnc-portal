6df72e1a77d829db1bf9486ad21e506b657dc439
farrellh1 <farrell.hauzan@gmail.com>
2024-04-25 22:32:05 +0700

    feat: add upgradeable deployment

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 import '@nomicfoundation/hardhat-chai-matchers'
+import '@openzeppelin/hardhat-upgrades'
 import dotenv from 'dotenv'
 import { NetworksUserConfig } from 'hardhat/types'
 
---
+++ contract/scripts/deploy.ts
@@ -0,0 +1,12 @@
+import { ethers, upgrades } from 'hardhat'
+
+async function main() {
+  const Tips = await ethers.getContractFactory('Tips')
+  const tips = await upgrades.deployProxy(Tips)
+  const contract = await tips.waitForDeployment()
+
+  console.log('Tips deployed to:', await contract.getAddress());
+  
+}
+
+main()
