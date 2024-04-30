c8c2435b1b40c5a6cd9a0e7e5e97c933b6a99a03
Hermann EHO <hermannleboss@gmail.com>
2024-04-30 20:52:35 +0000

    feat: proxy deploy script

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,11 +1,23 @@
 import { ethers, upgrades } from 'hardhat'
 
 async function main() {
-  const Tips = await ethers.getContractFactory('Tips')
-  const tips = await upgrades.deployProxy(Tips)
-  const contract = await tips.waitForDeployment()
+  const TipsImplementation = await ethers.getContractFactory('Tips')
+  const tipsProxy = await upgrades.deployProxy(TipsImplementation)
+  const contract = await tipsProxy.waitForDeployment()
+  
+  const implementationAddress = await upgrades.erc1967.getImplementationAddress(
+    await contract.getAddress()
+  );
+
 
   console.log('Tips deployed to:', await contract.getAddress())
+  
+  console.log("Implementation contract address: " + implementationAddress);
 }
 
-main()
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
