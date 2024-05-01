1c451fb1e18c1e032570444d8546204e80ae41fc
Hermann EHO <hermannleboss@gmail.com>
2024-05-01 12:54:42 +0000

    chore: format

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -4,20 +4,19 @@ async function main() {
   const TipsImplementation = await ethers.getContractFactory('Tips')
   const tipsProxy = await upgrades.deployProxy(TipsImplementation)
   const contract = await tipsProxy.waitForDeployment()
-  
+
   const implementationAddress = await upgrades.erc1967.getImplementationAddress(
     await contract.getAddress()
-  );
-
+  )
 
   console.log('Tips deployed to:', await contract.getAddress())
-  
-  console.log("Implementation contract address: " + implementationAddress);
+
+  console.log('Implementation contract address: ' + implementationAddress)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
