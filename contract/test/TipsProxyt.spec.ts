8d31d3a20550b5dd345d01e374338bc66dd6ec2a
Hermann EHO <hermannleboss@gmail.com>
2024-05-01 12:53:49 +0000

    test: fix tips test

--- contract/test/TipsProxyt.spec.ts
+++ contract/test/TipsProxyt.spec.ts
@@ -1,16 +1,18 @@
 import { ethers, ignition } from 'hardhat'
-import { expect } from "chai";
+import { expect } from 'chai'
 
-import TipsModule from "../ignition/modules/ProxyModule";
-describe.only("Tips Proxy", function () {
-    describe("Proxy interaction", async function () {
-      it("Should be interactable via proxy", async function () {
-        const [owner, otherAccount] = await ethers.getSigners();
-  
-        const { tips } = await ignition.deploy(TipsModule);
-  
-        expect(await tips.connect(otherAccount).owner()).to.equal("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-      });
-    });
-  
-  });
+import TipsModule from '../ignition/modules/ProxyModule'
+describe.only('Tips Proxy', function () {
+  describe('Proxy interaction', async function () {
+    it('Should be interactable via proxy', async function () {
+      const [owner, otherAccount] = await ethers.getSigners()
+      console.log('owner.address()', await owner.address)
+
+      const { tips } = await ignition.deploy(TipsModule)
+
+      expect(await tips.connect(otherAccount).owner()).to.equal(
+        '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+      )
+    })
+  })
+})
