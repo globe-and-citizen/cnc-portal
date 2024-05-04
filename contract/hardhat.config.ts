559bc9a298d3968c10a4ea4fcc3e364a5ee27be9
aliassan <alimkwananzi@gmail.com>
2024-05-04 11:34:35 +0200

    feat: added mnemonic seed for hardhat localhost

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -6,7 +6,11 @@ import { NetworksUserConfig } from 'hardhat/types'
 
 dotenv.config()
 let networks: NetworksUserConfig = {
-  hardhat: {}
+  hardhat: {
+    accounts: {
+      mnemonic: "seat case decrease cereal begin save surface truth pride keen reject gauge"
+    }
+  }
 }
 if (process.env.ALCHEMY_API_KEY === undefined || process.env.PRIVATE_KEY === undefined) {
   console.error('\x1b[33m Please set your ALCHEMY_API_KEY and PRIVATE_KEY in a .env file\x1b[0m')
