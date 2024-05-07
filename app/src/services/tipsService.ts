22c9992577095efe8f9043eef609270e04527dc6
farrellh1 <farrell.hauzan@gmail.com>
2024-05-07 19:36:23 +0700

    feat: create tipsService

---
+++ app/src/services/tipsService.ts
@@ -0,0 +1,87 @@
+import { type IWeb3Library } from '@/adapters/web3LibraryAdapter'
+import { TIPS_ADDRESS } from '@/constant'
+import type { AddressLike } from 'ethers'
+import ABI from '../abi/tips.json'
+import type { TipsEventType } from '@/types'
+import type { EventLog } from 'ethers'
+
+interface ISmartContract {
+  contract: any
+  contractAddress: string
+  contractAbi: any
+  getContract(address: string, abi: any): Promise<any>
+}
+
+export class SmartContract implements ISmartContract {
+  contract: any
+  contractAddress: string
+  contractAbi: any
+  web3Library: IWeb3Library
+
+  constructor(web3Library: IWeb3Library, contractAddress: string, contractAbi: any) {
+    this.web3Library = web3Library
+    this.contractAddress = contractAddress
+    this.contractAbi = contractAbi
+  }
+
+  async getContract(): Promise<any> {
+    if (!this.contract) {
+      this.contract = await this.web3Library.getContract(this.contractAddress, this.contractAbi)
+    }
+
+    return this.contract
+  }
+}
+
+export class TipsService extends SmartContract implements ISmartContract {
+  constructor(web3Library: IWeb3Library) {
+    super(web3Library, TIPS_ADDRESS, ABI)
+  }
+
+  async pushTip(addresses: AddressLike[], amount: number): Promise<void> {
+    if (!this.contract) {
+      this.getContract()
+    }
+
+    const tx = this.contract.pushTip(addresses, {
+      value: this.web3Library.parseEther(amount.toString())
+    })
+    await tx.wait()
+  }
+
+  async sendTip(addresses: AddressLike[], amount: number): Promise<void> {
+    if (!this.contract) {
+      this.getContract()
+    }
+
+    const tx = this.contract.sendTip(addresses, {
+      value: this.web3Library.parseEther(amount.toString())
+    })
+    await tx.wait()
+  }
+
+  async getBalance(): Promise<string> {
+    if (!this.contract) {
+      this.getContract()
+    }
+
+    return this.contract.getBalance()
+  }
+
+  async withdrawTips(): Promise<void> {
+    if (!this.contract) {
+      this.getContract()
+    }
+
+    const tx = this.contract.withdraw()
+    await tx.wait()
+  }
+
+  async getEvents(type: TipsEventType): Promise<EventLog[]> {
+    if (!this.contract) {
+      this.getContract()
+    }
+
+    return this.contract.queryFilter(type)
+  }
+}
