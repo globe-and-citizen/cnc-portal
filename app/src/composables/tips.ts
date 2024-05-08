06b33b128b350b53a9fc8a683d5f55968cb62303
farrellh1 <farrell.hauzan@gmail.com>
2024-05-08 14:43:39 +0700

    refactor: tips composable

--- app/src/composables/tips.ts
+++ app/src/composables/tips.ts
@@ -3,81 +3,96 @@ import { TipsService } from '@/services/tipsService'
 import type { EventResult, TipsEventType } from '@/types'
 import dayjs from 'dayjs'
 import type { EventLog } from 'ethers'
-import { ref, type Ref } from 'vue'
+import { ref } from 'vue'
 
 export function useTips() {
   const tipsService = new TipsService(EthersJsAdapter.getInstance())
-  const balance = ref('0')
-  const pushTipLoading = ref(false)
-  const sendTipLoading = ref(false)
-  const balanceLoading = ref(false)
+  const events = ref<EventResult[]>([])
+  const balance = ref<string>('0')
+  const loading = ref(false)
+  const error = ref<any>(null)
 
   async function getBalance() {
-    balanceLoading.value = true
-
-    balance.value = await tipsService.getBalance()
-
-    balanceLoading.value = false
+    try {
+      loading.value = true
+      balance.value = await tipsService.getBalance()
+    } catch (err) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
   }
 
   async function withdraw() {
-    await tipsService.withdrawTips()
-    await getBalance()
+    try {
+      loading.value = true
+      await tipsService.withdrawTips()
+      await getBalance()
+    } catch (err) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
   }
 
   async function pushTip(addresses: string[], amount: number): Promise<void> {
-    pushTipLoading.value = true
-
-    await tipsService.pushTip(addresses, amount)
-    await getBalance()
-
-    pushTipLoading.value = false
+    try {
+      loading.value = true
+      await tipsService.pushTip(addresses, amount)
+      await getBalance()
+    } catch (err) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
   }
 
   async function sendTip(addresses: string[], amount: number): Promise<void> {
-    sendTipLoading.value = true
-
-    await tipsService.sendTip(addresses, amount)
-    await getBalance()
-
-    sendTipLoading.value = false
+    try {
+      loading.value = true
+      await tipsService.sendTip(addresses, amount)
+      await getBalance()
+    } catch (err) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
   }
 
-  async function getEvents(type: TipsEventType, loading: Ref<boolean>): Promise<EventResult[]> {
-    loading.value = true
+  async function getEvents(type: TipsEventType): Promise<void> {
+    try {
+      loading.value = true
+      const response = await tipsService.getEvents(type)
+      events.value = await Promise.all(
+        response.map(async (eventData: EventLog) => {
+          const date = dayjs((await eventData.getBlock()).date).format('DD/MM/YYYY HH:mm')
 
-    const events = await tipsService.getEvents(type)
-
-    const result = await Promise.all(
-      events.map(async (eventData: EventLog) => {
-        const date = dayjs((await eventData.getBlock()).date).format('DD/MM/YYYY HH:mm')
-
-        return {
-          txHash: eventData.transactionHash,
-          date: date,
-          data: tipsService.contract.interface.decodeEventLog(
-            type,
-            eventData.data,
-            eventData.topics
-          )
-        }
-      })
-    )
-
-    loading.value = false
-
-    return result
+          return {
+            txHash: eventData.transactionHash,
+            date: date,
+            data: tipsService.contract.interface.decodeEventLog(
+              type,
+              eventData.data,
+              eventData.topics
+            )
+          }
+        })
+      )
+    } catch (err) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
   }
 
   return {
     balance,
-    pushTipLoading,
-    sendTipLoading,
-    balanceLoading,
+    loading,
+    error,
     getBalance,
     withdraw,
     pushTip,
     sendTip,
-    getEvents
+    getEvents,
   }
 }
