3312589635212106493119f3d5957bc90081cfb6
farrellh1 <farrell.hauzan@gmail.com>
2024-05-09 23:04:57 +0700

    feat: install vue-toastification lib

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Toast, { POSITION, type PluginOptions } from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +11,13 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+const toastOptions: PluginOptions = {
+    position: POSITION.BOTTOM_RIGHT,
+    pauseOnFocusLoss: false,
+    pauseOnHover: false,
+    timeout: 3000,
+}
+app.use(Toast, toastOptions)
 app.use(router)
 
 app.mount('#app')
