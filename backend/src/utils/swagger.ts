6e2564786b057749f0f0bd57f9d5e6252b5e07be
Dasarath G <dasarathg68@gmail.com>
2024-05-10 03:14:44 +0530

    fix: update deprecated redoc-cli to redocly

--- backend/src/utils/swagger.ts
+++ backend/src/utils/swagger.ts
@@ -2,7 +2,7 @@ const { exec } = require("child_process");
 
 const generateDocs = () => {
   exec(
-    "redoc-cli bundle -o src/utils/backend_specs.html src/utils/backend_specs.yaml",
+    "redocly build-docs -o src/utils/backend_specs.html src/utils/backend_specs.yaml",
     (error: { message: any }, stdout: any, stderr: any) => {
       if (error) {
         console.log(`error: ${error.message}`);
