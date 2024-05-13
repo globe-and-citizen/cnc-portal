4a5909e5787de7ee4145a6ce60ccf1d81c3e833c
Dasarath G <dasarathg68@gmail.com>
2024-05-13 20:03:39 +0530

    fix: get browser theme

--- app/src/components/NavBar.vue
+++ app/src/components/NavBar.vue
@@ -29,45 +29,14 @@
           <li><a @click="tipsStore.withdrawTips()">Withdraw Tips</a></li>
         </ul>
       </div>
-      <label className="flex cursor-pointer gap-2">
-        <svg
-          v-if="isDark"
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <circle cx="12" cy="12" r="5" />
-          <path
-            d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
-          />
-        </svg>
-        <svg
-          v-else
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-        </svg>
-        <input
-          type="checkbox"
-          :value="isDark ? 'light' : 'dark'"
-          @change="$emit('toggleTheme')"
-          className="toggle theme-controller"
-        />
-      </label>
+
+      <input
+        type="checkbox"
+        :value="theme ? 'light' : 'dark'"
+        :checked="!isDark"
+        @change="emits('toggleTheme')"
+        className="toggle theme-controller"
+      />
       <button class="btn btn-ghost btn-circle">
         <div class="indicator">
           <IconBell />
@@ -104,17 +73,19 @@
 <script setup lang="ts">
 import { useTipsStore } from '@/stores/tips'
 import { storeToRefs } from 'pinia'
-import { onMounted } from 'vue'
+import { onMounted, computed } from 'vue'
 import { logout } from '@/utils/navBarUtil'
 import IconHamburgerMenu from '@/components/icons/IconHamburgerMenu.vue'
 import IconBell from '@/components/icons/IconBell.vue'
-import { ref } from 'vue'
 
 const props = defineProps(['isDark'])
 const emits = defineEmits(['toggleSideButton', 'toggleEditUserModal', 'toggleTheme'])
 const tipsStore = useTipsStore()
 const { balance } = storeToRefs(tipsStore)
-
+const theme = computed(() => {
+  // Check if the prefers-color-scheme is dark
+  return window.matchMedia('(prefers-color-scheme: dark)').matches
+})
 onMounted(async () => {
   await tipsStore.getBalance()
 })
