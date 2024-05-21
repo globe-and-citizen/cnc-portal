5f1dc8f10f2ae752a68183ec94d14248877deaa1
aliassan <alimkwananzi@gmail.com>
2024-05-21 16:38:28 +0200

    fix: remove unnecessary details from metamask error
    Metamsk error was including all details which were not necessary for
    the user causing a very long and confusing error notification, this
    makes it more concise and less verbose

---
+++ app/src/utils/errorUtil.ts
@@ -0,0 +1,71 @@
+type MetaMaskErrorInfo = {
+    error: { code: number; message: string };
+    payload: { method: string; params: string[]; jsonrpc: string }
+}
+
+/**
+ * Parses an error object to extract a meaningful error message.
+ *
+ * This function takes an error object, checks if it is an instance of the 
+ * `Error` class, and then further inspects if it is a MetaMask error.
+ * 
+ * Depending on the structure and type of the error object, it returns a 
+ * relevant error message. If the type of the error object cannot be
+ * determined it returns a generic error.
+ *
+ * @param error - The error object to be parsed. It can be of any type.
+ * @returns A string containing the error message. 
+ */
+export const parseError = (error: any) => {
+    let message: string
+
+    if (error instanceof Error) {
+        if ("info" in error && isMetaMaskErrorInfo(error.info)) {
+            message = parseErrorInfo(error.info as MetaMaskErrorInfo)        
+        } else {
+            message = error.message
+        }
+    } else {
+        message = "Looks like something went wrong. Try again."
+    }
+
+    return message
+}
+
+/**
+ * A type-guard function for `MetaMaskErrorInfo`.
+ * 
+ * It checks whether an object has a structure consistent with the `info`
+ * field of a MetaMask error.
+ * 
+ * @param info - An `info` object to check. It can be of type any
+ * @returns A `boolean` indicating whether the supplied object implements 
+ * the MetaMaskErrorInfo interface
+ */
+const isMetaMaskErrorInfo = (info: any): info is MetaMaskErrorInfo => {
+    return typeof info === 'object' &&
+        info !== null &&
+        'error' in info &&
+        typeof info.error === 'object' &&
+        typeof info.error.code === 'number' &&
+        typeof info.error.message === 'string' &&
+        'payload' in info &&
+        typeof info.payload === 'object' &&
+        typeof info.payload.method === 'string' &&
+        Array.isArray(info.payload.params) &&
+        info.payload.params.every((param: any) => typeof param === 'string') &&
+        typeof info.payload.jsonrpc === 'string';
+}
+
+/**
+ * Parses an error info object to extract the `message` property excluding other 
+ * meta-data
+ * 
+ * @param info - An info object to be parsed. Should be of MetaMaskErrorInfo
+ * @returns A string containing only the message and no extra meta-data
+ */
+const parseErrorInfo = (info: MetaMaskErrorInfo) => {
+    const A: string[] = info.error.message.split(':')
+
+    return A[1].trim()
+}
