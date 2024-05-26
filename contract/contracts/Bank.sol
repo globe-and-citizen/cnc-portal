1284a904e9c9a614341135f0ab1aba9fe5326740
farrellh1 <farrell.hauzan@gmail.com>
2024-05-26 12:53:17 +0700

    feat: add event TipsAddressChanged

--- contract/contracts/Bank.sol
+++ contract/contracts/Bank.sol
@@ -2,16 +2,21 @@
 pragma solidity ^0.8.24;
 
 import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
-import {Tips} from './Tips.sol';
 
 contract Bank is Ownable {
   address public tipsAddress;
+
+  event TipsAddressChanged(address oldAddress, address newAddress);
 
   constructor(address _tipsAddress) Ownable(msg.sender) {
     tipsAddress = _tipsAddress;
   }
 
   function changeTipsAddress(address _tipsAddress) external onlyOwner {
+    require(_tipsAddress != address(0), "Address cannot be zero");
+
+    address oldAddress = tipsAddress;
     tipsAddress = _tipsAddress;
+    emit TipsAddressChanged(oldAddress, _tipsAddress);
   }
 }
