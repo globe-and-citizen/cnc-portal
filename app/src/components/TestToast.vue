087a7e4c0cf0d8dfab01d2a306b2e56bb05d3961
aliassan <alimkwananzi@gmail.com>
2024-05-28 11:59:57 +0200

    feat: add visibility logic to toast component
    In old toast system each compnent that used the toast component had
    to implement logic to toggle visibility of the toast component,
    this implements the logic in the component itself so other
    components don't have to repeat the functionality

---
+++ app/src/components/TestToast.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="visible" class="toast toast-end">
+    <div
+      class="alert"
+      :class="{
+        'alert-success': type === ToastType.Success,
+        'alert-info': type === ToastType.Info,
+        'alert-warning': type === ToastType.Warning,
+        'alert-error': type === ToastType.Error
+      }"
+    >
+      <IconCheck v-if="type === ToastType.Success" />
+      <IconInfo v-if="type === ToastType.Info" />
+      <IconWarning v-if="type === ToastType.Warning" />
+      <IconError v-if="type === ToastType.Error" />
+      <span>{{ message }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref, onMounted } from 'vue'
+import { ToastType } from '@/types'
+import IconCheck from '@/components/icons/IconCheck.vue'
+import IconInfo from '@/components/icons/IconInfo.vue'
+import IconWarning from '@/components/icons/IconWarning.vue'
+import IconError from '@/components/icons/IconError.vue'
+defineProps<{ type: ToastType; message: string }>()
+
+const visible = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    visible.value = false
+  }, 5000)
+})
+</script>
