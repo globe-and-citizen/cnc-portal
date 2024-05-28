7c3d531529df83da02e5808128e57766de810cbd
Hermann EHO <hermannleboss@gmail.com>
2024-05-28 17:50:06 +0000

    chore: Update backend vitest workflow to include PostgreSQL service and environment variables

--- backend/test/userController.test.ts
+++ backend/test/userController.test.ts
@@ -1,6 +1,6 @@
 import { afterAll, beforeAll, describe, expect, it, vitest } from "vitest";
 
-describe("get Nonce", () => {
+it("get Nonce", () => {
   // TODO: Write test cases for getNonce: Calling the endpoint
   expect(1).toBe(1);
 });
