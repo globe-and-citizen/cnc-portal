e0102493c8ab28881f4a9487b2c3db1a9233406d
aliassan <alimkwananzi@gmail.com>
2024-05-29 15:39:28 +0200

    refactor: use computed values for alert classes
    This commit uses computed values for the alert classes instead of
    coding the logic in the attribute binding to make the code more
    readable and for perfomance optimization

--- app/src/components/TestToast.vue
+++ app/src/components/TestToast.vue
@@ -1,14 +1,6 @@
 <template>
-  <div v-if="visible" class="toast toast-end">
-    <div
-      class="alert"
-      :class="{
-        'alert-success': type === ToastType.Success,
-        'alert-info': type === ToastType.Info,
-        'alert-warning': type === ToastType.Warning,
-        'alert-error': type === ToastType.Error
-      }"
-    >
+  <div v-if="visible">
+    <div :class="['alert', typeClass]">
       <IconCheck v-if="type === ToastType.Success" />
       <IconInfo v-if="type === ToastType.Info" />
       <IconWarning v-if="type === ToastType.Warning" />
@@ -19,19 +11,29 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, ref, onMounted } from 'vue'
+import { defineProps, ref, onMounted, computed } from 'vue'
 import { ToastType } from '@/types'
 import IconCheck from '@/components/icons/IconCheck.vue'
 import IconInfo from '@/components/icons/IconInfo.vue'
 import IconWarning from '@/components/icons/IconWarning.vue'
 import IconError from '@/components/icons/IconError.vue'
-defineProps<{ type: ToastType; message: string }>()
+
+const props = defineProps<{ type: ToastType; message: string; timeout: number }>()
 
 const visible = ref(true)
+
+const typeClass = computed(() => {
+  return {
+    'alert-success': props.type === ToastType.Success,
+    'alert-info': props.type === ToastType.Info,
+    'alert-warning': props.type === ToastType.Warning,
+    'alert-error': props.type === ToastType.Error
+  }
+})
 
 onMounted(() => {
   setTimeout(() => {
     visible.value = false
-  }, 5000)
+  }, props.timeout)
 })
 </script>
