f8a38d7a4a8914937b776ee6ed1d34d7084ddba0
aliassan <alimkwananzi@gmail.com>
2024-05-29 15:28:02 +0200

    feat: added toast store to share toasts state

---
+++ app/src/stores/useToastStore.ts
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import { ToastType } from '@/types'
+
+interface Toast {
+  message: string;
+  type: ToastType;
+  timeout: number;
+}
+
+export const useToastStore = defineStore('_toast', () => {
+  const toasts = ref<Toast[]>([]);
+
+  const addToast = (message: string, type: ToastType = ToastType.Success, timeout: number = 5000) => {
+    toasts.value.push({ message, type, timeout });
+    setTimeout(() => {
+      toasts.value.shift();
+    }, timeout);
+  };
+
+  return {
+    toasts,
+    addToast
+  };
+});
