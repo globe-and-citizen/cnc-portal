85f4c6b024f9624c6bc5bb66568ec0029d8842c9
farrellh1 <farrell.hauzan@gmail.com>
2024-05-30 23:25:59 +0700

    feat: add interfaces

---
+++ app/src/types/interfaces/contract-transaction-function.interface.ts
@@ -0,0 +1,9 @@
+import type { Ref } from "vue"
+
+export interface IContractTransactionFunction {
+  isLoading: Ref<boolean>
+  isSuccess: Ref<boolean>
+  error: Ref<any>
+  transaction: Ref<any>
+  execute: (...args: any[]) => Promise<void>
+}
