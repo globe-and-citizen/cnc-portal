6e20b45f1cc2491558e9ccb75c79e965f2c3d5e2
Hermann EHO <hermannleboss@gmail.com>
2024-05-31 17:13:06 +0000

    refactor user data store

--- app/src/stores/user.ts
+++ app/src/stores/user.ts
@@ -1,27 +1,22 @@
-import { useStorage } from '@vueuse/core'
 import { defineStore } from 'pinia'
+import { useStorage } from '@vueuse/core'
 
-export const useUserDataStore = defineStore({
-  id: 'userData',
-  state: () => ({
-    name: useStorage('name', ''),
-    address: useStorage('ownerAddress', ''),
-    nonce: useStorage('nonce', '')
-  }),
-  actions: {
-    setUserData(name: string, address: string, nonce: string) {
-      this.name = name
-      this.address = address
-      this.nonce = nonce
-      useStorage('name', name) // Save name to localStorage
-      useStorage('ownerAddress', address) // Save address to localStorage
-      useStorage('nonce', nonce) // Save nonce to localStorage
-    },
-    clearUserData() {
-      this.name = ''
-      this.address = ''
-      this.nonce = ''
-      localStorage.clear() // Clear all localStorage
-    }
+export const useUserDataStore = defineStore('user', () => {
+  const userName = useStorage('name', 'hermann')
+  const userAddress = useStorage('ownerAddress', '')
+  const userNonce = useStorage('nonce', '')
+
+  function setUserData(name: string, address: string, nonce: string) {
+    userName.value = name
+    userAddress.value = address
+    userNonce.value = nonce
   }
+
+  function clearUserData() {
+    userName.value = ''
+    userAddress.value = ''
+    userNonce.value = ''
+  }
+
+  return { name: userName, address: userAddress, nonce: userNonce, setUserData, clearUserData }
 })
