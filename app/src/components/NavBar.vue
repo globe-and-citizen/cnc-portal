2cb9706b7c6b20b6b4ffae4c11cac2eefc12195f
Hermann EHO <hermannleboss@gmail.com>
2024-06-02 14:58:17 +0000

    chore: add data-test attributes to NavBar component buttons

--- app/src/components/NavBar.vue
+++ app/src/components/NavBar.vue
@@ -6,7 +6,7 @@
         <img src="../assets/Logo.png" alt="Logo" />
       </div>
       <div class="">
-        <button class="btn btn-square btn-ghost drawer-overlay" @click="emits('toggleSideButton')">
+        <button class="btn btn-square btn-ghost drawer-overlay" @click="emits('toggleSideButton')" data-test="toggleSideButton">
           <IconHamburgerMenu />
         </button>
       </div>
@@ -17,7 +17,7 @@
           <div class="btn w-full flex flex-row justify-between bg-base-primary">
             <img src="../assets/Ethereum.png" height="20" width="20" alt="Ethereum Icon" />
             <div class="text-xs flex" v-if="balanceLoading">
-              <span class="loading loading-dots loading-xs"></span>
+              <span class="loading loading-dots loading-xs" data-test="balance-loading"></span>
               <span>{{ NETWORK.currencySymbol }}</span>
             </div>
             <div v-else>
@@ -32,10 +32,10 @@
           tabindex="0"
           class="mt-3 dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-48"
         >
-          <li v-if="withdrawLoading">
+          <li v-if="withdrawLoading"  data-test="withdraw-loading">
             <a href="#"> Processing <span class="loading loading-dots loading-xs"></span> </a>
           </li>
-          <li v-else><a @click="emits('withdraw')">Withdraw Tips</a></li>
+          <li v-else><a @click="emits('withdraw')" data-test="withdraw">Withdraw Tips</a></li>
         </ul>
       </div>
       <button class="btn btn-ghost btn-circle">
@@ -58,7 +58,7 @@
           class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
         >
           <li>
-            <a class="justify-between" @click="emits('toggleEditUserModal')">
+            <a class="justify-between" @click="emits('toggleEditUserModal')" data-test="toggleEditUser">
               Profile
               <span class="badge">New</span>
             </a>
