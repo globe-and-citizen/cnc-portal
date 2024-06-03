936cb8690fb025ce30145d55c799442edf004bb1
farrellh1 <farrell.hauzan@gmail.com>
2024-06-03 23:20:53 +0700

    refactor: rename it to more clear

--- contract/test/Bank.spec.ts
+++ contract/test/Bank.spec.ts
@@ -5,17 +5,17 @@ import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers'
 import { ContractTransactionResponse } from 'ethers'
 
 describe('Bank', () => {
-  let bank: Bank
-  let tips: Tips
+  let bankProxy: Bank
+  let tipsProxy: Tips
 
   async function deployContract(owner: SignerWithAddress) {
-    const TipsImpl = await ethers.getContractFactory('Tips')
-    tips = (await upgrades.deployProxy(TipsImpl, [], {
+    const TipsImplementation = await ethers.getContractFactory('Tips')
+    tipsProxy = (await upgrades.deployProxy(TipsImplementation, [], {
       initializer: 'initialize'
     })) as unknown as Tips
 
-    const BankImpl = await ethers.getContractFactory('Bank')
-    bank = (await upgrades.deployProxy(BankImpl, [await tips.getAddress()], {
+    const BankImplementation = await ethers.getContractFactory('Bank')
+    bankProxy = (await upgrades.deployProxy(BankImplementation, [await tipsProxy.getAddress()], {
       initializer: 'initialize',
       initialOwner: await owner.getAddress()
     })) as unknown as Bank
@@ -35,20 +35,20 @@ describe('Bank', () => {
       context('When I deploy a bank contract', async () => {
         before(async () => {
           await deployContract(owner)
-          expect(await bank.getAddress()).to.be.string
-          expect(await tips.getAddress()).to.be.string
-          expect(await bank.tipsAddress()).to.eq(await tips.getAddress())
+          expect(await bankProxy.getAddress()).to.be.string
+          expect(await tipsProxy.getAddress()).to.be.string
+          expect(await bankProxy.tipsAddress()).to.eq(await tipsProxy.getAddress())
         })
 
         it('Then I can operate transaction with it', async () => {
           const amount = ethers.parseEther('1')
-          const bankAddress = await bank.getAddress()
+          const bankAddress = await bankProxy.getAddress()
           await owner.sendTransaction({ to: bankAddress, value: amount })
           expect(await ethers.provider.getBalance(bankAddress)).to.eq(amount)
         })
 
         it('Then I owned the contract', async () => {
-          expect(await bank.owner()).to.eq(await owner.getAddress())
+          expect(await bankProxy.owner()).to.eq(await owner.getAddress())
         })
       })
     })
@@ -70,9 +70,9 @@ describe('Bank', () => {
       context('When I deposit into my team’s bank contract', () => {
         it('Then I can see funds in the team’s bank contract', async () => {
           const amount = ethers.parseEther('1')
-          const bankAddress = await bank.getAddress()
+          const bankAddress = await bankProxy.getAddress()
           await expect(owner.sendTransaction({ to: bankAddress, value: amount }))
-            .to.emit(bank, 'Deposited')
+            .to.emit(bankProxy, 'Deposited')
             .withArgs(owner.address, amount)
           expect(await ethers.provider.getBalance(bankAddress)).to.eq(amount)
         })
@@ -95,15 +95,15 @@ describe('Bank', () => {
       context('When I have a service I want to buy', () => {
         before(async () => {
           const amount = ethers.parseEther('5')
-          await owner.sendTransaction({ to: await bank.getAddress(), value: amount })
+          await owner.sendTransaction({ to: await bankProxy.getAddress(), value: amount })
         })
 
         it("Then I can operate transfer operation to a provider company account as payment for the service I'm buying. Using my team's bank contract funds", async () => {
-          const tx = await bank.transfer(digitalService.address, ethers.parseEther('5'))
+          const tx = await bankProxy.transfer(digitalService.address, ethers.parseEther('5'))
 
-          expect(tx).to.emit(bank, 'Transfer')
+          expect(tx).to.emit(bankProxy, 'Transfer')
           expect(tx).to.changeEtherBalance(digitalService, ethers.parseEther('5'))
-          expect(tx).to.changeEtherBalance(bank, -ethers.parseEther('5'))
+          expect(tx).to.changeEtherBalance(bankProxy, -ethers.parseEther('5'))
         })
       })
     })
@@ -120,7 +120,10 @@ describe('Bank', () => {
       before(async () => {
         ;[owner, employee] = await ethers.getSigners()
         await deployContract(owner)
-        await owner.sendTransaction({ to: await bank.getAddress(), value: ethers.parseEther('10') })
+        await owner.sendTransaction({
+          to: await bankProxy.getAddress(),
+          value: ethers.parseEther('10')
+        })
       })
 
       context('When I send crypto to my Employee', () => {
@@ -129,12 +132,12 @@ describe('Bank', () => {
 
         before(async () => {
           amount = ethers.parseEther('5')
-          tx = await bank.transfer(employee.address, amount)
+          tx = await bankProxy.transfer(employee.address, amount)
         })
 
         it('Then he will receive it in his wallet', async () => {
           expect(tx).to.changeEtherBalance(employee, amount)
-          expect(tx).to.changeEtherBalance(bank, -amount)
+          expect(tx).to.changeEtherBalance(bankProxy, -amount)
         })
       })
     })
@@ -152,23 +155,26 @@ describe('Bank', () => {
       before(async () => {
         ;[owner, member1, member2] = await ethers.getSigners()
         await deployContract(owner)
-        await owner.sendTransaction({ to: await bank.getAddress(), value: ethers.parseEther('12') })
+        await owner.sendTransaction({
+          to: await bankProxy.getAddress(),
+          value: ethers.parseEther('12')
+        })
       })
 
       context('When I call send tips with my team address', () => {
         it("Then my team members' balance increases.", async () => {
-          await bank
+          await bankProxy
             .connect(owner)
             .sendTip([member1.address, member2.address], ethers.parseEther('6'))
 
-          expect(await tips.getBalance(member1.address)).to.eq(ethers.parseEther('3'))
-          expect(await tips.getBalance(member2.address)).to.eq(ethers.parseEther('3'))
+          expect(await tipsProxy.getBalance(member1.address)).to.eq(ethers.parseEther('3'))
+          expect(await tipsProxy.getBalance(member2.address)).to.eq(ethers.parseEther('3'))
         })
       })
 
       context('When I call push tips with my team address', () => {
         it("Then my team members' balance increases.", async () => {
-          const tx = await bank
+          const tx = await bankProxy
             .connect(owner)
             .pushTip([member1.address, member2.address], ethers.parseEther('6'))
           expect(tx).to.changeEtherBalance(member1, ethers.parseEther('3'))
