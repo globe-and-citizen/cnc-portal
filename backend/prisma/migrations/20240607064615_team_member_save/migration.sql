7ef4c22e7e529c336b6b638374315fe8a54bbbe7
Hermann EHO <hermannleboss@gmail.com>
2024-06-07 13:17:47 +0000

    refactor: update migration

--- backend/prisma/migrations/20240607064615_team_member_save/migration.sql
+++ backend/prisma/migrations/20240607064615_team_member_save/migration.sql
@@ -2,21 +2,14 @@
   Warnings:
 
   - You are about to drop the column `ownerId` on the `Team` table. All the data in the column will be lost.
-  - You are about to drop the `Member` table. If the table is not empty, all the data it contains will be lost.
   - Added the required column `ownerAddress` to the `Team` table without a default value. This is not possible if the table is not empty.
 
 */
--- DropForeignKey
-ALTER TABLE "Member" DROP CONSTRAINT "Member_teamId_fkey";
-
 -- DropForeignKey
 ALTER TABLE "Team" DROP CONSTRAINT "Team_ownerId_fkey";
 
 -- AlterTable
 ALTER TABLE "Team" RENAME  COLUMN "ownerId" to "ownerAddress";
-
--- DropTable
-DROP TABLE "Member";
 
 -- CreateTable
 CREATE TABLE "_MemberTeams" (
