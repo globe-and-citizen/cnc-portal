d2ffd63ddfef4055dd1ac792c7abc85dbe62a821
Dasarath G <dasarathg68@gmail.com>
2024-06-09 11:53:04 +0530

    feat: add confirmation dialog before delete

--- app/src/components/MemberCard.vue
+++ app/src/components/MemberCard.vue
@@ -6,18 +6,26 @@
       <button
         v-if="!isOwner"
         class="btn btn-error btn-xs"
-        @click="emits('deleteMember', teamId, member.address)"
+        @click="showDeleteConfirmModal = !showDeleteConfirmModal"
       >
         Delete
       </button>
     </th>
   </tr>
+  <DeleteConfirmModal
+    :showDeleteConfirmModal="showDeleteConfirmModal"
+    @toggleDeleteConfirmModal="showDeleteConfirmModal = !showDeleteConfirmModal"
+    @deleteMember="emits('deleteMember', teamId, member.address)"
+    :member="member"
+  />
 </template>
 <script setup lang="ts">
 import type { MemberInput } from '@/types'
+import DeleteConfirmModal from '@/components/modals/DeleteConfirmModal.vue'
 import { ref } from 'vue'
 
 const emits = defineEmits(['deleteMember'])
+const showDeleteConfirmModal = ref(false)
 const props = defineProps<{
   member: Partial<MemberInput>
   teamId: Number
