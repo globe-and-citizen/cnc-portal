fe6c461b6f1c5589fa037c933fcbd92bcc4eef72
Dasarath G <dasarathg68@gmail.com>
2024-06-09 12:06:56 +0530

    feat: updated tests for membercard

--- app/src/components/__tests__/MemberCard.spec.ts
+++ app/src/components/__tests__/MemberCard.spec.ts
@@ -1,15 +1,23 @@
 import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
 import MemberCard from '@/components/MemberCard.vue'
+import DeleteConfirmModal from '@/components/modals/DeleteConfirmModal.vue'
 
 describe('MemberCard', () => {
-  const props = {
-    member: { name: 'Dasarath', address: '0x4b6Bf5cD91446408290725879F5666dcd9785F62' },
-    teamId: 1
-  }
-
-  const wrapper = mount(MemberCard, { props })
-
+  const member = { name: 'Dasarath', address: '0x4b6Bf5cD91446408290725879F5666dcd9785F62' }
+  const teamId = 1
+  const isOwner = false
+  const props = { member, teamId, isOwner }
+  const wrapper = mount(MemberCard, {
+    props,
+    global: {
+      stubs: {
+        DeleteConfirmModal: {
+          template: '<button @click="$emit(\'deleteMember\')"></button>'
+        }
+      }
+    }
+  })
   // Check if the component is rendered properly
   describe('Render', () => {
     it('Should render the component', () => {
@@ -20,16 +28,21 @@ describe('MemberCard', () => {
       expect(wrapper.text()).toContain(props.member.name)
       expect(wrapper.text()).toContain(props.member.address)
     })
-
-    it('Should render a delete button', () => {
-      expect(wrapper.find('button').text()).toContain('Delete')
-    })
   })
 
   describe('Emits', () => {
-    it('Should emit deleteMember event with correct arguments when delete button is clicked', async () => {
-      await wrapper.find('button').trigger('click')
-      expect(wrapper.emitted('deleteMember')).toBeTruthy()
+    it('Should emit deleteMember event when deleteMember event is received', async () => {
+      await wrapper.find('button.btn-error').trigger('click')
+      expect((wrapper.vm as any)?.showDeleteConfirmModal).toBe(true)
+
+      // Simulate deleteMember event from modal
+      await wrapper.findComponent(DeleteConfirmModal).vm.$emit('deleteMember')
+
+      const emittedEvents = wrapper.emitted('deleteMember')
+      expect(emittedEvents).toBeTruthy()
+      if (emittedEvents) {
+        expect(emittedEvents[0]).toEqual([teamId, member.address])
+      }
     })
   })
 })
