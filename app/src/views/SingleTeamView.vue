13142b1aea611acbf6ce7504dcaf2f9bd9f806e5
Dasarath G <dasarathg68@gmail.com>
2024-06-12 16:30:55 +0530

    fix: merge conflicts

--- app/src/views/SingleTeamView.vue
+++ app/src/views/SingleTeamView.vue
@@ -1,36 +1,53 @@
 <template>
   <div class="pt-10 flex flex-col gap-5">
     <div class="flex justify-between gap-5">
-      <div>
-        <h2 class="pl-5">{{ team.name }}</h2>
-        <p class="pl-5 pb-3 text-xl">{{ team.description }}</p>
-        <p class="pl-5" v-if="team.bankAddress">Bank Contract Address: {{ team.bankAddress }}</p>
-        <p class="pl-5" v-if="team.bankAddress && !balanceLoading">
-          Team Balance: {{ teamBalance }} {{ NETWORK.currencySymbol }}
-        </p>
-        <p class="pl-5 flex flex-row gap-2" v-if="balanceLoading">
-          <span>Team Balance: </span>
-          <SkeletonLoading class="w-40 h-4 self-center" />
-        </p>
-      </div>
-      <div class="flex flex-wrap justify-between gap-1 items-center">
-        <button
-          class="btn btn-primary btn-disabled"
-          @click="bankModal = true"
-          v-if="!team.bankAddress"
-        >
-          Create Bank Account Smart Contract
-        </button>
-        <button class="btn btn-primary" @click="depositModal = true" v-if="team.bankAddress">
-          Deposit
-        </button>
-        <button class="btn btn-primary" @click="transferModal = true" v-if="team.bankAddress">
-          Transfer
-        </button>
-        <button class="btn btn-primary" @click="updateTeamModalOpen">Update</button>
+      <div
+        class="collapse collapse-arrow border"
+        :class="`${team.ownerAddress == useUserDataStore().address ? 'bg-green-100' : 'bg-blue-100'}`"
+      >
+        <input type="checkbox" />
+        <div class="collapse-title text-xl font-medium">
+          <div class="flex items-center justify-center">
+            <h2 class="pl-5">{{ team.name }}</h2>
+            <div
+              class="badge badge-sm badge-primary flex items-center justify-center ml-2"
+              v-if="team.ownerAddress == useUserDataStore().address"
+            >
+              Owner
+            </div>
+            <div class="badge badge-sm badge-secondary ml-2" v-else>Employee</div>
+          </div>
+        </div>
+        <div class="collapse-content">
+          <p class="pl-5">{{ team.description }}</p>
+          <p class="pl-5" v-if="team.bankAddress">Bank Contract Address: {{ team.bankAddress }}</p>
+          <p class="pl-5" v-if="team.bankAddress && !balanceLoading">
+            Team Balance: {{ teamBalance }} {{ NETWORK.currencySymbol }}
+          </p>
+          <p class="pl-5 flex flex-row gap-2" v-if="balanceLoading">
+            <span>Team Balance: </span>
+            <SkeletonLoading class="w-40 h-4 self-center" />
+          </p>
 
-        <button class="btn btn-primary" @click="deleteTeam">Delete Team</button>
+          <div class="pl-5 flex flex-row justify-center gap-2 mt-5 items-center">
+            <button
+              class="btn btn-secondary btn-sm"
+              v-if="team.ownerAddress == useUserDataStore().address"
+              @click="updateTeamModalOpen"
+            >
+              Update
+            </button>
+            <button
+              class="btn btn-error btn-sm"
+              v-if="team.ownerAddress == useUserDataStore().address"
+              @click="showDeleteConfirmModal = !showDeleteConfirmModal"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
       </div>
+
       <DeleteConfirmModal
         :showDeleteConfirmModal="showDeleteConfirmModal"
         @toggleDeleteConfirmModal="showDeleteConfirmModal = !showDeleteConfirmModal"
@@ -40,6 +57,23 @@
         <span class="font-bold">{{ team.name }}</span
         >?
       </DeleteConfirmModal>
+    </div>
+    <div class="flex justify-between">
+      <button
+        class="btn btn-primary btn-disabled"
+        @click="bankModal = true"
+        v-if="!team.bankAddress"
+      >
+        Create Bank Account Smart Contract
+      </button>
+      <div class="flex gap-2">
+        <button class="btn btn-primary" @click="depositModal = true" v-if="team.bankAddress">
+          Deposit
+        </button>
+        <button class="btn btn-primary" @click="transferModal = true" v-if="team.bankAddress">
+          Transfer
+        </button>
+      </div>
     </div>
     <div
       class="bg-base-100 flex h-16 items-center rounded-xl text-sm font-bold justify-between px-4"
@@ -312,7 +346,6 @@ const team = ref<Team>({
   description: '',
   bankAddress: null,
   members: [],
-  ownerAddress: '',
   ownerAddress: ''
 })
 
