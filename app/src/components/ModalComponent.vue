039d999181bc72be4520005d1529311512fc0016
Hermann EHO <hermannleboss@gmail.com>
2024-06-14 21:36:40 +0000

    feat: modal dual bind and key and click event

--- app/src/components/ModalComponent.vue
+++ app/src/components/ModalComponent.vue
@@ -1,13 +1,35 @@
 <template>
-    <div>
-
+  <dialog
+    id="custom-modal"
+    class="modal modal-bottom sm:modal-middle"
+    :class="{ 'modal-open': toggleOpen }"
+  >
+    <div class="modal-box">
+      <slot></slot>
     </div>
+
+    <form method="dialog" class="modal-backdrop" @click="toggleOpen = false">
+      <button>close</button>
+    </form>
+  </dialog>
 </template>
 
 <script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue'
 
+const toggleOpen = defineModel({ default: false })
+
+const handleEscapePress = (event: KeyboardEvent) => {
+  if (event.key === 'Escape') {
+    toggleOpen.value = false
+  }
+}
+onMounted(() => {
+  document.addEventListener('keydown', handleEscapePress)
+})
+onUnmounted(() => {
+  document.removeEventListener('keydown', handleEscapePress)
+})
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
