7d12aa6937fe42cd1f0c9b610ee91ca96516f9b4
aliassan <alimkwananzi@gmail.com>
2024-06-14 12:51:59 +0200

    feat: add notification composable

---
+++ app/src/composables/useNotification.ts
@@ -0,0 +1,29 @@
+import { NotificationAPI } from "@/apis/notificationApi";
+import { parseError } from "@/utils";
+import { ref } from "vue";
+import { useToastStore } from "@/stores/useToastStore";
+import { useUserDataStore } from "@/stores/user";
+import { ToastType } from "@/types";
+
+const isUnread = ref(false)
+const notifications = ref()
+
+export const useNotification = () => {
+    const { addToast } = useToastStore()
+
+    async function fetchData() {
+        try {
+            notifications.value = await NotificationAPI.getNotifications()
+            const idx = notifications.value.findIndex((notification: any) => notification.isRead === false)
+            /*console.log(`notifications: `, notifications.value)
+            console.log(`idx: `, idx)*/
+            isUnread.value = idx > -1
+        } catch(error) {
+            addToast({message: parseError(error), type: ToastType.Error, timeout: 3000})
+        }
+    } 
+
+    fetchData()
+
+    return {notifications, isUnread}
+}
