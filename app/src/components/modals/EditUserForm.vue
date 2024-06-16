e729b9c3419fc44b825902d60eed5af29268e6b4
Hermann EHO <hermannleboss@gmail.com>
2024-06-16 09:56:47 +0000

    feat: editUser form

---
+++ app/src/components/modals/EditUserForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col gap-5 mt-4">
+    <label class="input input-bordered flex items-center gap-2 input-md">
+      <span class="w-24">Name</span>
+      <input type="text" class="grow" placeholder="John Doe" v-model="user.name" />
+    </label>
+    <label class="input input-bordered flex items-center gap-2 input-md input-disabled">
+      <span class="w-24">Wallet Address</span>
+      <input
+        type="text"
+        class="grow"
+        placeholder="Enter wallet address"
+        :value="user.address"
+        readonly
+      />
+    </label>
+  </div>
+  <div class="modal-action justify-center">
+    <button class="btn btn-primary" @click="emits('submitEditUser')">Save</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const user = defineModel({
+  default: {
+    name: '',
+    address: ''
+  }
+})
+const emits = defineEmits(['submitEditUser'])
+</script>
+
+<style scoped></style>
