fd8b2ccda851bda50882876de91418eac853485a
Dasarath G <dasarathg68@gmail.com>
2024-06-20 00:16:50 +0530

    feat: tests for add member and team

---
+++ app/src/components/__tests__/AddTeamCard.spec.ts
@@ -0,0 +1,28 @@
+import { it, describe, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import AddTeamCard from '@/components/AddTeamCard.vue'
+import IconPlus from '@/components/icons/IconPlus.vue'
+
+describe('AddTeamCard.vue', () => {
+  const wrapper = mount(AddTeamCard, {
+    global: {
+      components: {
+        IconPlus
+      }
+    }
+  })
+  describe('Render', () => {
+    it('renders correctly', () => {
+      expect(wrapper.find('h1').text()).toBe('Add Team')
+    })
+    it('renders icon plus', () => {
+      expect(wrapper.findComponent(IconPlus).exists()).toBe(true)
+    })
+  })
+  describe('Emits', () => {
+    it('emits addTeamCard when add team card is clicked', async () => {
+      await wrapper.findComponent(IconPlus).trigger('click')
+      expect(wrapper.emitted()).toHaveProperty('openAddTeamModal')
+    })
+  })
+})
