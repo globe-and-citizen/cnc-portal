cdee8324b118c39b86c76b756532473dd76758f4
farrellh1 <farrell.hauzan@gmail.com>
2024-06-23 00:06:50 +0700

    feat: create TabNavigation component

---
+++ app/src/components/TabNavigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div role="tablist" class="tabs tabs-boxed">
+    <a
+      v-for="tab in tabs"
+      :key="tab"
+      role="tab"
+      class="tab bg-white border shadow-sm !rounded-none"
+      :class="{ 'tab-active': activeTab === tab }"
+      @click="$emit('setTab', tab)"
+      >{{ tab }}</a
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+defineEmits(['setTab'])
+defineProps<{
+  activeTab: string
+  tabs: Array<any>
+}>()
+</script>
