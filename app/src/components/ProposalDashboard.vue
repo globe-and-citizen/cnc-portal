bc28abf6f7290901497c69d696a52f1af4ef4ccf
Dasarath G <dasarathg68@gmail.com>
2024-06-28 23:49:14 +0530

    feat: implement ongoing and done tabs in proposals

--- app/src/components/ProposalDashboard.vue
+++ app/src/components/ProposalDashboard.vue
@@ -6,13 +6,26 @@
         Create Proposal
       </button>
     </div>
+    <TabNavigation :initial-active-tab="0" :tabs="tabs" class="w-full">
+      <template #tab-0>
+        <ProposalCard
+          v-for="proposal in activeProposals"
+          :proposal="proposal"
+          class="mt-10"
+          :key="proposal.title"
+        />
+      </template>
+      <template #tab-1>
+        <ProposalCard
+          v-for="proposal in oldProposals"
+          :proposal="proposal"
+          :isDone="true"
+          class="mt-10"
+          :key="proposal.title"
+        />
+      </template>
+    </TabNavigation>
 
-    <ProposalCard
-      v-for="proposal in proposals"
-      :proposal="proposal"
-      class="mt-10"
-      :key="proposal.title"
-    />
     <ModalComponent v-model="showModal">
       <CreateProposalForm />
     </ModalComponent>
@@ -24,8 +37,24 @@ import type { Proposal } from '@/types/index'
 import { ref } from 'vue'
 import ModalComponent from './ModalComponent.vue'
 import CreateProposalForm from './forms/CreateProposalForm.vue'
+import TabNavigation from './TabNavigation.vue'
+import { ProposalTabs } from '@/types/index'
 const showModal = ref(false)
-const proposals = ref<Partial<Proposal>[]>([
+const tabs = ref([ProposalTabs.Ongoing, ProposalTabs.Done])
+const oldProposals = ref<Partial<Proposal>[]>([
+  {
+    title: 'Remote work: Yay or Nay?',
+    description: 'What is the best way to work? Remote or Office?',
+    draftedBy: 'Dasarath',
+    votes: {
+      yes: 10,
+      no: 2,
+      abstain: 1
+    },
+    isElection: false
+  }
+])
+const activeProposals = ref<Partial<Proposal>[]>([
   {
     title: 'Board of Directors Election this June',
     description:
