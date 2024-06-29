57db4d9b5b4716e4b6c0b421de1d1565aaa73983
farrellh1 <farrell.hauzan@gmail.com>
2024-06-29 19:02:43 +0700

    test: add accordion test

--- app/src/components/AccordionComponent.vue
+++ app/src/components/AccordionComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <template v-for="(accordion, index) in accordions" :key="index">
     <div class="collapse collapse-arrow bg-base-200">
-      <input type="radio" name="my-accordion-2" :checked="activeAccordion == index" />
+      <input type="radio" :name="`accordion-${index}`" :checked="activeAccordion == index" />
       <div class="collapse-title text-xl font-medium">{{ accordion }}</div>
       <div class="collapse-content">
         <slot :name="`accordion-${index}`"></slot>
