bf9998cddc47d24e844d5de37aa1ea5d1a59ad09
Dasarath G <dasarathg68@gmail.com>
2024-07-01 11:53:14 +0530

    feat: define events

--- contract/contracts/Voting.sol
+++ contract/contracts/Voting.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+
 import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol';
@@ -10,30 +11,28 @@ contract Voting is OwnableUpgradeable, PausableUpgradeable, ReentrancyGuardUpgra
     mapping(address => Types.Member) public members;
     address[] public memberAddresses;
 
-    mapping(uint256 id => Types.Proposal proposal) public proposals;
+    mapping(uint256 => Types.Proposal) public proposals;
     uint256 public proposalCount;
 
-   function initialize(Types.MemberInput[] memory inputs) public initializer {
-    __Ownable_init(msg.sender);
+    event MemberAdded(address indexed memberAddress, string name, uint256 teamId);
+    event ProposalAdded(uint256 indexed proposalId, string title, string description);
+    event Voted(address indexed voter, uint256 indexed proposalId, uint256 vote);
+    event ElectionVoted(address indexed voter, uint256 indexed proposalId, uint256 indexed candidateId);
 
-    for (uint256 i = 0; i < inputs.length; i++) {
-        members[inputs[i].memberAddress] = Types.Member({
-            name: inputs[i].name,
-            teamId: inputs[i].teamId,
-            isEligible: true,
-            isVoted: false
-        });
-        memberAddresses.push(inputs[i].memberAddress);
+    function initialize(Types.MemberInput[] memory inputs) public initializer {
+        __Ownable_init(msg.sender);
+        addMembers(inputs);
     }
-    }
-     function getElectoralRoll() public view returns (Types.Member[] memory) {
+
+    function getElectoralRoll() public view returns (Types.Member[] memory) {
         Types.Member[] memory _members = new Types.Member[](memberAddresses.length);
         for (uint256 i = 0; i < memberAddresses.length; i++) {
             _members[i] = members[memberAddresses[i]];
         }
         return _members;
     }
-    function addMembers(Types.MemberInput[] memory inputs) public onlyOwner {
+
+    function addMembers(Types.MemberInput[] memory inputs) public onlyOwner whenNotPaused  {
         for (uint256 i = 0; i < inputs.length; i++) {
             members[inputs[i].memberAddress] = Types.Member({
                 name: inputs[i].name,
@@ -42,12 +41,16 @@ contract Voting is OwnableUpgradeable, PausableUpgradeable, ReentrancyGuardUpgra
                 isVoted: false
             });
             memberAddresses.push(inputs[i].memberAddress);
+            emit MemberAdded(inputs[i].memberAddress, inputs[i].name, inputs[i].teamId);
         }
     }
-    function addProposal(Types.Proposal memory _proposal) public onlyOwner {
+
+    function addProposal(Types.Proposal memory _proposal) public onlyOwner whenNotPaused {
         proposals[proposalCount] = _proposal;
+        emit ProposalAdded(proposalCount,_proposal.title, _proposal.description);
         proposalCount++;
     }
+
     function getProposals() public view returns (Types.Proposal[] memory) {
         Types.Proposal[] memory _proposals = new Types.Proposal[](proposalCount);
         for (uint256 i = 0; i < proposalCount; i++) {
@@ -55,7 +58,8 @@ contract Voting is OwnableUpgradeable, PausableUpgradeable, ReentrancyGuardUpgra
         }
         return _proposals;
     }
-    function voteProposal(uint256 proposalId, uint256 vote) public {
+
+    function voteProposal(uint256 proposalId, uint256 vote) public whenNotPaused {
         require(members[msg.sender].isEligible, "You are not eligible to vote");
         require(!members[msg.sender].isVoted, "You have already voted");
         require(proposalId < proposalCount, "Proposal does not exist");
@@ -71,14 +75,25 @@ contract Voting is OwnableUpgradeable, PausableUpgradeable, ReentrancyGuardUpgra
         }
 
         members[msg.sender].isVoted = true;
+        emit Voted(msg.sender, proposalId, vote);
     }
-    function voteElection(uint256 proposalId, uint256 candidateId) public {
+
+    function voteElection(uint256 proposalId, uint256 candidateId) public whenNotPaused {
         require(members[msg.sender].isEligible, "You are not eligible to vote");
         require(!members[msg.sender].isVoted, "You have already voted");
-        
+        require(proposalId < proposalCount, "Proposal does not exist");
+
         proposals[proposalId].candidates[candidateId].votes++;
         members[msg.sender].isVoted = true;
+
+        emit ElectionVoted(msg.sender, proposalId, candidateId);
     }
 
+    function pause() external onlyOwner {
+        _pause();
+    }
 
+    function unpause() external onlyOwner {
+        _unpause();
+    }
 }
