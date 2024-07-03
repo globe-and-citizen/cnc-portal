c03f524fc2cecf7d25a25f2c2ee3e83abccc7eeb
Dasarath G <dasarathg68@gmail.com>
2024-07-03 14:49:25 +0530

    feat: add voting composables

--- app/src/composables/voting.ts
+++ app/src/composables/voting.ts
@@ -65,3 +65,67 @@ export function useGetProposals() {
 
   return { execute: getProposals, isLoading: loading, error, data: proposals }
 }
+export function useConcludeProposal() {
+  const transaction = ref<any>(null)
+  const loading = ref(false)
+  const error = ref<any>(null)
+  const isSuccess = ref(false)
+
+  async function concludeProposal(votingAddress: string, proposalId: number) {
+    try {
+      loading.value = true
+      transaction.value = await votingService.concludeProposal(votingAddress, proposalId)
+      isSuccess.value = true
+    } catch (err) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return { execute: concludeProposal, isLoading: loading, isSuccess, error, transaction }
+}
+export function useVoteDirective() {
+  const transaction = ref<any>(null)
+  const loading = ref(false)
+  const error = ref<any>(null)
+  const isSuccess = ref(false)
+
+  async function voteDirective(votingAddress: string, proposalId: number, directive: number) {
+    try {
+      loading.value = true
+      transaction.value = await votingService.voteDirective(votingAddress, proposalId, directive)
+      isSuccess.value = true
+    } catch (err) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return { execute: voteDirective, isLoading: loading, isSuccess, error, transaction }
+}
+export function useVoteElection() {
+  const transaction = ref<any>(null)
+  const loading = ref(false)
+  const error = ref<any>(null)
+  const isSuccess = ref(false)
+
+  async function voteElection(votingAddress: string, electionId: number, candidateAddress: string) {
+    try {
+      loading.value = true
+      transaction.value = await votingService.voteElection(
+        votingAddress,
+        electionId,
+        candidateAddress
+      )
+      isSuccess.value = true
+    } catch (err) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return { execute: voteElection, isLoading: loading, isSuccess, error, transaction }
+}
