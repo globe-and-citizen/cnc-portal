dfb428aecc5d8d17bb3b8f721c84665e4b7a528f
farrellh1 <farrell.hauzan@gmail.com>
2024-07-17 22:51:11 +0700

    test; add test edit user form

--- app/src/components/forms/EditUserForm.vue
+++ app/src/components/forms/EditUserForm.vue
@@ -1,27 +1,52 @@
 <template>
   <div class="flex flex-col gap-5 mt-4">
     <label class="input input-bordered flex items-center gap-2 input-md">
-      <span class="w-24">Name</span>
-      <input type="text" class="grow" placeholder="John Doe" v-model="user.name" />
+      <span class="w-24" data-test="name-label">Name</span>
+      <input
+        type="text"
+        class="grow"
+        data-test="name-input"
+        placeholder="John Doe"
+        v-model="user.name"
+      />
     </label>
     <label class="input input-bordered flex items-center gap-2 input-md input-disabled">
-      <span class="w-24 text-xs">Wallet Address</span>
-      <ToolTip content="Click to see address in block explorer">
-        <div type="text" class="w-full cursor-pointer" @click="openExplorer(user.address)" readonly>
+      <span class="w-24 text-xs" data-test="address-label">Wallet Address</span>
+      <ToolTip data-test="address-tooltip" content="Click to see address in block explorer">
+        <div
+          type="text"
+          class="w-full cursor-pointer"
+          @click="openExplorer(user.address)"
+          data-test="user-address"
+          readonly
+        >
           {{ user.address }}
         </div>
       </ToolTip>
-      <ClipboardDocumentListIcon
-        v-if="isSupported && !copied"
-        class="size-5 cursor-pointer"
-        @click="copy(user.address)"
-      />
-      <ClipboardDocumentCheckIcon class="size-5" v-if="copied" />
+      <ToolTip
+        data-test="copy-address-tooltip"
+        :content="copied ? 'Copied!' : 'Click to copy address'"
+      >
+        <ClipboardDocumentListIcon
+          v-if="isSupported && !copied"
+          data-test="copy-address-icon"
+          class="size-5 cursor-pointer"
+          @click="copy(user.address)"
+        />
+        <ClipboardDocumentCheckIcon class="size-5" v-if="copied" />
+      </ToolTip>
     </label>
   </div>
   <div class="modal-action justify-center">
     <LoadingButton v-if="isLoading" color="primary min-w-24" />
-    <button v-else class="btn btn-primary" @click="emits('submitEditUser')">Save</button>
+    <button
+      v-else
+      class="btn btn-primary"
+      data-test="submit-edit-user"
+      @click="emits('submitEditUser')"
+    >
+      Save
+    </button>
   </div>
 </template>
 
---
+++ app/src/components/forms/__tests__/EditUserForm.spec.ts
@@ -0,0 +1,96 @@
+import { mount, type VueWrapper } from '@vue/test-utils'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import EditUserForm from '@/components/forms/EditUserForm.vue'
+import { ClipboardDocumentListIcon, ClipboardDocumentCheckIcon } from '@heroicons/vue/24/outline'
+import { ref } from 'vue'
+import { NETWORK } from '@/constant'
+
+const mockCopy = vi.fn()
+const mockClipboard = {
+  copy: mockCopy,
+  copied: ref(false),
+  isSupported: ref(true)
+}
+vi.mock('@vueuse/core', () => ({
+  useClipboard: vi.fn(() => mockClipboard)
+}))
+
+describe('EditUserForm', () => {
+  let wrapper: VueWrapper
+  const user = {
+    name: 'John Doe',
+    address: '0x4b6Bf5cD91446408290725879F5666dcd9785F62'
+  }
+
+  beforeEach(() => {
+    wrapper = mount(EditUserForm, {
+      props: {
+        isLoading: false,
+        modelValue: user
+      }
+    })
+  })
+
+  describe('Render', () => {
+    it('renders label and input for name correctly', () => {
+      expect(wrapper.find('span[data-test="name-label"]').text()).toBe('Name')
+      expect(wrapper.find('input[data-test="name-input"]').exists()).toBeTruthy()
+    })
+
+    it('renders label and address with tooltip correctly', () => {
+      expect(wrapper.find('span[data-test="address-label"]').text()).toBe('Wallet Address')
+      expect(wrapper.find('div[data-test="user-address"]').text()).toBe(user.address)
+
+      // Tooltip
+      const addressTooltip = wrapper.find('[data-test="address-tooltip"]').findComponent({
+        name: 'ToolTip'
+      })
+      expect(addressTooltip.props().content).toBe('Click to see address in block explorer')
+    })
+
+    it('renders copy address icon correctly', () => {
+      expect(wrapper.findComponent(ClipboardDocumentListIcon).exists()).toBeTruthy()
+
+      // Tooltip
+      const copyIconTooltip = wrapper.find('[data-test="copy-address-tooltip"]').findComponent({
+        name: 'ToolTip'
+      })
+      expect(copyIconTooltip.props().content).toBe('Click to copy address')
+    })
+
+    it('renders submit button correctly', () => {
+      expect(wrapper.find('button[data-test="submit-edit-user"]').text()).toBe('Save')
+    })
+
+    it('renders loading button if isLoading true', async () => {
+      await wrapper.setProps({ isLoading: true })
+      expect(wrapper.findComponent({ name: 'LoadingButton' }).exists()).toBeTruthy()
+      expect(wrapper.find('button[data-test="submit-edit-user"]').exists()).toBeFalsy()
+    })
+  })
+
+  describe('Emits', () => {
+    it('emits submitEditUser when submit button is clicked', async () => {
+      await wrapper.find('button[data-test="submit-edit-user"]').trigger('click')
+      expect(wrapper.emitted('submitEditUser')).toBeTruthy()
+    })
+
+    it('opens new tab when address is clicked', async () => {
+      // mock window.open
+      window.open = vi.fn()
+
+      await wrapper.find('[data-test="user-address"]').trigger('click')
+
+      expect(window.open).toBeCalledWith(
+        `${NETWORK.blockExplorerUrl}/address/${user.address}`,
+        '_blank'
+      )
+    })
+
+    it('copies address when copy icon is clicked', async () => {
+      await wrapper.findComponent(ClipboardDocumentListIcon).trigger('click')
+
+      expect(mockCopy).toBeCalledWith(user.address)
+    })
+  })
+})
