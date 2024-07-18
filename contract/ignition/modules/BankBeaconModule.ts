62266b9c1fd8aa29f05b44a2c357f83be155a3a0
farrellh1 <farrell.hauzan@gmail.com>
2024-07-18 23:13:04 +0700

    fix: adjust contract in bank module

--- contract/ignition/modules/BankBeaconModule.ts
+++ contract/ignition/modules/BankBeaconModule.ts
@@ -2,7 +2,7 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
 
 const bankBeaconModule = buildModule('BankBeaconModule', (m) => {
   const beaconAdmin = m.getAccount(0)
-  const bankImplementation = m.contract('BankV2')
+  const bankImplementation = m.contract('Bank')
   const beacon = m.contract('Beacon', [bankImplementation], {
     from: beaconAdmin
   })
