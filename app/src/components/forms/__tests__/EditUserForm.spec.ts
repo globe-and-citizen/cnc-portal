b7e1a676ed80852e4c452e18fce8330ae130d261
farrellh1 <farrell.hauzan@gmail.com>
2024-07-19 23:50:44 +0700

    refactor: tests

--- app/src/components/forms/__tests__/EditUserForm.spec.ts
+++ app/src/components/forms/__tests__/EditUserForm.spec.ts
@@ -1,5 +1,5 @@
-import { mount, type VueWrapper } from '@vue/test-utils'
-import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { describe, expect, it, vi } from 'vitest'
 import EditUserForm from '@/components/forms/EditUserForm.vue'
 import { ClipboardDocumentListIcon, ClipboardDocumentCheckIcon } from '@heroicons/vue/24/outline'
 import { ref } from 'vue'
@@ -16,31 +16,31 @@ vi.mock('@vueuse/core', () => ({
 }))
 
 describe('EditUserForm', () => {
-  let wrapper: VueWrapper
   const user = {
     name: 'John Doe',
     address: '0x4b6Bf5cD91446408290725879F5666dcd9785F62'
   }
 
-  beforeEach(() => {
-    wrapper = mount(EditUserForm, {
+  const createComponent = (props?: any) => {
+    return mount(EditUserForm, {
       props: {
         isLoading: false,
-        modelValue: user
+        modelValue: user,
+        ...props
       }
     })
-  })
+  }
 
   describe('Render', () => {
     it('renders label and input for name correctly', () => {
+      const wrapper = createComponent()
       expect(wrapper.find('span[data-test="name-label"]').text()).toBe('Name')
       expect(wrapper.find('input[data-test="name-input"]').exists()).toBeTruthy()
-      expect(
-        (wrapper.find('input[data-test="name-input"]').element as HTMLInputElement).value
-      ).toBe(user.name)
+      expect(wrapper.props().modelValue.name).toBe(user.name)
     })
 
     it('renders label and address with tooltip correctly', () => {
+      const wrapper = createComponent()
       expect(wrapper.find('span[data-test="address-label"]').text()).toBe('Wallet Address')
       expect(wrapper.find('div[data-test="user-address"]').text()).toBe(user.address)
 
@@ -52,6 +52,7 @@ describe('EditUserForm', () => {
     })
 
     it('renders copy address icon correctly', () => {
+      const wrapper = createComponent()
       expect(wrapper.findComponent(ClipboardDocumentListIcon).exists()).toBeTruthy()
 
       // Tooltip
@@ -62,17 +63,19 @@ describe('EditUserForm', () => {
     })
 
     it('renders copied icon when copied', async () => {
+      const wrapper = createComponent()
       mockClipboard.copied.value = true
       await wrapper.vm.$nextTick()
       expect(wrapper.findComponent(ClipboardDocumentCheckIcon).exists()).toBeTruthy()
     })
 
     it('renders submit button correctly', () => {
+      const wrapper = createComponent()
       expect(wrapper.find('button[data-test="submit-edit-user"]').text()).toBe('Save')
     })
 
     it('renders loading button if isLoading true', async () => {
-      await wrapper.setProps({ isLoading: true })
+      const wrapper = createComponent({ isLoading: true })
       expect(wrapper.findComponent({ name: 'LoadingButton' }).exists()).toBeTruthy()
       expect(wrapper.find('button[data-test="submit-edit-user"]').exists()).toBeFalsy()
     })
@@ -80,11 +83,14 @@ describe('EditUserForm', () => {
 
   describe('Emits', () => {
     it('emits submitEditUser when submit button is clicked', async () => {
+      const wrapper = createComponent()
       await wrapper.find('button[data-test="submit-edit-user"]').trigger('click')
       expect(wrapper.emitted('submitEditUser')).toBeTruthy()
     })
 
     it('opens new tab when address is clicked', async () => {
+      const wrapper = createComponent()
+
       // mock window.open
       window.open = vi.fn()
 
@@ -97,6 +103,8 @@ describe('EditUserForm', () => {
     })
 
     it('copies address when copy icon is clicked', async () => {
+      const wrapper = createComponent()
+
       // mock clipboard
       mockClipboard.isSupported.value = true
       mockClipboard.copied.value = false
