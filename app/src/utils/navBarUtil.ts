5a1627c849a76dd631670059d88319763830e0db
Hermann EHO <hermannleboss@gmail.com>
2024-07-21 12:34:21 +0000

    refactor: remove navbar util

--- app/src/utils/navBarUtil.ts
+++
@@ -1,9 +0,0 @@
-import { AuthService } from '@/services/authService'
-import { useUserDataStore } from '@/stores/user'
-
-export const logout = () => {
-  AuthService.logout()
-  useUserDataStore().clearUserData()
-  useUserDataStore().setAuthStatus(false)
-  window.location.reload()
-}
