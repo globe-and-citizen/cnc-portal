37e6282c47469e57bf0b0c0f14b07fff0361b909
Hermann EHO <hermannleboss@gmail.com>
2024-07-22 21:47:14 +0000

    feat: reach 94% of use custom fetch

--- app/src/composables/__tests__/useCustomFetch.spec.ts
+++ app/src/composables/__tests__/useCustomFetch.spec.ts
@@ -1,5 +1,6 @@
 import { useCustomFetch } from '@/composables/useCustomFetch'
-import { describe, expect, it, vi } from 'vitest'
+import { createPinia, setActivePinia } from 'pinia'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 // const createMockResponse = (data: any): Response => {
 //   const blob = new Blob([JSON.stringify(data)], { type: 'application/json' })
 //   const init: ResponseInit = { status: 200, statusText: 'OK', headers: { 'Content-Type': 'application/json' } }
@@ -27,15 +28,15 @@ const createMockResponse = (data: any): Response => {
 
 const createMock401Response = (data: any): Response => {
   return {
-    ok: true,
+    ok: false,
     status: 401,
     json: () => Promise.resolve(data),
     headers: new Headers({ 'Content-Type': 'application/json' }),
     redirected: false,
-    statusText: 'OK',
+    statusText: 'Unauthorized',
     type: 'basic',
     url: '',
-    clone: () => createMockResponse(data),
+    clone: () => createMock401Response(data),
     body: null,
     bodyUsed: false,
     arrayBuffer: async () => new ArrayBuffer(0),
@@ -53,9 +54,31 @@ export async function fetchData(url: string): Promise<any> {
 }
 
 describe('useCustomFetch', () => {
+  const originalLocation = window.location
+
+  beforeEach(() => {
+    // Mock the window.location object
+    const mockedLocation = {
+      ...originalLocation,
+      reload: vi.fn()
+    }
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: mockedLocation
+    })
+  })
+
+  afterEach(() => {
+    // Restore the original window.location
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: originalLocation
+    })
+  })
   it('Should return the correct data', async () => {
     const mockData = { data: 'mocked data' }
 
+    setActivePinia(createPinia())
     // Mocking the fetch function
     global.fetch = vi.fn(() => Promise.resolve(createMockResponse(mockData)))
 
@@ -84,16 +107,15 @@ describe('useCustomFetch', () => {
     // Mocking the fetch function
     global.fetch = vi.fn(() => Promise.resolve(createMock401Response(mockData)))
 
+    // Mock widows.location.reload
+    window.location.reload = vi.fn()
+
     const { error, data, response } = await useCustomFetch<string>('https://google.com/').json()
     console.log({ data: data.value, error: error.value, response: response.value })
     // expect(global.fetch).toHaveBeenCalledWith('https://google.com/')
-    expect(data.value).toMatchInlineSnapshot(`
-      {
-        "data": "Error",
-      }
-    `)
-    // expect(data.value).toEqual(mockData)
-    console.log('ds', { error: error.value, data: data.value })
+    expect(data.value).toMatchInlineSnapshot(`null`)
+    expect(error.value).toMatchInlineSnapshot(`[Error: Unauthorized]`)
+    // console.log('ds', { error: error.value, data: datua.value })
 
     expect(true).toBe(true)
   })
