7689fc0b64eddda30dc5fe50060a84981acd8e7f
aliassan <alimkwananzi@gmail.com>
2024-07-30 16:08:56 +0200

    feat: add form error message component

---
+++ app/src/components/FormInputError.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="relative p-2 mb-4 text-sm text-red-500 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 border border-red-200" 
+    role="alert"
+  >
+    <slot></slot>
+  </div>
+</template>
