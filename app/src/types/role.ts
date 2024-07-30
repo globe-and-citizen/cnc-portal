94fdfd93a43eecc05e6795f94586ef5564720d96
aliassan <alimkwananzi@gmail.com>
2024-07-30 16:07:39 +0200

    feat: add role types

---
+++ app/src/types/role.ts
@@ -0,0 +1,25 @@
+export interface RoleCategory {
+  id?: number
+  name: string
+  description?: string
+  entitlements?: Entitlement[]
+  roles?: Role[]
+}
+
+export interface Role {
+  id?: number
+  name: string
+  description?: string
+  entitlements?: Entitlement[]
+}
+
+export interface Entitlement {
+  id?: number
+  value: string
+  entitlementTypeId: number
+}
+
+export interface EntitlementType {
+  id: number
+  name: string
+}
