66693d83950ba154978d40ace0d9fec16d652c64
farrellh1 <farrell.hauzan@gmail.com>
2024-08-17 21:52:49 +0700

    chore: add validation if env not valid and not defined

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,25 +1,42 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import inject from '@rollup/plugin-inject'
+import { ENV_LIST } from './env-list'
+import { isAddress } from 'ethers'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    inject({
-      Buffer: ['buffer', 'Buffer']
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      buffer: 'buffer/'
+export default defineConfig(({ mode }) => {
+  // Validate .env file
+  const env = loadEnv(mode, process.cwd(), '')
+
+  ENV_LIST.forEach((key) => {
+    if (!env[key]) {
+      throw new Error(`Missing ${key} in .env file`)
+    } else if (key.includes('ADDRESS') && !isAddress(env[key])) {
+      throw new Error(`Invalid address in ${key}`)
+    } else if (key.includes('URL') && !env[key].startsWith('http')) {
+      throw new Error(`Invalid URL in ${key}`)
     }
-  },
-  server: {
-    host: true,
-    port: 5173
+  })
+
+  return {
+    plugins: [
+      vue(),
+      inject({
+        Buffer: ['buffer', 'Buffer']
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        buffer: 'buffer/'
+      }
+    },
+    server: {
+      host: true,
+      port: 5173
+    }
   }
 })
