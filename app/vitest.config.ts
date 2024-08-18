092b7d12dbacaa1e3fdf2e675ba1d40258b4d090
farrellh1 <farrell.hauzan@gmail.com>
2024-08-18 14:55:04 +0700

    fix: vitest config

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -7,9 +7,9 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
+export default defineConfig((env) => mergeConfig(
+  viteConfig(env),
+  {
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'test/e2e/*'],
@@ -21,5 +21,5 @@ export default mergeConfig(
           : false
       }
     }
-  })
-)
+  }
+))
