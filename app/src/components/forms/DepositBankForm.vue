98cdb4548419f6b0eda337d150f485de66f76826
Dasarath G <dasarathg68@gmail.com>
2024-08-19 12:13:06 +0530

    feat: validation for deposit form

--- app/src/components/forms/DepositBankForm.vue
+++ app/src/components/forms/DepositBankForm.vue
@@ -5,12 +5,13 @@
   </h3>
 
   <input type="text" class="input input-bordered w-full mt-8" v-model="amount" />
+  <div class="pl-4 text-red-500 text-sm" v-for="error of $v.amount.$errors" :key="error.$uid">
+    {{ error.$message }}
+  </div>
 
   <div class="modal-action justify-center">
     <LoadingButton color="primary" class="w-24" v-if="loading" />
-    <button class="btn btn-primary" @click="$emit('deposit', amount)" v-if="!loading">
-      Deposit
-    </button>
+    <button class="btn btn-primary" @click="submitForm" v-if="!loading">Deposit</button>
     <button class="btn btn-error" @click="$emit('closeModal')">Cancel</button>
   </div>
 </template>
@@ -19,10 +20,29 @@
 import LoadingButton from '@/components/LoadingButton.vue'
 import { NETWORK } from '@/constant'
 import { ref } from 'vue'
+import { required, numeric } from '@vuelidate/validators'
+import { useVuelidate } from '@vuelidate/core'
 
 const amount = ref<string>('0')
-defineEmits(['deposit', 'closeModal'])
+const emits = defineEmits(['deposit', 'closeModal'])
 defineProps<{
   loading: boolean
 }>()
+
+const rules = {
+  amount: {
+    required,
+    numeric
+  }
+}
+
+const $v = useVuelidate(rules, { amount })
+
+const submitForm = () => {
+  $v.value.$touch()
+  if ($v.value.$invalid) {
+    return
+  }
+  emits('deposit', amount.value)
+}
 </script>
