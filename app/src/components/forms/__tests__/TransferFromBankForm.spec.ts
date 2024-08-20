b8b5a9bf5a774cc94e98697925be2800bed3d7f1
Dasarath G <dasarathg68@gmail.com>
2024-08-20 15:34:45 +0530

    tests: form validation for transfer bank

--- app/src/components/forms/__tests__/TransferFromBankForm.spec.ts
+++ app/src/components/forms/__tests__/TransferFromBankForm.spec.ts
@@ -57,4 +57,61 @@ describe('TransferFromBankForm.vue', () => {
       expect(wrapper.emitted('closeModal')).toBeTruthy()
     })
   })
+  describe('form validation', () => {
+    it('shows error when amount is 0', async () => {
+      const wrapper = mount(TransferFromBankForm, {
+        props: {
+          loading: false,
+          bankBalance: '100',
+          filteredMembers: [
+            { id: '1', name: 'John Doe', address: '0xAddress1' },
+            { id: '2', name: 'Jane Smith', address: '0xAddress2' }
+          ]
+        }
+      })
+      const recipientInput = wrapper.find('input[data-test="recipient-input"]')
+      await recipientInput.setValue('0xaFeF48F7718c51fb7C6d1B314B3991D2e1d8421E')
+      const amountInput = wrapper.find('input[data-test="amount-input"]')
+      await amountInput.setValue('0')
+      await wrapper.find('.btn-primary').trigger('click')
+      await wrapper.vm.$nextTick()
+      console.log(wrapper.find('.text-red-500').html())
+
+      expect(wrapper.find('.text-red-500').exists()).toBe(true)
+    })
+    it('shows error when amount is empty', async () => {
+      const wrapper = mount(TransferFromBankForm, {
+        props: {
+          loading: false,
+          bankBalance: '100',
+          filteredMembers: [
+            { id: '1', name: 'John Doe', address: '0xAddress1' },
+            { id: '2', name: 'Jane Smith', address: '0xAddress2' }
+          ]
+        }
+      })
+
+      const amountInput = wrapper.find('input[data-test="amount-input"]')
+      await amountInput.setValue('')
+      await wrapper.find('.btn-primary').trigger('click')
+      expect(wrapper.find('.text-red-500').exists()).toBe(true)
+    })
+    it('shows error when amount is not numeric', async () => {
+      const wrapper = mount(TransferFromBankForm, {
+        props: {
+          loading: false,
+          bankBalance: '100',
+          filteredMembers: [
+            { id: '1', name: 'John Doe', address: '0xAddress1' },
+            { id: '2', name: 'Jane Smith', address: '0xAddress2' }
+          ]
+        }
+      })
+
+      const amountInput = wrapper.find('input[data-test="amount-input"]')
+      await amountInput.setValue('sdkjnvc')
+      await wrapper.find('.btn-primary').trigger('click')
+      expect(wrapper.find('.text-red-500').exists()).toBe(true)
+    })
+  })
 })
