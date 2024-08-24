c3b413b76a59ec6d651ebc7f7d1c8924a069d08e
Hermann EHO <hermannleboss@gmail.com>
2024-08-24 19:21:59 +0000

    feat: bank beacon usign tips address for tipsModule

--- contract/ignition/modules/BankBeaconModule.ts
+++ contract/ignition/modules/BankBeaconModule.ts
@@ -1,11 +1,12 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
-
+import tipsModule from './ProxyModule'
 const bankBeaconModule = buildModule('BankBeaconModule', (m) => {
+  const { tips } = m.useModule(tipsModule)
   const beaconAdmin = m.getAccount(0)
   const bankImplementation = m.contract('Bank')
 
   // Call the implementation function initialize by passing the address of the tips
-  m.call(bankImplementation, 'initialize', ['0x79EF3aE86725D7D0Ae4eF3A31758A7c7fe40e5B4'])
+  m.call(bankImplementation, 'initialize', [tips.address])
   const beacon = m.contract('Beacon', [bankImplementation], {
     from: beaconAdmin
   })
