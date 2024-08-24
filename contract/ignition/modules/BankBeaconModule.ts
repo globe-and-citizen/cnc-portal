4d63d486696e9271ac1b6f55113e05d77e8b17a5
Hermann EHO <hermannleboss@gmail.com>
2024-08-24 16:09:23 +0000

    feat: initialize bank implementation in deploy time

--- contract/ignition/modules/BankBeaconModule.ts
+++ contract/ignition/modules/BankBeaconModule.ts
@@ -3,6 +3,9 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
 const bankBeaconModule = buildModule('BankBeaconModule', (m) => {
   const beaconAdmin = m.getAccount(0)
   const bankImplementation = m.contract('Bank')
+
+  // Call the implementation function initialize by passing the address of the tips
+  m.call(bankImplementation, 'initialize', ['0x79EF3aE86725D7D0Ae4eF3A31758A7c7fe40e5B4'])
   const beacon = m.contract('Beacon', [bankImplementation], {
     from: beaconAdmin
   })
