14203cf5d17b6a0f15ca30932ac7477068a0a2bc
aliassan <alimkwananzi@gmail.com>
2024-08-27 12:35:15 +0200

    refactor: simplify contract

--- contract/contracts/expense-account/ExpenseAccount.sol
+++ contract/contracts/expense-account/ExpenseAccount.sol
@@ -4,98 +4,46 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
 
 contract ExpenseAccount is 
     OwnableUpgradeable, 
     ReentrancyGuardUpgradeable, 
-    EIP712Upgradeable, 
     PausableUpgradeable {
 
-    struct BudgetLimit {
-        address approvedAddress;
-        BudgetType budgetType;
-        uint256 value;
-        uint256 expiry;
-    }
+    uint256 maxLimit;
 
-    enum BudgetType {
-        TransactionsPerPeriod,
-        AmountPerPeriod,
-        AmountPerTransaction
-    }
-
-    bytes32 constant BUDGETLIMIT_TYPEHASH = 
-        keccak256(
-            "BudgetLimit(address approvedAddress,uint8 budgetType,uint256 value,uint256 expiry)"
-        );
-
-    struct Balance {
-        uint256 transactionCount;
-        uint256 amountWithdrawn;
-    }
-
-    mapping(bytes32 => Balance) balances;
+    mapping(address => bool) approvedAddresses;
 
     event NewDeposit(address indexed depositor, uint256 amount);
 
-    event NewWithdrawal(address indexed withdrawr, uint256 amount);
-
-    error UnauthorizedAccess(address expected, address received);
+    event NewWithdrawal(address indexed withdrawer, uint256 amount);
     
     function initialize() public initializer {
         __Ownable_init(msg.sender);
-        __EIP712_init("CNCExpenseAccount", "1");
-    }
-
-    function budgetLimitHash (BudgetLimit calldata limit) private pure returns( bytes32 ) {
-        return keccak256(abi.encode(
-            BUDGETLIMIT_TYPEHASH,
-            limit.approvedAddress,
-            limit.budgetType,
-            limit.value,
-            limit.expiry
-        ));
     }
 
     function withdraw(
-        uint256 amount, 
-        BudgetLimit calldata limit, 
-        uint8 v, 
-        bytes32 r, 
-        bytes32 s
-    ) external whenNotPaused {        
-        require(msg.sender == limit.approvedAddress, "Withdrawer not approved");
+        uint256 amount
+    ) external nonReentrant whenNotPaused {        
+        require(approvedAddresses[msg.sender], "Withdrawer not approved");
 
-        bytes32 digest = keccak256(abi.encodePacked(
-            "\x19\x01",
-            _domainSeparatorV4(),
-            budgetLimitHash(limit)
-        ));
+        require(amount <= maxLimit, "Max limit exceeded");
 
-        address signer = ecrecover(digest, v, r, s);
+        payable(msg.sender).transfer(amount);
 
-        //require(signer == owner(), signer);
+        emit NewWithdrawal(msg.sender, amount);
+    }
 
-        if (signer != owner()) {
-            revert UnauthorizedAccess(owner(), signer);
-        }
+    function setMaxLimit(uint256 amount) external onlyOwner whenNotPaused {
+        maxLimit = amount;
+    }
 
-        require((block.timestamp <= limit.expiry), "Authorization expired");
+    function approveAddress(address _address) external onlyOwner whenNotPaused{
+        approvedAddresses[_address] = true;
+    }
 
-        if (limit.budgetType == BudgetType.TransactionsPerPeriod) {
-            require(balances[digest].transactionCount <= limit.value, "Transaction limit reached");
-            balances[digest].transactionCount++;
-            payable(limit.approvedAddress).transfer(amount);
-        } else if (limit.budgetType == BudgetType.AmountPerPeriod) {
-            require(balances[digest].amountWithdrawn+amount <= limit.value, "Authorized amount exceeded");
-            payable(limit.approvedAddress).transfer(amount);
-        } else {
-            require(amount <= limit.value, "Authorized amount exceeded");
-            payable(limit.approvedAddress).transfer(amount);
-        }
-
-        emit NewWithdrawal(limit.approvedAddress, amount);
+    function disapproveAddress(address _address) external onlyOwner whenNotPaused{
+        approvedAddresses[_address] = false;
     }
 
     function pause() external onlyOwner {
