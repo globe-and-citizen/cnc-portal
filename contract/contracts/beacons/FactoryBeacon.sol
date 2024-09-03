cbe0aa544cb6ab8d3e8661765f25361293fc3406
aliassan <alimkwananzi@gmail.com>
2024-09-03 14:21:00 +0200

    chore: rename variable

--- contract/contracts/beacons/FactoryBeacon.sol
+++ contract/contracts/beacons/FactoryBeacon.sol
@@ -8,7 +8,7 @@ import "./UserBeaconProxy.sol";
 contract FactoryBeacon is UpgradeableBeacon {
   event BeaconProxyCreated(address indexed proxy, address indexed deployer);
 
-  constructor(address implementation) UpgradeableBeacon(implementation, msg.sender) {}
+  constructor(address implementationAddress) UpgradeableBeacon(implementationAddress, msg.sender) {}
 
   function createBeaconProxy(bytes memory data) external {
     UserBeaconProxy proxy = new UserBeaconProxy(address(this), data);
