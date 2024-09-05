075b9ea2087a430f4dc43571c2837b017cff754c
Dasarath G <dasarathg68@gmail.com>
2024-09-06 00:05:54 +0530

    feat: add polygon to config

--- app/src/wagmi.config.ts
+++ app/src/wagmi.config.ts
@@ -1,10 +1,11 @@
 import { http, createConfig } from '@wagmi/vue'
-import { mainnet, sepolia } from '@wagmi/vue/chains'
+import { mainnet, sepolia, polygon } from '@wagmi/vue/chains'
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
   transports: {
     [mainnet.id]: http(),
-    [sepolia.id]: http()
+    [sepolia.id]: http(),
+    [polygon.id]: http()
   }
 })
