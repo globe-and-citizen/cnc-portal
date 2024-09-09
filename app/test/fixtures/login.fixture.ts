e36c73ef04214ac5792ca9f519339006b0dae9e5
farrellh1 <farrell.hauzan@gmail.com>
2024-09-09 21:05:09 +0700

    fix: login fixture

--- app/test/fixtures/login.fixture.ts
+++ app/test/fixtures/login.fixture.ts
@@ -7,44 +7,24 @@ export const test = metaMaskFixtures(basicSetup).extend<{
   login: async ({ page, metamask }, use) => {
     await use(async () => {
       const address = await metamask.getAccountAddress()
-      await page.route('**/api/user/nonce/*', async (route) => {
-        await route.fulfill({
-          status: 200,
-          contentType: 'application/json',
-          body: JSON.stringify({ success: true, nonce: '41vj7bz5Ow8oT5xaE' })
-        })
-      })
-      await page.route('**/api/auth/siwe', async (route) => {
-        await route.fulfill({
-          status: 200,
-          contentType: 'application/json',
-          body: JSON.stringify({ success: true, accessToken: 'token' })
-        })
-      })
-      await page.route(`**/api/user/${address}`, async (route) => {
+      await page.route(`**/api/auth/token`, async (route) => {
         await route.fulfill({
           status: 200,
           contentType: 'application/json',
           body: JSON.stringify({
-            address,
-            name: null,
-            nonce: '41vj7bz5Ow8oT5xaE'
+            success: true
           })
         })
       })
-      await page.goto('http://localhost:5173/login')
-      await page.getByTestId('sign-in').click()
-      await page.waitForLoadState('networkidle')
-      await metamask.connectToDapp()
-      await page.waitForLoadState('networkidle')
-      await metamask.confirmSignature()
       await page.addInitScript(() => {
         window.localStorage.setItem('isAuth', 'true')
         window.localStorage.setItem('name', 'test')
         window.localStorage.setItem('ownerAddress', address)
         window.localStorage.setItem('nonce', '41vj7bz5Ow8oT5xaE')
+        window.localStorage.setItem('authToken', '1234567890')
       })
-      await page.waitForURL('http://localhost:5173/teams')
+      await page.reload()
+      await metamask.connectToDapp()
     })
   }
 })
