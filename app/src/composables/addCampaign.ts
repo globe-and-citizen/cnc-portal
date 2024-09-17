91ce1f22d456cc3acf3108aab77b4afb918d7392
achille <achillegley@gmail.com>
2024-09-17 20:28:33 +0700

    continue add tests

--- app/src/composables/addCampaign.ts
+++ app/src/composables/addCampaign.ts
@@ -1,11 +1,6 @@
 import { AddCampaignService} from '@/services/AddCampaignService'
 import type { BankEventType, EventResult } from '@/types'
-import type { IContractReadFunction, IContractTransactionFunction } from '@/types/interfaces'
-import dayjs from 'dayjs'
-import type { Log } from 'ethers'
-import type { EventLog } from 'ethers'
 import { ref } from 'vue'
-import { useCustomFetch } from '@/composables/useCustomFetch'
 
 
 const addCamapaignService = new AddCampaignService()
@@ -39,36 +34,3 @@ export function useDeployAddCampaignContract() {
 
 
 
-export function useAddCampaignEvents(addCampaignAddress: string) {
-  const events = ref<EventResult[]>([])
-  const loading = ref(false)
-  const error = ref<any>(null)
-
-  async function getEvents(type: BankEventType): Promise<void> {
-    try {
-      loading.value = true
-      const response = await addCamapaignService.getEvents(addCampaignAddress, "CampaignCreated")
-      events.value = await Promise.all(
-        response.map(async (eventData: EventLog | Log) => {
-          const date = dayjs((await eventData.getBlock()).date).format('DD/MM/YYYY HH:mm')
-
-          return {
-            txHash: eventData.transactionHash,
-            date: date,
-            data: (await addCamapaignService.getContract(addCampaignAddress)).interface.decodeEventLog(
-              type,
-              eventData.data,
-              eventData.topics
-            )
-          }
-        })
-      )
-    } catch (err) {
-      error.value = err
-    } finally {
-      loading.value = false
-    }
-  }
-
-  return { events, getEvents, loading, error }
-}
