2cc1250d5aacf132f16a80371ee695e2e304a2f4
Dasarath G <dasarathg68@gmail.com>
2024-09-20 15:31:29 +0530

    refactor: Update Officer contract tests and dependencies

--- contract/contracts/Officer.sol
+++ contract/contracts/Officer.sol
@@ -11,139 +11,92 @@ import '@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol
 
 import "hardhat/console.sol";
 
-// Example implementation contracts
 interface IBankAccount {
-    function initialize(address teamOwner) external;
+    function initialize(address tipsAddress) external;
 }
 
-interface IGovernanceContract {
-    function initialize(address teamOwner) external;
+interface IVotingContract {
+    function initialize() external;
 }
 
 contract Officer is OwnableUpgradeable, ReentrancyGuardUpgradeable, PausableUpgradeable  {
-    struct Team {
-        address[] founders;
-        address[] members;
-        address bankAccountContract;
-        address governanceContract;
-        address teamOwner;
-    }
-
-    mapping(uint256 => Team) public teams;
-    uint256 public teamCounter;
+    
+    address[] founders;
+    address[] members;
+    address bankAccountContract;
+    address votingContract;
 
     address public bankAccountBeacon;
     address public employeeContractBeacon;
-    address public governanceContractBeacon;
+    address public votingContractBeacon;
 
-    event TeamCreated(uint256 teamId, address[] founders, address[] members);
-    event AgreementSigned(uint256 teamId, address[] signatories);
-    event ContractDeployed(uint256 teamId, string contractType, address contractAddress);
-    event getTeamEvent(uint256 teamId, Team team);
+    event TeamCreated( address[] founders, address[] members);
+    event ContractDeployed( string contractType, address contractAddress);
 
-  function initialize( address _bankAccountBeacon, address _governanceContractBeacon) public initializer {
+  function initialize( address _bankAccountBeacon, address _votingContractBeacon) public initializer {
         __Ownable_init(msg.sender);
         __ReentrancyGuard_init();
         __Pausable_init();
         bankAccountBeacon = _bankAccountBeacon;
-        governanceContractBeacon = _governanceContractBeacon;
+        votingContractBeacon = _votingContractBeacon;
     }
-    // Function to create a new team with founders
+
    function createTeam(
     address[] memory _founders, 
     address[] memory _members
-) external {
-    require(_founders.length > 0, "Must have at least one founder");
-    
-    
-    // Initialize the new team
-    Team storage newTeam = teams[teamCounter];
-    newTeam.founders = _founders;
-    newTeam.teamOwner = msg.sender;
-    newTeam.bankAccountContract = address(0);
-    newTeam.governanceContract = address(0);
-    
-    // Add founders to the team
-    for (uint256 i = 0; i < _founders.length; i++) {
-        require(_founders[i] != address(0), "Invalid founder address");
-        newTeam.founders.push(_founders[i]);
+    ) external {
+        require(_founders.length > 0, "Must have at least one founder");
+        bankAccountContract = address(0);
+        votingContract = address(0);
+
+        for (uint256 i = 0; i < _founders.length; i++) {
+            require(_founders[i] != address(0), "Invalid founder address");
+            founders.push(_founders[i]);
+        }
+        for (uint256 i = 0; i < _members.length; i++) {
+            require(_members[i] != address(0), "Invalid member address");
+            members.push(_members[i]);
+        }
+        emit TeamCreated( _founders, _members);
     }
 
-    // Add members to the team
-    for (uint256 i = 0; i < _members.length; i++) {
-        require(_members[i] != address(0), "Invalid member address");
-        newTeam.members.push(_members[i]);
-    }
-
-    teams[teamCounter] = newTeam;
-    // Emit event after successful creation
-    teamCounter++;
-
-    emit TeamCreated(teamCounter, _founders, _members);
-}
-
-
-
-    // Function to deploy the bank account contract using BeaconProxy
-    function deployBankAccount(uint256 teamId) external {
-        require(isFounderOrOwner(teamId, msg.sender), "Only founders or team owner can deploy");
-
-        Team storage team = teams[teamId];
-        require(team.bankAccountContract == address(0), "Bank account contract already deployed");
-
+    function deployBankAccount() external onlyOwner {
+        require(bankAccountContract == address(0), "Bank account contract already deployed");
         BeaconProxy proxy = new BeaconProxy(
             bankAccountBeacon,
-            abi.encodeWithSelector(IBankAccount.initialize.selector, team.teamOwner)
+            abi.encodeWithSelector(IBankAccount.initialize.selector, owner())
         );
-        team.bankAccountContract = address(proxy);
+        bankAccountContract = address(proxy);
 
-        emit ContractDeployed(teamId, "BankAccount", address(proxy));
+        emit ContractDeployed("BankAccount", address(proxy));
     }
 
+    function deployVotingContract() external onlyOwner {
+        require(votingContract == address(0), "Governance contract already deployed");
 
+        console.log("Deploying Governance Contract",votingContractBeacon);
 
-  
-
-    // Function to deploy the governance contract using BeaconProxy
-    function deployGovernanceContract(uint256 teamId) external {
-        require(isFounderOrOwner(teamId, msg.sender), "Only founders or team owner can deploy");
-
-        Team storage team = teams[teamId];
-        require(team.governanceContract == address(0), "Governance contract already deployed");
+        require(votingContractBeacon != address(0), "Invalid governance contract beacon");
 
         BeaconProxy proxy = new BeaconProxy(
-            governanceContractBeacon,
-            abi.encodeWithSelector(IGovernanceContract.initialize.selector, team.teamOwner)
+            votingContractBeacon,
+            abi.encodeWithSelector(IVotingContract.initialize.selector) 
         );
-        team.governanceContract = address(proxy);
+        
+        votingContract = address(proxy);
 
-        emit ContractDeployed(teamId, "GovernanceContract", address(proxy));
+        console.log("Voting Contract deployed at: ", votingContract);
+
+        emit ContractDeployed("VotingContract", votingContract);
     }
 
-    // Function to transfer ownership of the team contract to the Board of Directors (BOD)
-    function transferOwnershipToBOD(uint256 teamId, address newOwner) external {
-        require(isFounderOrOwner(teamId, msg.sender), "Only founders or team owner can transfer ownership");
 
-        Team storage team = teams[teamId];
-        team.teamOwner = newOwner;
+    function transferOwnershipToBOD(address newOwner) external {
+        transferOwnership(newOwner);
+        emit OwnershipTransferred(owner(), newOwner);
     }
 
-    // Internal helper to check if an address is a founder of the team
-    function isFounder(uint256 teamId, address user) internal view returns (bool) {
-        Team storage team = teams[teamId];
-        for (uint256 i = 0; i < team.founders.length; i++) {
-            if (team.founders[i] == user) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function isFounderOrOwner(uint256 teamId, address user) internal view returns (bool) {
-        return isFounder(teamId, user) || teams[teamId].teamOwner == user;
-    }
-    function getTeam(uint256 teamId) external view returns (Team memory) {
-        console.log("teamId", teams[teamId].teamOwner);
-        return teams[teamId];
+    function getTeam() external view returns (address[] memory, address[] memory , address , address ) {
+        return (founders, members, bankAccountContract, votingContract);
     }
 }
--- contract/test/Officer.spec.ts
+++ contract/test/Officer.spec.ts
@@ -1,118 +1,75 @@
 import { expect } from 'chai'
 import { ethers, upgrades } from 'hardhat'
+import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers'
+import { Officer } from '../typechain-types'
 
 describe('Officer Contract', function () {
-  let Officer, officer: any
-  let BankAccount, GovernanceContract
-  let bankAccountBeacon, governanceContractBeacon
-  let owner: any, addr1: any, addr2: any
+  let Officer, officer: unknown
+  let BankAccount, VotingContract
+  let bankAccountBeacon, votingContractBeacon
+  let owner: SignerWithAddress, addr1: SignerWithAddress, addr2: SignerWithAddress
 
-  beforeEach(async function () {
+  this.beforeAll(async function () {
     BankAccount = await ethers.getContractFactory('Bank')
     bankAccountBeacon = await upgrades.deployBeacon(BankAccount)
 
-    GovernanceContract = await ethers.getContractFactory('Voting')
-    governanceContractBeacon = await upgrades.deployBeacon(GovernanceContract)
+    VotingContract = await ethers.getContractFactory('Voting')
+    votingContractBeacon = await upgrades.deployBeacon(VotingContract)
+    console.log(votingContractBeacon)
     ;[owner, addr1, addr2] = await ethers.getSigners()
 
     Officer = await ethers.getContractFactory('Officer')
-    // console.log(bankAccountBeacon.target, governanceContractBeacon.target)
-    // console.log(Officer)
     officer = await upgrades.deployProxy(
       Officer,
-      [await bankAccountBeacon.getAddress(), await governanceContractBeacon.getAddress()],
+      [await bankAccountBeacon.getAddress(), await votingContractBeacon.getAddress()],
       { initializer: 'initialize' }
     )
   })
 
   it('Should create a new team', async function () {
     const founders = [addr1.address, addr2.address]
-    // console.log('founders', founders)
-    const tx = await officer
+    const tx = await (officer as Officer)
       .connect(owner)
       .createTeam(founders, ['0xaFeF48F7718c51fb7C6d1B314B3991D2e1d8421E'])
 
-    const receipt = await tx.wait()
+    await tx.wait()
+    const team = await (officer as Officer).getTeam()
 
-    console.log('receipt', receipt.logs[0].args)
-    const team = await officer.getTeam(0)
-    // const receipt2 = await team.wait()
-    console.log('receipt2', team)
-    expect(team.teamOwner).to.equal(owner.address)
-    console.log('team', team)
-
-    expect(team.founders[0]).to.equal(addr1.address)
-    expect(team.founders[1]).to.equal(addr2.address)
+    expect(team[0][0]).to.equal(addr1.address)
+    expect(team[0][1]).to.equal(addr2.address)
   })
+  it('Should deploy the Voting contract via BeaconProxy', async function () {
+    await (officer as Officer).connect(owner).deployVotingContract()
 
+    const team = await (officer as Officer).getTeam()
+    console.log(team[3])
+    expect(team[3]).to.be.not.equal('0x0000000000000000000000000000000000000000')
+  })
   it('Should deploy the BankAccount contract via BeaconProxy', async function () {
-    const founders = [addr1.address, addr2.address]
+    await (officer as Officer).connect(owner).deployBankAccount()
 
-    await officer.connect(owner).createTeam(founders)
-    await officer.connect(owner).deployBankAccount(1)
-
-    const team = await officer.teams(1)
-    expect(team.bankAccountContract).to.not.equal(ethers.constants.AddressZero)
-  })
-
-  it('Should deploy the EmployeeContract contract via BeaconProxy', async function () {
-    const founders = [addr1.address, addr2.address]
-
-    await officer.connect(owner).createTeam(founders)
-    await officer.connect(owner).deployEmployeeContract(1)
-
-    const team = await officer.teams(1)
-    expect(team.employeeContract).to.not.equal(ethers.constants.AddressZero)
-  })
-
-  it('Should deploy the GovernanceContract contract via BeaconProxy', async function () {
-    const founders = [addr1.address, addr2.address]
-
-    await officer.connect(owner).createTeam(founders)
-    await officer.connect(owner).deployGovernanceContract(1)
-
-    const team = await officer.teams(1)
-    expect(team.governanceContract).to.not.equal(ethers.constants.AddressZero)
+    const team = await (officer as Officer).getTeam()
+    expect(team[2]).to.be.not.equal('0x0000000000000000000000000000000000000000')
   })
 
   it('Should transfer ownership to a new owner', async function () {
-    const founders = [addr1.address, addr2.address]
+    const team = await (officer as Officer).connect(owner).transferOwnershipToBOD(addr1.address)
 
-    await officer.connect(owner).createTeam(founders)
-    await officer.connect(owner).transferOwnershipToBOD(1, addr1.address)
+    const receipt = await team.wait()
+    interface TransactionReceipt {
+      logs: {
+        args: {
+          newOwner: string
+        }
+      }[]
+    }
 
-    const team = await officer.teams(1)
-    expect(team.teamOwner).to.equal(addr1.address)
+    expect((receipt as unknown as TransactionReceipt).logs[0].args.newOwner).to.equal(addr1.address)
   })
 
   it('Should fail if non-founder or non-owner tries to deploy', async function () {
-    const founders = [addr1.address, addr2.address]
-
-    await officer.connect(owner).createTeam(founders)
-
-    await expect(officer.connect(addr2).deployBankAccount(1)).to.be.revertedWith(
-      'Only founders or team owner can deploy'
-    )
-  })
-
-  it('Should not allow signing the agreement twice', async function () {
-    const founders = [addr1.address, addr2.address]
-
-    await officer.connect(owner).createTeam(founders)
-    await officer.connect(addr1).signAgreement(1)
-
-    await expect(officer.connect(addr1).signAgreement(1)).to.be.revertedWith(
-      'Agreement already signed'
-    )
-  })
-
-  it('Should fail if non-founder tries to sign the agreement', async function () {
-    const founders = [addr1.address, addr2.address]
-
-    await officer.connect(owner).createTeam(founders)
-
-    await expect(officer.connect(owner).signAgreement(1)).to.be.revertedWith(
-      'Only founders can sign the agreement'
-    )
+    await expect(
+      (officer as Officer).connect(addr2).deployBankAccount()
+    ).to.be.revertedWithCustomError(officer as Officer, 'OwnableUnauthorizedAccount')
   })
 })
