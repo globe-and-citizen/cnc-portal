ba64732247db1809cc9bf22d65d2eab161d80b4d
Dasarath G <dasarathg68@gmail.com>
2024-09-23 18:52:23 +0530

    feat: define ui for officer form

--- app/src/components/forms/OfficerForm.vue
+++ app/src/components/forms/OfficerForm.vue
@@ -1,11 +1,20 @@
 <template>
-  <div></div>
+  <div class="flex flex-col">
+    <h3 class="text-xl font-bold">Manage deployments</h3>
+    <hr />
+
+    <div class="flex items-center justify-center mt-4">
+      <button class="btn btn-primary btn-sm" v-if="!team?.officerAddress">
+        Create Officer Contract
+      </button>
+      <div v-else></div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
-// import type { Team } from '@/types'
-// import { useReadContract } from '@wagmi/vue'
+import type { Team } from '@/types'
 
-// const props = defineProps<{
-//   team: Partial<Team>
-// }>()
+defineProps<{
+  team: Partial<Team>
+}>()
 </script>
