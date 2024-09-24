ec674e4ef5ab875e5cda7731b90428f34aa99923
Dasarath G <31665486+dasarathg68@users.noreply.github.com>
2024-09-24 18:27:39 +0530

    chore: Apply suggestions from code review
    Co-authored-by: Muhammad Farrell Hauzan <49224826+farrellh1@users.noreply.github.com>

--- contract/contracts/Officer.sol
+++ contract/contracts/Officer.sol
@@ -9,7 +9,6 @@ import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol';
 
-import "hardhat/console.sol";
 
 interface IBankAccount {
     function initialize(address tipsAddress) external;
@@ -44,10 +43,8 @@ contract Officer is OwnableUpgradeable, ReentrancyGuardUpgradeable, PausableUpgr
    function createTeam(
     address[] memory _founders, 
     address[] memory _members
-    ) external {
+    ) external  whenNotPaused {
         require(_founders.length > 0, "Must have at least one founder");
-        bankAccountContract = address(0);
-        votingContract = address(0);
 
         for (uint256 i = 0; i < _founders.length; i++) {
             require(_founders[i] != address(0), "Invalid founder address");
@@ -60,7 +57,7 @@ contract Officer is OwnableUpgradeable, ReentrancyGuardUpgradeable, PausableUpgr
         emit TeamCreated( _founders, _members);
     }
 
-    function deployBankAccount(address tipsAddress) external onlyOwners {
+    function deployBankAccount(address tipsAddress) external onlyOwners whenNotPaused {
         require(bankAccountContract == address(0), "Bank account contract already deployed");
         BeaconProxy proxy = new BeaconProxy(
             bankAccountBeacon,
