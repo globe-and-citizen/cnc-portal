3a38a873290024be687d2dd9fee325a57ba8d5f2
Dasarath G <dasarathg68@gmail.com>
2024-09-24 18:01:03 +0530

    chore: apply suggestion

--- contract/ignition/modules/OfficerModule.ts
+++ contract/ignition/modules/OfficerModule.ts
@@ -1,13 +1,9 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
-import boardOfDirectorsBeaconModule from './BoardOfDirectorsBeaconModule'
-import VotingBeaconModule from './VotingBeaconModule'
 
 export default buildModule('Officer', (m) => {
   const beaconAdmin = m.getAccount(0)
   const officer = m.contract('Officer')
-  const { beacon: BoDBeacon } = m.useModule(boardOfDirectorsBeaconModule)
-  const { beacon: votingBeacon } = m.useModule(VotingBeaconModule)
-  m.call(officer, 'initialize', [BoDBeacon, votingBeacon])
+
   const beacon = m.contract('Beacon', [officer], {
     from: beaconAdmin
   })
