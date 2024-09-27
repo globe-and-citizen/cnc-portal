62f394bd0d4c08b387f7ee99ef893a517c6b8213
Hermann EHO <hermannleboss@gmail.com>
2024-09-27 06:59:42 +0000

    feat: :zap: Improve Employee contract perf and Dev Experience
    Introduce enum for offer status, update event, remove updateOffer (Create is doing the same), and optimise check on offer interaction

--- contract/contracts/Employee.sol
+++ contract/contracts/Employee.sol
@@ -4,14 +4,28 @@ pragma solidity ^0.8.24;
 import '@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol';
+import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
+
 
 contract Employee is OwnableUpgradeable, ReentrancyGuardUpgradeable, PausableUpgradeable {
+  using EnumerableSet for EnumerableSet.AddressSet;
+
+  // Enum for offer statuses
+  enum OfferStatus {
+    Offered,
+    Accepted,
+    Rejected,
+    Resigned,
+    Fired
+  }
+
+  // Struct for employee offers with better packing
   struct EmployeeOffer {
-    string contractUrl;
-    uint256 salary;
-    uint256 status; // 0 - offered, 1 - accepted, 2 - rejected, 3 - resigned, 4 - fired
+    OfferStatus status;
     uint256 startDate;
     uint256 endDate;
+    uint256 salary;
+    string contractUrl;
   }
 
   struct EmployeeOffers {
@@ -19,22 +33,56 @@ contract Employee is OwnableUpgradeable, ReentrancyGuardUpgradeable, PausableUpg
     EmployeeOffer pendingOffer;
   }
 
+  // Using EnumerableSet for more efficient storage of employee addresses
+  EnumerableSet.AddressSet private employees;
   mapping(address => EmployeeOffers) private employeeOffers;
-  address[] private employeeAddresses;
 
-  event EmployeeOffered(address indexed employee, uint256 salary, string contractUrl);
-  event EmployeeAccepted(address indexed employee, uint256 salary, string contractUrl);
-  event EmployeeRejected(address indexed employee, uint256 salary, string contractUrl);
-  event EmployeeResigned(address indexed employee, uint256 salary, string contractUrl);
-  event EmployeeFired(address indexed employee, uint256 salary, string contractUrl);
-  event EmployeeOfferApproved(address indexed employee, uint256 salary, string contractUrl);
+  // Events with indexed parameters for better searchability
+  event EmployeeOffered(
+    address indexed employee,
+    uint256 salary,
+    string contractUrl,
+    OfferStatus status
+  );
+  event EmployeeAccepted(
+    address indexed employee,
+    uint256 salary,
+    string contractUrl,
+    OfferStatus status
+  );
+  event EmployeeRejected(
+    address indexed employee,
+    uint256 salary,
+    string contractUrl,
+    OfferStatus status
+  );
+  event EmployeeResigned(
+    address indexed employee,
+    uint256 salary,
+    string contractUrl,
+    OfferStatus status
+  );
+  event EmployeeFired(
+    address indexed employee,
+    uint256 salary,
+    string contractUrl,
+    OfferStatus status
+  );
+  event EmployeeOfferApproved(
+    address indexed employee,
+    uint256 salary,
+    string contractUrl,
+    OfferStatus status
+  );
 
+  // Initialization function for upgradeable contract
   function initialize() public initializer {
     __Ownable_init(msg.sender);
     __ReentrancyGuard_init();
     __Pausable_init();
   }
 
+  // Create a new offer for an employee with added checks and status management
   function createOffer(
     address _employee,
     string memory _contractUrl,
@@ -44,116 +92,114 @@ contract Employee is OwnableUpgradeable, ReentrancyGuardUpgradeable, PausableUpg
     require(_employee != address(0), 'Invalid employee address');
     require(bytes(_contractUrl).length > 0, 'Invalid contract URL');
     require(_salary > 0, 'Invalid salary');
+    require(_endDate > block.timestamp, 'End date must be in the future');
 
-    if (employeeOffers[_employee].pendingOffer.status == 0 && bytes(employeeOffers[_employee].pendingOffer.contractUrl).length == 0) {
-      employeeAddresses.push(_employee);
+    // If this is a new employee, add to the employees set
+    if (!employees.contains(_employee)) {
+      employees.add(_employee);
     }
 
     employeeOffers[_employee].pendingOffer = EmployeeOffer({
       contractUrl: _contractUrl,
       salary: _salary,
-      status: 0,
+      status: OfferStatus.Offered,
       startDate: 0,
       endDate: _endDate
     });
 
-    emit EmployeeOffered(_employee, _salary, _contractUrl);
+    emit EmployeeOffered(_employee, _salary, _contractUrl, OfferStatus.Offered);
   }
 
+  // Employee accepts an offer
   function acceptOffer() external nonReentrant whenNotPaused {
-    require(bytes(employeeOffers[msg.sender].pendingOffer.contractUrl).length > 0, 'No pending offer found');
-    require(employeeOffers[msg.sender].pendingOffer.status == 0, 'No active pending offer found');
+    EmployeeOffer storage pendingOffer = employeeOffers[msg.sender].pendingOffer;
+    require(pendingOffer.status == OfferStatus.Offered, 'No valid pending offer to accept');
 
-    employeeOffers[msg.sender].pendingOffer.status = 1;
-    employeeOffers[msg.sender].pendingOffer.startDate = block.timestamp;
+    pendingOffer.status = OfferStatus.Accepted;
+    pendingOffer.startDate = block.timestamp;
 
     emit EmployeeAccepted(
       msg.sender,
-      employeeOffers[msg.sender].pendingOffer.salary,
-      employeeOffers[msg.sender].pendingOffer.contractUrl
+      pendingOffer.salary,
+      pendingOffer.contractUrl,
+      OfferStatus.Accepted
     );
   }
 
+  // Owner approves the accepted offer for the employee
   function approvePendingOffer(address _employee) external onlyOwner nonReentrant whenNotPaused {
-    require(bytes(employeeOffers[_employee].pendingOffer.contractUrl).length > 0, 'No pending offer found');
-    require(employeeOffers[_employee].pendingOffer.status == 1, 'Pending offer not accepted by employee');
+    EmployeeOffer storage pendingOffer = employeeOffers[_employee].pendingOffer;
+    require(pendingOffer.status == OfferStatus.Accepted, 'Pending offer not accepted by employee');
 
-    employeeOffers[_employee].activeOffer = employeeOffers[_employee].pendingOffer;
+    employeeOffers[_employee].activeOffer = pendingOffer;
     delete employeeOffers[_employee].pendingOffer;
 
     emit EmployeeOfferApproved(
       _employee,
       employeeOffers[_employee].activeOffer.salary,
-      employeeOffers[_employee].activeOffer.contractUrl
+      employeeOffers[_employee].activeOffer.contractUrl,
+      OfferStatus.Accepted
     );
   }
 
+  // Employee rejects the pending offer
   function rejectOffer() external nonReentrant whenNotPaused {
-    require(bytes(employeeOffers[msg.sender].pendingOffer.contractUrl).length > 0, 'No pending offer found');
-    require(employeeOffers[msg.sender].pendingOffer.status == 0, 'No active pending offer found');
+    EmployeeOffer storage pendingOffer = employeeOffers[msg.sender].pendingOffer;
+    require(pendingOffer.status == OfferStatus.Offered, 'No active pending offer found');
 
-    employeeOffers[msg.sender].pendingOffer.status = 2;
+    pendingOffer.status = OfferStatus.Rejected;
 
     emit EmployeeRejected(
       msg.sender,
-      employeeOffers[msg.sender].pendingOffer.salary,
-      employeeOffers[msg.sender].pendingOffer.contractUrl
+      pendingOffer.salary,
+      pendingOffer.contractUrl,
+      OfferStatus.Rejected
     );
   }
 
+  // Employee resigns from an active offer
   function resignOffer() external nonReentrant whenNotPaused {
-    require(bytes(employeeOffers[msg.sender].activeOffer.contractUrl).length > 0, 'No active offer found');
-    require(employeeOffers[msg.sender].activeOffer.status == 1, 'No accepted active offer found');
+    EmployeeOffer storage activeOffer = employeeOffers[msg.sender].activeOffer;
+    require(activeOffer.status == OfferStatus.Accepted, 'No active accepted offer found');
 
-    employeeOffers[msg.sender].activeOffer.status = 3;
-    employeeOffers[msg.sender].activeOffer.endDate = block.timestamp;
+    activeOffer.status = OfferStatus.Resigned;
+    activeOffer.endDate = block.timestamp;
 
     emit EmployeeResigned(
       msg.sender,
-      employeeOffers[msg.sender].activeOffer.salary,
-      employeeOffers[msg.sender].activeOffer.contractUrl
+      activeOffer.salary,
+      activeOffer.contractUrl,
+      OfferStatus.Resigned
     );
   }
 
+  // Owner fires an employee
   function fireEmployee(address _employee) external onlyOwner nonReentrant whenNotPaused {
-    require(bytes(employeeOffers[_employee].activeOffer.contractUrl).length > 0, 'No active offer found');
-    require(employeeOffers[_employee].activeOffer.status == 1, 'No accepted active offer found');
+    EmployeeOffer storage activeOffer = employeeOffers[_employee].activeOffer;
+    require(activeOffer.status == OfferStatus.Accepted, 'No active accepted offer found');
 
-    employeeOffers[_employee].activeOffer.status = 4;
-    employeeOffers[_employee].activeOffer.endDate = block.timestamp;
+    activeOffer.status = OfferStatus.Fired;
+    activeOffer.endDate = block.timestamp;
 
-    emit EmployeeFired(
-      _employee,
-      employeeOffers[_employee].activeOffer.salary,
-      employeeOffers[_employee].activeOffer.contractUrl
-    );
+    emit EmployeeFired(_employee, activeOffer.salary, activeOffer.contractUrl, OfferStatus.Fired);
   }
 
-  function updateOffer(
-    address _employee,
-    uint256 _salary,
-    string memory _contractUrl
-  ) external onlyOwner nonReentrant whenNotPaused {
-    require(_employee != address(0), 'Invalid employee address');
-    require(bytes(_contractUrl).length > 0, 'Invalid contract URL');
-    require(_salary > 0, 'Invalid salary');
-
-    employeeOffers[_employee].pendingOffer = EmployeeOffer({
-      contractUrl: _contractUrl,
-      salary: _salary,
-      status: 0,
-      startDate: 0,
-      endDate: 0
-    });
-
-    emit EmployeeOffered(_employee, _salary, _contractUrl);
-  }
-
+  // List all employees
   function listEmployees() external view returns (address[] memory) {
-    return employeeAddresses;
+    return employees.values();
   }
 
+  // Get employee offers
   function getEmployeeOffers(address _employee) external view returns (EmployeeOffers memory) {
     return employeeOffers[_employee];
+  }
+
+  // Clear pending offers after they are rejected or expired (could be used with Chainlink Keepers)
+  function clearExpiredOffers(address _employee) external onlyOwner nonReentrant {
+    require(
+      employeeOffers[_employee].pendingOffer.endDate <= block.timestamp,
+      'Offer has not expired yet'
+    );
+    delete employeeOffers[_employee].pendingOffer;
   }
 }
