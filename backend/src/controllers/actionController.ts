8eea75ca508b6309f78043abcc65b367590838f2
farrellh1 <farrell.hauzan@gmail.com>
2024-09-28 23:46:20 +0700

    feat: add action controller and routes

---
+++ backend/src/controllers/actionController.ts
@@ -0,0 +1,100 @@
+import { Prisma } from "@prisma/client";
+import { prisma, errorResponse } from "../utils";
+import { Request, Response } from "express";
+
+const getActions = async (req: Request, res: Response) => {
+  try {
+    const { teamId, isExecuted, page, take } = req.query;
+    if (!teamId) return errorResponse(400, "Team ID empty or not set", res);
+
+    const where: Prisma.BoardOfDirectorActionsWhereInput = {};
+
+    if (teamId) {
+      where.teamId = parseInt(teamId as string);
+    }
+    if (isExecuted) {
+      where.isExecuted = isExecuted === "true";
+    }
+    const actions = await prisma.boardOfDirectorActions.findMany({
+      where,
+      skip: page
+        ? (parseInt(page as string) - 1) * parseInt(take as string)
+        : 0,
+      take: take ? parseInt(take as string) : 10,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const totalActions = await prisma.boardOfDirectorActions.count({
+      where,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: actions,
+      total: totalActions,
+    });
+  } catch (error) {
+    return errorResponse(500, error, res);
+  }
+};
+
+const addAction = async (req: Request, res: Response) => {
+  const { teamId, actionId, description, targetAddress, data } = req.body;
+  if (!teamId || !description || !targetAddress || !data) {
+    return errorResponse(400, "Missing required fields", res);
+  }
+
+  try {
+    const newAction = await prisma.boardOfDirectorActions.create({
+      data: {
+        teamId: parseInt(teamId as string),
+        actionId: parseInt(actionId as string),
+        description: description as string,
+        targetAddress: targetAddress as string,
+        userAddress: (req as any).address,
+        data: data as string,
+      },
+    });
+
+    res.status(201).json({
+      success: true,
+      data: newAction,
+    });
+  } catch (error) {
+    return errorResponse(500, error, res);
+  }
+};
+
+const executeAction = async (req: Request, res: Response) => {
+  const { actionId } = req.params;
+  if (!actionId) return errorResponse(400, "Action ID empty or not set", res);
+
+  try {
+    const action = await prisma.boardOfDirectorActions.findUnique({
+      where: {
+        id: parseInt(actionId as string),
+      },
+    });
+
+    if (!action) return errorResponse(404, "Action not found", res);
+
+    await prisma.boardOfDirectorActions.update({
+      where: {
+        id: parseInt(actionId as string),
+      },
+      data: {
+        isExecuted: true,
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    return errorResponse(500, error, res);
+  }
+};
+
+export { getActions, addAction, executeAction };
