987dc63243e09ab229c7f78027fa4239ed291315
Hermann EHO <hermannleboss@gmail.com>
2024-10-24 09:11:01 +0000

    test: clean up test

--- app/src/components/sections/SingleTeamView/__tests__/MemberRow.spec.ts
+++ app/src/components/sections/SingleTeamView/__tests__/MemberRow.spec.ts
@@ -1,10 +1,8 @@
-import { ref } from 'vue'
 import MemberRow from '@/components/sections/SingleTeamView/MemberRow.vue'
 import { mount } from '@vue/test-utils'
 import { describe, expect, it, vi, beforeEach } from 'vitest'
 import { useUserDataStore } from '@/stores/user'
 import { useToastStore } from '@/stores/useToastStore'
-import * as vueuse from '@vueuse/core'
 
 vi.mock('@/stores/user', () => ({
   useUserDataStore: vi.fn()
@@ -20,21 +18,6 @@ vi.mock('vue-router', () => ({
     }
   }))
 }))
-
-const mockCopy = vi.fn()
-const mockClipboard = {
-  copy: mockCopy,
-  copied: ref(false),
-  isSupported: ref(true)
-}
-
-vi.mock('@vueuse/core', async () => {
-  const originalModule = await vi.importActual<typeof vueuse>('@vueuse/core')
-  return {
-    ...originalModule,
-    useClipboard: vi.fn(() => mockClipboard)
-  }
-})
 
 describe('MemberRow.vue', () => {
   let wrapper: ReturnType<typeof mount>
@@ -72,21 +55,6 @@ describe('MemberRow.vue', () => {
       expect(wrapper.text()).toContain(props.member.name)
       expect(wrapper.text()).toContain(props.member.address)
     })
-
-    it('render the copy button in the copyed stat', async () => {
-      mockClipboard.isSupported.value = true
-      mockClipboard.copied.value = false
-      await wrapper.vm.$nextTick()
-
-      expect(wrapper.find('[data-test="copy-address-tooltip"]').exists()).toBe(true)
-    })
-
-    it('not render the copy button if copy is not supported', async () => {
-      mockClipboard.isSupported.value = false
-      mockClipboard.copied.value = false
-      await wrapper.vm.$nextTick()
-      expect(wrapper.find('[data-test="copy-address-tooltip"]').exists()).toBe(false)
-    })
   })
 
   describe('methods', () => {
@@ -98,5 +66,7 @@ describe('MemberRow.vue', () => {
       await wrapper.vm.$nextTick()
       expect((wrapper.vm as unknown as ComponentData).showDeleteMemberConfirmModal).toBe(true)
     })
+
+    // TODO: test when delete is validated
   })
 })
