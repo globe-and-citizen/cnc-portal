aaa339c7bb691ee36db4ff4f79e803fc4750c232
farrellh1 <farrell.hauzan@gmail.com>
2024-11-03 14:38:09 +0700

    test: add mocks

---
+++ app/src/__mocks__/viem.ts
@@ -0,0 +1,4 @@
+import { vi } from 'vitest'
+
+export const parseAbiItem = vi.fn()
+export const formatEther = vi.fn()
---
+++ app/src/__mocks__/viem/actions.ts
@@ -0,0 +1,4 @@
+import { vi } from 'vitest'
+
+export const getLogs = vi.fn()
+export const getBlock = vi.fn()
