cbe4660245771b790f8ddbc95e1085903d9b01df
Dasarath G <dasarathg68@gmail.com>
2024-11-06 00:12:08 +0530

    fix: replace useUserDataStore() with address for team owner checks

--- app/src/components/sections/SingleTeamView/TeamDetails.vue
+++ app/src/components/sections/SingleTeamView/TeamDetails.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="collapse collapse-arrow border"
-    :class="`${team.ownerAddress == useUserDataStore().address ? 'bg-green-100' : 'bg-blue-100'}`"
+    :class="`${team.ownerAddress == address ? 'bg-green-100' : 'bg-blue-100'}`"
   >
     <input type="checkbox" />
     <div class="collapse-title text-xl font-medium">
@@ -9,7 +9,7 @@
         <h2 class="pl-5">{{ team.name }}</h2>
         <div
           class="badge badge-sm badge-primary flex items-center justify-center ml-2"
-          v-if="team.ownerAddress == useUserDataStore().address"
+          v-if="team.ownerAddress == address"
         >
           Owner
         </div>
@@ -22,14 +22,14 @@
       <div class="pl-5 flex flex-row justify-center gap-2 mt-5 items-center">
         <button
           class="btn btn-secondary btn-sm"
-          v-if="team.ownerAddress == useUserDataStore().address"
+          v-if="team.ownerAddress == address"
           @click="emits('updateTeamModalOpen')"
         >
           Update
         </button>
         <button
           class="btn btn-error btn-sm"
-          v-if="team.ownerAddress == useUserDataStore().address"
+          v-if="team.ownerAddress == address"
           @click="emits('deleteTeam')"
         >
           Delete
@@ -41,6 +41,8 @@
 <script setup lang="ts">
 import { useUserDataStore } from '@/stores/user'
 import type { Team } from '@/types'
+
+const { address } = useUserDataStore()
 
 const emits = defineEmits(['updateTeamModalOpen', 'deleteTeam'])
 defineProps<{
