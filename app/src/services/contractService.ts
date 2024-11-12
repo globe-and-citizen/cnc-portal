f18edc50d347cccf81a0c0ddd82c0964aea9ad94
achille <achillegley@gmail.com>
2024-11-12 14:50:48 +0700

    feat: add getEvent function to contractService

--- app/src/services/contractService.ts
+++ app/src/services/contractService.ts
@@ -1,12 +1,13 @@
 import { EthersJsAdapter, type IWeb3Library } from '@/adapters/web3LibraryAdapter'
 import type { InterfaceAbi } from 'ethers'
-import type { Contract } from 'ethers'
+import type { EventLog , Log, Contract} from 'ethers'
 
 interface ISmartContract {
   contract: Contract | undefined
   contractAddress: string
   contractAbi: InterfaceAbi
   getContract(address: string, abi: InterfaceAbi): Promise<Contract>
+  getEvents(type: string): Promise<EventLog[] | Log[]>
 }
 
 export class SmartContract implements ISmartContract {
@@ -31,5 +32,13 @@ export class SmartContract implements ISmartContract {
     }
 
     return this.contract
+  }
+
+  async getEvents(type: string): Promise<EventLog[] | Log[]> {
+    if (!this.contract) {
+      this.contract = await this.web3Library.getContract(this.contractAddress, this.contractAbi)
+    }
+    const logs=await this.contract.queryFilter(type) as EventLog[];
+    return logs
   }
 }
