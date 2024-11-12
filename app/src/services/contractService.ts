089b2d21bd642adf9243f933d0258a3197c919e5
achille <achillegley@gmail.com>
2024-11-12 14:59:06 +0700

    fix: correct contract service format

--- app/src/services/contractService.ts
+++ app/src/services/contractService.ts
@@ -1,6 +1,6 @@
 import { EthersJsAdapter, type IWeb3Library } from '@/adapters/web3LibraryAdapter'
 import type { InterfaceAbi } from 'ethers'
-import type { EventLog , Log, Contract} from 'ethers'
+import type { EventLog, Log, Contract } from 'ethers'
 
 interface ISmartContract {
   contract: Contract | undefined
@@ -38,7 +38,7 @@ export class SmartContract implements ISmartContract {
     if (!this.contract) {
       this.contract = await this.web3Library.getContract(this.contractAddress, this.contractAbi)
     }
-    const logs=await this.contract.queryFilter(type) as EventLog[];
+    const logs = (await this.contract.queryFilter(type)) as EventLog[]
     return logs
   }
 }
