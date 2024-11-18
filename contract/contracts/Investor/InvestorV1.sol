4caafc7b6fb012b736e95cf2640e742f55824ac0
farrellh1 <farrell.hauzan@gmail.com>
2024-11-18 20:52:53 +0700

    fix: remove unnecessary code

--- contract/contracts/Investor/InvestorV1.sol
+++ contract/contracts/Investor/InvestorV1.sol
@@ -40,9 +40,6 @@ contract InvestorV1 is ERC20Upgradeable, OwnableUpgradeable, PausableUpgradeable
   }
 
   function individualMint(address shareholder, uint256 amount) external onlyOwner whenNotPaused nonReentrant {
-    if (!shareholders.contains(shareholder)) {
-      shareholders.add(shareholder);
-    }
     _mint(shareholder, amount);
     emit Minted(shareholder, amount);
   }
