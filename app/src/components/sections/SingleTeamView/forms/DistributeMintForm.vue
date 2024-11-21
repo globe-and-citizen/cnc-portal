7e17981d39d4d5884faae7b5c5cd00b5f97c6c07
farrellh1 <farrell.hauzan@gmail.com>
2024-11-22 00:48:31 +0700

    feat: add search functionality

--- app/src/components/sections/SingleTeamView/forms/DistributeMintForm.vue
+++ app/src/components/sections/SingleTeamView/forms/DistributeMintForm.vue
@@ -14,8 +14,36 @@
             class="grow"
             data-test="address-input"
             v-model="shareholder.shareholder"
+            @keyup.stop="
+              () => {
+                searchUsers({ name: shareholder.shareholder })
+                showDropdown[index] = true
+              }
+            "
           />
         </label>
+
+        <div
+          class="dropdown"
+          :class="{ 'dropdown-open': !!foundUsers && foundUsers.length > 0 && showDropdown[index] }"
+          :key="index"
+          v-if="showDropdown[index]"
+        >
+          <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-96">
+            <li v-for="user in foundUsers" :key="user.address">
+              <a
+                @click="
+                  () => {
+                    shareholderWithAmounts[index].shareholder = user.address ?? ''
+                    showDropdown[index] = false
+                  }
+                "
+              >
+                {{ user.name }} | {{ user.address }}
+              </a>
+            </li>
+          </ul>
+        </div>
         <div
           class="pl-4 text-red-500 text-sm w-full text-left"
           v-for="error of $v.shareholderWithAmounts.$each.$response.$errors[index].shareholder"
@@ -42,14 +70,24 @@
     <div class="flex justify-end pt-3">
       <div
         class="w-6 h-6 cursor-pointer"
-        @click="() => shareholderWithAmounts.push({ shareholder: '', amount: 0 })"
+        @click="
+          () => {
+            shareholderWithAmounts.push({ shareholder: '', amount: 0 })
+            showDropdown.push(false)
+          }
+        "
         data-test="plus-icon"
       >
         <PlusCircleIcon class="size-6 text-green-700" />
       </div>
       <div
         class="w-6 h-6 cursor-pointer"
-        @click="() => shareholderWithAmounts.length > 1 && shareholderWithAmounts.pop()"
+        @click="
+          () => {
+            shareholderWithAmounts.length > 1 && shareholderWithAmounts.pop()
+            showDropdown = showDropdown.slice(0, showDropdown.length - 1)
+          }
+        "
         data-test="minus-icon"
       >
         <MinusCircleIcon class="size-6 text-red-700" />
@@ -67,12 +105,17 @@
 
 <script setup lang="ts">
 import LoadingButton from '@/components/LoadingButton.vue'
+import { useCustomFetch } from '@/composables/useCustomFetch'
+import { useToastStore } from '@/stores'
+import type { User } from '@/types'
 import { MinusCircleIcon, PlusCircleIcon } from '@heroicons/vue/24/outline'
 import useVuelidate from '@vuelidate/core'
 import { helpers, numeric, required } from '@vuelidate/validators'
 import { parseEther, isAddress } from 'viem'
+import { ref, watch } from 'vue'
 import { reactive } from 'vue'
 
+const { addErrorToast } = useToastStore()
 const emits = defineEmits(['submit'])
 defineProps<{
   tokenSymbol: string
@@ -90,7 +133,8 @@ const rules = {
       },
       amount: {
         required,
-        numeric
+        numeric,
+        minValue: helpers.withMessage('Amount must be greater than 0', (value: number) => value > 0)
       }
     })
   }
@@ -109,5 +153,46 @@ const onSubmit = () => {
       }
     })
   )
+}
+
+const searchUserName = ref('')
+const foundUsers = ref<User[]>([])
+const showDropdown = reactive<boolean[]>([false])
+
+const {
+  execute: executeSearchUser,
+  response: searchUserResponse,
+  data: users
+} = useCustomFetch('user/search', {
+  immediate: false,
+  beforeFetch: async ({ options, url, cancel }) => {
+    const params = new URLSearchParams()
+    params.append('name', searchUserName.value)
+
+    url += '?' + params.toString()
+    return { options, url, cancel }
+  }
+})
+  .get()
+  .json()
+
+watch(searchUserResponse, () => {
+  if (searchUserResponse.value?.ok && users.value?.users) {
+    foundUsers.value = users.value.users
+  }
+})
+const searchUsers = async (input: { name: string }) => {
+  try {
+    if (input.name !== searchUserName.value && input.name.length > 0) {
+      searchUserName.value = input.name
+    }
+    await executeSearchUser()
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      addErrorToast(error.message)
+    } else {
+      addErrorToast('An unknown error occurred')
+    }
+  }
 }
 </script>
--- app/src/components/sections/SingleTeamView/forms/MintForm.vue
+++ app/src/components/sections/SingleTeamView/forms/MintForm.vue
@@ -12,8 +12,34 @@
         data-test="address-input"
         v-model="to"
         :disabled="Boolean(address)"
+        @keyup.stop="
+          () => {
+            searchUsers({ name: to ?? '' })
+            showDropdown = true
+          }
+        "
       />
     </label>
+    <div
+      class="dropdown"
+      :class="{ 'dropdown-open': !!foundUsers && foundUsers.length > 0 && showDropdown }"
+      v-if="showDropdown"
+    >
+      <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-96">
+        <li v-for="user in foundUsers" :key="user.address">
+          <a
+            @click="
+              () => {
+                to = user.address ?? ''
+                showDropdown = false
+              }
+            "
+          >
+            {{ user.name }} | {{ user.address }}
+          </a>
+        </li>
+      </ul>
+    </div>
     <div
       class="pl-4 text-red-500 text-sm w-full text-left"
       v-for="error of $v.address.$errors"
@@ -46,11 +72,16 @@
 
 <script setup lang="ts">
 import LoadingButton from '@/components/LoadingButton.vue'
+import { useCustomFetch } from '@/composables/useCustomFetch'
+import { useToastStore } from '@/stores'
+import type { User } from '@/types'
 import useVuelidate from '@vuelidate/core'
 import { helpers, numeric, required } from '@vuelidate/validators'
 import { isAddress, type Address } from 'viem'
+import { watch } from 'vue'
 import { onMounted, ref } from 'vue'
 
+const { addErrorToast } = useToastStore()
 const to = ref<string | null>(null)
 const amount = ref<number | null>(null)
 
@@ -68,7 +99,8 @@ const rules = {
   },
   amount: {
     required,
-    numeric
+    numeric,
+    minValue: helpers.withMessage('Amount must be greater than 0', (value: number) => value > 0)
   }
 }
 
@@ -80,6 +112,47 @@ const onSubmit = () => {
 }
 
 const $v = useVuelidate(rules, { address: to, amount })
+
+const searchUserName = ref('')
+const foundUsers = ref<User[]>([])
+const showDropdown = ref(false)
+
+const {
+  execute: executeSearchUser,
+  response: searchUserResponse,
+  data: users
+} = useCustomFetch('user/search', {
+  immediate: false,
+  beforeFetch: async ({ options, url, cancel }) => {
+    const params = new URLSearchParams()
+    params.append('name', searchUserName.value)
+
+    url += '?' + params.toString()
+    return { options, url, cancel }
+  }
+})
+  .get()
+  .json()
+
+watch(searchUserResponse, () => {
+  if (searchUserResponse.value?.ok && users.value?.users) {
+    foundUsers.value = users.value.users
+  }
+})
+const searchUsers = async (input: { name: string }) => {
+  try {
+    if (input.name !== searchUserName.value && input.name.length > 0) {
+      searchUserName.value = input.name
+    }
+    await executeSearchUser()
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      addErrorToast(error.message)
+    } else {
+      addErrorToast('An unknown error occurred')
+    }
+  }
+}
 
 onMounted(() => {
   if (props.address) {
