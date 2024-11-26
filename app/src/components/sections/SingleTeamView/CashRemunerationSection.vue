8d01612aeded7c71df660591355c23ff3a142eb8
aliassan <alimkwananzi@gmail.com>
2024-11-26 12:25:42 +0200

    feat: cash remuneration ui

--- app/src/components/sections/SingleTeamView/CashRemunerationSection.vue
+++ app/src/components/sections/SingleTeamView/CashRemunerationSection.vue
@@ -1,3 +1,62 @@
 <template>
-  <h2>Cash Remuneration</h2>
+  <div class="flex flex-col gap-y-4 py-6 lg:px-4 sm:px-6">
+    <div class="flex justify-between">
+      <span class="text-2xl sm:text-3xl font-bold">Pending Claims</span>
+      <div class="flex gap-2">
+        <label class="input input-bordered flex items-center gap-2 input-md">
+          <span class="w-24">Hours Worked</span>
+          |
+          <input
+            type="text"
+            class="grow"
+            placeholder="Enter hours worked..."
+            data-test="max-hours-input"
+          />
+        </label>
+        <button class="btn btn-success">Submit Hours</button>
+      </div>
+    </div>
+    <div class="divider m-0"></div>
+    <div class="overflow-x-auto">
+    <table class="table table-zebra">
+      <!-- head -->
+      <thead class="text-sm font-bold">
+        <tr>
+          <th>Date</th>
+          <th>Name</th>
+          <th>Address</th>
+          <th>Hours</th>
+          <th>Rate</th>
+          <th v-if="team.ownerAddress === currentUserAddress">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(data, index) in dummyData">
+          <td>{{ (new Date()).toLocaleDateString() }}</td>
+          <td>{{ data.name }}</td>
+          <td>{{ data.address }}</td>
+          <td>{{ data.hoursWorked }}</td>
+          <td>{{ data.hourlyRate }}</td>
+          <td class="flex justify-end">
+            <button class="btn btn-success">Approve</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useUserDataStore } from '@/stores'
+import type { Team } from '@/types';
+import { ref } from 'vue';
+
+const currentUserAddress = useUserDataStore().address
+const dummyData = ref([
+  {name: 'Member', address: '0x123', hoursWorked: 20, hourlyRate: 50}
+])
+
+
+const props = defineProps<{ team: Partial<Team> }>()
+</script>
