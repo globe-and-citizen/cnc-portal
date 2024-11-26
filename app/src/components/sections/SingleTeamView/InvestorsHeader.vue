71bfe9a7f18a5b91c96ccf1f481d176cafc6724f
farrellh1 <farrell.hauzan@gmail.com>
2024-11-26 21:36:59 +0700

    test: add investors header spec

--- app/src/components/sections/SingleTeamView/InvestorsHeader.vue
+++ app/src/components/sections/SingleTeamView/InvestorsHeader.vue
@@ -2,32 +2,30 @@
   <div class="flex justify-between">
     <div class="flex flex-col text-right">
       <h2>Balance</h2>
-      <p v-if="!loadingTokenBalance && !tokenSymbolLoading">
-        <span class="text-3xl">{{
-          tokenBalance != undefined ? formatEther(tokenBalance) : '-'
-        }}</span>
+      <p v-if="!loadingTokenBalance && !tokenSymbolLoading" data-test="token-balance">
+        <span class="text-3xl">{{ formatEther(tokenBalance) }}</span>
         {{ tokenSymbol }}
       </p>
       <span
         v-if="loadingTokenBalance && tokenSymbolLoading"
         class="loading loading-dots loading-sm self-end"
+        data-test="token-balance-loading"
       ></span>
     </div>
     <div class="flex flex-col text-right gap-y-4">
       <h3>Total Supply</h3>
-      <p v-if="!tokenSymbolLoading && !totalSupplyLoading">
-        <span class="text-3xl">{{
-          totalSupply != undefined ? formatEther(totalSupply) : '-'
-        }}</span>
+      <p v-if="!tokenSymbolLoading && !totalSupplyLoading" data-test="total-supply">
+        <span class="text-3xl">{{ formatEther(totalSupply) }}</span>
         {{ tokenSymbol }}
       </p>
       <span
         v-if="tokenSymbolLoading && totalSupplyLoading"
         class="loading loading-dots loading-sm self-end"
+        data-test="total-supply-loading"
       ></span>
       <div class="flex gap-x-1">
         <h4>Contract Address :</h4>
-        <AddressToolTip :address="team.investorsAddress ?? ''" />
+        <AddressToolTip :address="team.investorsAddress" />
       </div>
     </div>
   </div>
@@ -39,7 +37,7 @@ import { formatEther } from 'viem'
 import AddressToolTip from '@/components/AddressToolTip.vue'
 
 defineProps<{
-  team: Team
+  team: Partial<Team>
   tokenSymbol: string | undefined
   totalSupply: bigint | undefined
   tokenSymbolLoading: boolean
