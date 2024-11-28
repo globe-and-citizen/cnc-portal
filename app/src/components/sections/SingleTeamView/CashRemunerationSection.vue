4727d17773620c23e5ea2e89bfdf88c889d38b9b
aliassan <alimkwananzi@gmail.com>
2024-11-28 12:35:51 +0200

    feat: add claim approval functionality

--- app/src/components/sections/SingleTeamView/CashRemunerationSection.vue
+++ app/src/components/sections/SingleTeamView/CashRemunerationSection.vue
@@ -40,7 +40,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="data in wageClaims" :key="data.id">
+          <tr v-for="data in (wageClaims as ClaimResponse[])" :key="data.id">
             <td>{{ new Date(data.createdAt).toLocaleDateString() }}</td>
             <td>{{ data.name }}</td>
             <td>{{ data.address }}</td>
@@ -51,7 +51,7 @@
               class="flex justify-end"
               data-test="action-td"
             >
-              <button class="btn btn-success">Approve</button>
+              <button class="btn btn-success" @click="approveClaim(data)">Approve</button>
             </td>
           </tr>
         </tbody>
@@ -63,21 +63,31 @@
 <script setup lang="ts">
 import { useUserDataStore } from '@/stores'
 import ButtonUI from '@/components/ButtonUI.vue'
-import type { Team } from '@/types'
+import type { Team, WageClaim, ClaimResponse } from '@/types'
 import { onMounted, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { useCustomFetch } from '@/composables/useCustomFetch'
 import { useToastStore } from '@/stores'
-
+import { EthersJsAdapter } from '@/adapters/web3LibraryAdapter'
+import { log, parseError } from "@/utils";
 
 const route = useRoute()
+const web3Library = new EthersJsAdapter()
 
 const { addErrorToast, addSuccessToast } = useToastStore()
+const props = defineProps<{ team: Partial<Team> }>()
+const team = ref(props.team)
 const currentUserAddress = useUserDataStore().address
 const isSubmittingHours = ref(false)
 const hoursWorked = ref<{hoursWorked: string | undefined}>({hoursWorked: undefined})
+const approvalData = ref<{
+  signature: string | undefined;
+  id: number
+}>({signature: undefined, id: 0})
+const loadingApprove = ref(false)
 const dummyData = ref([{ name: 'Member', address: '0x123', hoursWorked: 20, hourlyRate: 50 }])
 
+//#region add wage claim
 const {
   error: addWageClaimError,
   isFetching: isWageClaimAdding,
@@ -102,7 +112,9 @@ watch(addWageClaimError, (newVal) => {
 const addWageClaim = async () => {
   await addWageClaimAPI()
 }
+//#endregion add wage claim
 
+//#region get wage claims
 const {
   error: getWageClaimsError,
   isFetching: isWageClaimsFetching,
@@ -111,17 +123,84 @@ const {
 } = useCustomFetch(`teams/${String(route.params.id)}/cash-remuneration/claim/pending`)
   .get()
   .json()
-watch(wageClaims, async (newVal) => {
-  if (newVal) {
-    addSuccessToast('Wage claims fetched successfully')
-  }
-})
+// watch(wageClaims, async (newVal) => {
+//   if (newVal) {
+//     addSuccessToast('Wage claims fetched successfully')
+//   }
+// })
 watch(getWageClaimsError, (newVal) => {
   if (newVal) {
     addErrorToast(getWageClaimsError.value)
   }
 })
-const props = defineProps<{ team: Partial<Team> }>()
+//#endregion get wage claims
+
+//#region approve wage claims
+const {
+  error: addApprovalError,
+  isFetching: isApprovalAdding,
+  execute: addApprovalAPI
+} = useCustomFetch(`teams/${String(route.params.id)}/cash-remuneration/claim/employer`, {
+  immediate: false
+})
+  .put(approvalData)
+  .json()
+watch([() => isApprovalAdding.value, () => addApprovalError.value], async (newVal) => {
+  if (newVal) {
+    addSuccessToast('Claim approved successfully')
+  }
+})
+watch(addApprovalError, (newVal) => {
+  if (newVal) {
+    addErrorToast(addApprovalError.value)
+  }
+})
+
+const approveClaim = async (claim: ClaimResponse) => {
+  loadingApprove.value = true
+  const provider = await web3Library.getProvider()
+  const signer = await web3Library.getSigner()
+  const chainId = (await provider.getNetwork()).chainId
+  const verifyingContract = team.value.cashRemunerationEip712Address
+
+  const domain = {
+    name: 'CashRemuneration',
+    version: '1',
+    chainId, //: 31337n,
+    verifyingContract //: '0x6DcBc91229d812910b54dF91b5c2b592572CD6B0'
+  }
+
+  const types = {
+    WageClaim: [
+      { name: 'employeeAddress', type: 'address' },
+      { name: 'hoursWorked', type: 'uint8' },
+      { name: 'hourlyRate', type: 'uint256' },
+      { name: 'date', type: 'uint256' }
+    ]
+  }
+
+  const data: WageClaim = {
+    hourlyRate: claim.hourlyRate,
+    hoursWorked: claim.hoursWorked,
+    employeeAddress: claim.address,
+    date: Math.floor((new Date(claim.createdAt)).getTime() / 1000)
+  }
+
+  try {
+    const signature = await signer.signTypedData(domain, types, data)
+    approvalData.value = {
+      id: claim.id,
+      signature
+    }
+    await addApprovalAPI()
+    // emits('getTeam')
+  } catch (err) {
+    log.error(parseError(err))
+    addErrorToast(parseError(err))
+  } finally {
+    loadingApprove.value = false
+  }
+}
 
 onMounted(async () => {
   await getWageClaimsAPI()
