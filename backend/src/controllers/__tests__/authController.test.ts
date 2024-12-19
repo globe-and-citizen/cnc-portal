593ca6aaac2b7e27cd9db66db4254cd508a9f48b
aliassan <alimkwananzi@gmail.com>
2024-12-19 14:26:40 +0200

    test: auth controller

---
+++ backend/src/controllers/__tests__/authController.test.ts
@@ -0,0 +1,126 @@
+import request from 'supertest'
+import express, { Request, Response, NextFunction } from 'express'
+import { 
+  authenticateSiwe,
+  authenticateToken
+} from '../authController'
+import { prisma } from "../../utils";
+import { errorResponse, extractAddressAndNonce } from "../../utils/utils";
+import jwt from "jsonwebtoken";
+import { describe, it, beforeEach, expect, vi } from 'vitest'
+
+function setAddressMiddleware(address: string) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    (req as any).address = address
+    next()
+  }
+}
+
+describe('authController', () => {
+  describe('POST: /siwe', () => {
+    const mockUser = {
+      name: `Mock User`,
+      address: `0x123`,
+      nonce: null
+    }
+    beforeEach(() => {
+      vi.clearAllMocks()
+    })
+    it('should return 401 if message not set', async () => {
+      const app = express()
+      app.use(express.json())
+      app.post('/siwe', authenticateSiwe)
+      vi.spyOn(prisma.user, 'findUnique').mockResolvedValue(null)
+
+      const response = await request(app)
+        .post('/siwe')
+
+      expect(response.status).toBe(401)
+      expect(response.body).toEqual({
+        message: 'Auth error: Missing message',
+        success: false
+      })
+    })
+    it('should return 401 if signature not set', async () => {
+      const app = express()
+      app.use(express.json())
+      app.post('/siwe', authenticateSiwe)
+      vi.spyOn(prisma.user, 'findUnique').mockResolvedValue(null)
+
+      const response = await request(app)
+        .post('/siwe')
+        .send({
+          message: `A message`
+        })
+
+      expect(response.status).toBe(401)
+      expect(response.body).toEqual({
+        message: 'Auth error: Missing signature',
+        success: false
+      })
+    })
+    it('should return 401 if SIWE verification fails', async () => {
+      const app = express()
+      app.use(express.json())
+      app.post('/siwe', authenticateSiwe)
+      vi.spyOn(prisma.user, 'findUnique').mockResolvedValue(null)
+      vi.spyOn(prisma.user, 'update').mockResolvedValue({...mockUser, nonce: '123abc'})
+      vi.spyOn(prisma.user, 'create')
+      vi.spyOn(jwt, 'sign').mockImplementation(() => 'jsonWebToken')
+
+      const response = await request(app)
+        .post('/siwe')
+        .send({
+          "message": `localhost:5173 wants you to sign in with your Ethereum account:\n0x70997970C51812dc3A010C7d01b50e0d17dc79C8\n\nSign in with Ethereum to the app.\n\nURI: http://localhost:5173\nVersion: 1\nChain ID: 1\nNonce: BuEqovAcm4cRvRHlx\nIssued At: 2024-12-18T11:57:47.715Z`,
+          "signature": "0x162ef821f3a9fbd0d38fcad0d6f19014d031767944fe8d686166f08ce4328eda3eace9c0d57fbb0fcdb276005a3429ed54e75f67f1b0049f55ba71b646775f9f1b"
+        })
+
+      expect(response.status).toBe(401)
+      expect(response.body).toEqual({
+        message: 'Signature does not match address of the message.',
+        success: false
+      })      
+    })
+    it('should return 200 if authentication successful', async () => {
+      const app = express()
+      app.use(express.json())
+      app.post('/siwe', authenticateSiwe)
+      vi.spyOn(prisma.user, 'findUnique').mockResolvedValue(null)
+      vi.spyOn(prisma.user, 'update').mockResolvedValue({...mockUser, nonce: '123abc'})
+      vi.spyOn(prisma.user, 'create')
+      vi.spyOn(jwt, 'sign').mockImplementation(() => 'jsonWebToken')
+
+      const response = await request(app)
+        .post('/siwe')
+        .send({
+          "message": `localhost:5173 wants you to sign in with your Ethereum account:\n0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266\n\nSign in with Ethereum to the app.\n\nURI: http://localhost:5173\nVersion: 1\nChain ID: 1\nNonce: BuEqovAcm4cRvRHlx\nIssued At: 2024-12-18T11:57:47.715Z`,
+          "signature": "0x162ef821f3a9fbd0d38fcad0d6f19014d031767944fe8d686166f08ce4328eda3eace9c0d57fbb0fcdb276005a3429ed54e75f67f1b0049f55ba71b646775f9f1b"
+        })
+
+      expect(response.status).toBe(200)
+      expect(response.body).toEqual({
+        accessToken: 'jsonWebToken',
+        success: true
+      })
+    })
+    it('It should return 500 if internal server error occurs', async () => {
+      const app = express()
+      app.use(express.json())
+      app.post('/siwe', authenticateSiwe)
+
+      const response = await request(app)
+        .post('/siwe')
+        .send({
+          "message": `Test message`,
+          "signature": `0xSignature`
+        })
+
+      expect(response.status).toBe(500)
+      expect(response.body).toEqual({
+        error: "Extract address error: Eth address missing ",
+        message: "Internal server error has occured",
+        success: false
+      })
+    })
+  })
+})
