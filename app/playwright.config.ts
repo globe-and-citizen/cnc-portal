ad9dd1cf647c51ec5217807eb1b1c3f8e2a70f9f
farrellh1 <farrell.hauzan@gmail.com>
2025-01-01 17:19:47 +0700

    chore: update config

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -36,10 +36,5 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
     }
-  ],
-
-  webServer: {
-    command: 'npm run dev',
-    port: 5173
-  }
+  ]
 })
