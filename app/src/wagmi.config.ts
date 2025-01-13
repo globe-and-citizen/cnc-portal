8ca9b86af84a643f059422886dda2e07f8c22447
aliassan <alimkwananzi@gmail.com>
2025-01-13 18:18:51 +0200

    fix: remove metaMask connector

--- app/src/wagmi.config.ts
+++ app/src/wagmi.config.ts
@@ -1,10 +1,8 @@
 import { http, createConfig } from '@wagmi/vue'
 import { mainnet, sepolia, polygon, hardhat } from '@wagmi/vue/chains'
-import { metaMask } from '@wagmi/vue/connectors';
 
 export const config = createConfig({
   chains: [mainnet, sepolia, polygon, hardhat],
-  connectors: [metaMask()],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http('https://sepolia.drpc.org'),
