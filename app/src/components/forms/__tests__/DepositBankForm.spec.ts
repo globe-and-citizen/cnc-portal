4fdc8b9b66c2c0050b23e909caf8a0e1895713ec
Hermann EHO <hermannleboss@gmail.com>
2025-01-16 10:01:02 +0000

    refactor: remove unused import from DepositBankForm test file

--- app/src/components/forms/__tests__/DepositBankForm.spec.ts
+++ app/src/components/forms/__tests__/DepositBankForm.spec.ts
@@ -2,7 +2,6 @@ import { mount } from '@vue/test-utils'
 import { describe, it, expect } from 'vitest'
 import DepositBankForm from '@/components/forms/DepositBankForm.vue'
 import ButtonUI from '@/components/ButtonUI.vue'
-import { NETWORK } from '@/constant'
 
 describe('DepositBankModal.vue', () => {
   describe('render', () => {
