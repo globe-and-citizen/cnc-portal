a45ed9e91e3f9809e8b5f86fd8ffc3f03f68b01d
aliassan <alimkwananzi@gmail.com>
2025-01-22 14:07:38 +0200

    feat: add wallet checks composable

---
+++ app/src/composables/useWalletChecks.ts
@@ -0,0 +1,78 @@
+import { ref } from 'vue'
+import { useToastStore } from '@/stores'
+import {
+  useConnect,
+  useSwitchChain,
+  useAccount,
+  type Connector,
+  type CreateConnectorFn
+} from '@wagmi/vue'
+import { NETWORK } from '@/constant'
+import { log, parseError } from '@/utils'
+
+export function useWalletChecks() {
+  const { addErrorToast } = useToastStore()
+  const { connectors, connect } = useConnect()
+  const { switchChain } = useSwitchChain()
+  const { isConnected } = useAccount()
+
+  const isProcessing = ref(false)
+
+  async function validateMetaMask() {
+    const metaMaskConnector = connectors.find(
+      (connector) => connector.name.split(' ')[0] === 'MetaMask'
+    )
+
+    if (!metaMaskConnector) {
+      addErrorToast('MetaMask is not installed. Please install MetaMask to continue.')
+      return false
+    }
+
+    return metaMaskConnector
+  }
+
+  async function validateNetwork(metaMaskConnector: Connector<CreateConnectorFn>) {
+    const networkChainId = parseInt(NETWORK.chainId)
+
+    if (!isConnected.value) {
+      connect({ connector: metaMaskConnector, chainId: networkChainId })
+    }
+
+    const currentChainId = await metaMaskConnector.getChainId()
+    if (currentChainId !== networkChainId) {
+      switchChain({
+        chainId: networkChainId,
+        connector: metaMaskConnector
+      })
+    }
+
+    return true
+  }
+
+  async function performChecks() {
+    try {
+      isProcessing.value = true
+
+      const metaMaskConnector = await validateMetaMask()
+      if (!metaMaskConnector) {
+        isProcessing.value = false
+        return false
+      }
+
+      const networkValid = await validateNetwork(metaMaskConnector)
+      if (!networkValid) {
+        isProcessing.value = false
+        return false
+      }
+      console.log(`completed checks...`)
+      return true
+    } catch (error) {
+      addErrorToast('Failed to validate wallet and network.')
+      log.error('performChecks.catch', parseError(error))
+      isProcessing.value = false
+      return false
+    }
+  }
+
+  return { isProcessing, performChecks }
+}
