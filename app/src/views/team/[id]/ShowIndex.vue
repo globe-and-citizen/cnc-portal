36d43833ce3245aa701839d9c0b1f058abef091f
Hermann EHO <hermannleboss@gmail.com>
2025-01-31 06:49:27 +0000

    feat: add ShowIndex component for team detail view with navigation

---
+++ app/src/views/team/[id]/ShowIndex.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- Navigation and breadcrumb -->
+  <div>
+    <h2>{{ route.meta.name }}</h2>
+    <div class="breadcrumbs text-sm">
+      <ul>
+        <li>
+          <a v-if="teamStore.currentTeam">{{ teamStore.currentTeam?.name }}</a>
+          <div class="skeleton h-4 w-20" v-else></div>
+        </li>
+
+        <li>{{ route.meta.name }}</li>
+      </ul>
+    </div>
+    <router-view />
+  </div>
+</template>
+<script setup lang="ts">
+import { useTeamStore } from '@/stores/teamStore'
+import { watch } from 'fs'
+import { computed, onMounted, onUpdated, watchEffect } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+const teamStore = useTeamStore()
+
+const router = useRouter()
+const route = useRoute()
+
+onMounted(() => {
+  console.log('Mounted')
+  teamStore.setCurrentTeamId(route.params.id as string)
+})
+</script>
+<style>
+* {
+  /* border: 1px solid red; */
+}
+</style>
