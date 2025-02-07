ebccd4a1842ca008be5d3a3b4b4349bb73fe20c4
Hermann EHO <hermannleboss@gmail.com>
2025-02-07 10:56:10 +0000

    refactor: clean up UserComponent template for improved readability

--- app/src/components/UserComponent.vue
+++ app/src/components/UserComponent.vue
@@ -1,32 +1,29 @@
 <template>
-  
   <div
-      class="flex flex-row justify-start gap-4 transition-all duration-300 shadow-sm rounded-xl"
-      :class="{ 'justify-center': isCollapsed }"
-    >
-      <div role="button" class="relative group">
-        <div class="relative rounded-full overflow-hidden w-11 h-11 ring-2 ring-white/50">
-          <img
-            alt="User Avatar"
-            :src="
-              user.avatarUrl ||
-              'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
-            "
-            class="w-full h-full object-cover"
-          />
-        </div>
-      </div>
-      <div class="flex flex-col text-gray-600" v-if="!isCollapsed">
-        <p class="font-bold text-sm line-clamp-1" data-test="user-name">
-          {{ user.name || 'User' }}
-        </p>
-        <p class="text-sm" data-test="formatted-address">
-          {{ formatedUserAddress }}
-        </p>
+    class="flex flex-row justify-start gap-4 transition-all duration-300 shadow-sm rounded-xl"
+    :class="{ 'justify-center': isCollapsed }"
+  >
+    <div role="button" class="relative group">
+      <div class="relative rounded-full overflow-hidden w-11 h-11 ring-2 ring-white/50">
+        <img
+          alt="User Avatar"
+          :src="
+            user.avatarUrl ||
+            'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
+          "
+          class="w-full h-full object-cover"
+        />
       </div>
     </div>
-
-
+    <div class="flex flex-col text-gray-600" v-if="!isCollapsed">
+      <p class="font-bold text-sm line-clamp-1" data-test="user-name">
+        {{ user.name || 'User' }}
+      </p>
+      <p class="text-sm" data-test="formatted-address">
+        {{ formatedUserAddress }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
