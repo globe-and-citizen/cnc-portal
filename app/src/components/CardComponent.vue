7f3b660ca34a4759d2d7f2da7766bc85b4022b8d
Hermann EHO <hermannleboss@gmail.com>
2025-02-18 16:14:47 +0000

    feat: add CardComponent with template and button integration

---
+++ app/src/components/CardComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card bg-base-100 shadow-lg">
+    <div class="card-body">
+      <div class="flex justify-between items-center">
+        <span class="card-title">Card Title</span>
+        <div class="card-actions justify-end">
+          <ButtonUI variant="primary">Card Action Button</ButtonUI>
+        </div>
+      </div>
+      <div class="card-content">Lorem Ipsum</div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import ButtonUI from './ButtonUI.vue'
+</script>
+
+<style scoped>
+</style>
