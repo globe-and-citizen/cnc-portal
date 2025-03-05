6671de3b5ef0bc5505c80fc472e9d7e0cf607ba0
aliassan <alimkwananzi@gmail.com>
2025-03-05 11:08:55 +0200

    chore: remove unused

--- app/src/components/sections/ExpenseAccountView/TransactionHistorySection.vue
+++ app/src/components/sections/ExpenseAccountView/TransactionHistorySection.vue
@@ -12,13 +12,12 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
+import { computed, watch } from 'vue'
 import GenericTransactionHistory from '@/components/GenericTransactionHistory.vue'
 import type { ExpenseTransaction, BaseTransaction } from '@/types/transactions'
 import { useQuery } from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 import { formatEtherUtil, log, tokenSymbol } from '@/utils'
-import { formatEther } from 'viem'
 
 interface Props {
   currencyRates: {
@@ -30,30 +29,30 @@ interface Props {
 
 defineProps<Props>()
 
-const transactions = ref<ExpenseTransaction[]>([
-  {
-    txHash: '0xfc9fc4e2c32197c0868a96134b027755e5f7eacb88ffdb7c8e70a27f38d5b55e',
-    date: Date.now(),
-    type: 'deposit',
-    from: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
-    to: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
-    amountUSD: 10,
-    amount: '0.01',
-    token: 'POL'
-  },
-  {
-    txHash: '0xfc9fc4e2c32197c0868a96134b027755e5f7eacb88ffdb7c8e70a27f38d5b55f',
-    date: Date.now(),
-    type: 'transfer',
-    from: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
-    to: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
-    amountUSD: 10,
-    amount: '0.01',
-    token: 'POL'
-  }
-])
+// const transactions = ref<ExpenseTransaction[]>([
+//   {
+//     txHash: '0xfc9fc4e2c32197c0868a96134b027755e5f7eacb88ffdb7c8e70a27f38d5b55e',
+//     date: Date.now(),
+//     type: 'deposit',
+//     from: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+//     to: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+//     amountUSD: 10,
+//     amount: '0.01',
+//     token: 'POL'
+//   },
+//   {
+//     txHash: '0xfc9fc4e2c32197c0868a96134b027755e5f7eacb88ffdb7c8e70a27f38d5b55f',
+//     date: Date.now(),
+//     type: 'transfer',
+//     from: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+//     to: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+//     amountUSD: 10,
+//     amount: '0.01',
+//     token: 'POL'
+//   }
+// ])
 
-const { result, loading, error } = useQuery(gql`
+const { result, error } = useQuery(gql`
   query GetTransfers {
     transfers {
       id
