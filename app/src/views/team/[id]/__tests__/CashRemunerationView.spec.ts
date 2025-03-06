975e0d3661c98d65b5f504b84296fe4ef7594d15
aliassan <alimkwananzi@gmail.com>
2025-03-06 15:57:00 +0200

    test: cash remuneration

--- app/src/views/team/[id]/__tests__/CashRemunerationView.spec.ts
+++ app/src/views/team/[id]/__tests__/CashRemunerationView.spec.ts
@@ -1,8 +1,10 @@
 import { describe, expect, it, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import CashRemunerationView from '@/views/team/[id]/CashRemunerationView.vue'
+import GenericTokenHoldingsSection from '@/components/GenericTokenHoldingsSection.vue'
 import { createTestingPinia } from '@pinia/testing'
 import { ref } from 'vue'
+import { parseEther } from 'viem'
 
 vi.mock('@/stores/useToastStore')
 vi.mock('vue-router', async (importOriginal) => {
@@ -34,6 +36,53 @@ vi.mock('@/composables/useClaim', () => {
     }))
   }
 })
+// Add mock for teamStore
+const mockTeamStore = {
+  currentTeam: {
+    cashRemunerationEip712Address: '0x123',
+    id: '1',
+    name: 'Test Team'
+  },
+  setCurrentTeamId: vi.fn()
+}
+
+vi.mock('@/stores', async (importOriginal) => {
+  const actual: object = await importOriginal()
+  return {
+    ...actual,
+    useTeamStore: vi.fn(() => mockTeamStore),
+    useToastStore: vi.fn(() => ({ addErrorToast: vi.fn() }))
+  }
+})
+
+const mockUseBalance = {
+  data: ref({
+    decimals: 18,
+    formatted: `100`,
+    symbol: `SepoliaETH`,
+    value: parseEther(`100`)
+  }),
+  refetch: vi.fn(),
+  error: ref<Error | null>(null),
+  isLoading: ref(false)
+}
+
+const mockUseReadContract = {
+  data: ref(BigInt(20000 * 1e6)),
+  refetch: vi.fn(),
+  error: ref<Error | null>(null),
+  isLoading: ref(false)
+}
+
+vi.mock('@wagmi/vue', async (importOriginal) => {
+  const original: object = await importOriginal()
+  return {
+    ...original,
+    useBalance: vi.fn(() => ({ ...mockUseBalance })),
+    useReadContract: vi.fn(() => ({ ...mockUseReadContract })),
+    useChainId: vi.fn(() => ref(1))
+  }
+})
 
 describe('CashRemunerationView.vue', () => {
   const createComponent = () => {
@@ -50,5 +99,16 @@ describe('CashRemunerationView.vue', () => {
     await input.setValue('10')
 
     expect((input.element as HTMLInputElement).value).toBe('10')
+  })
+  it('should pass correct props to GenericTokenHoldingsSection', () => {
+    const wrapper = createComponent()
+    const genericTokenHoldingSection = wrapper.findComponent({
+      name: 'GenericTokenHoldingsSection'
+    })
+
+    expect(genericTokenHoldingSection.exists()).toBeTruthy()
+    expect(genericTokenHoldingSection.props('address')).toBe(
+      mockTeamStore.currentTeam.cashRemunerationEip712Address
+    )
   })
 })
