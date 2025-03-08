398817771a1a23b44b6b2b884110cf1a2f5d7ed7
Hermann EHO <hermannleboss@gmail.com>
2025-03-08 14:53:06 +0000

    refactor: update TeamMetaComponent to use isSelected prop and adjust layout

--- app/src/components/TeamMetaComponent.vue
+++ app/src/components/TeamMetaComponent.vue
@@ -1,23 +1,22 @@
 <template>
-  <div class="p-4 flex gap-4 border-b-2">
+  <div class="p-4 flex gap-4 border-b-2 items-center text-sm">
     <p class="text-xl font-black w-11 h-11 bg-red-200 flex items-center justify-center rounded-lg">
       {{ team.name.charAt(0) }}
     </p>
-    <div>
+    <div class="pr-8">
       <p class="min-w-max font-semibold">{{ team.name }}</p>
       <p class="min-w-max text-slate-400">3 Team Members</p>
     </div>
-    <CheckIcon class="size-6" v-if="false" />
+    <CheckIcon class="size-6" v-if="isSelected" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { CheckIcon } from '@heroicons/vue/24/outline'
+import { CheckIcon } from '@heroicons/vue/24/solid'
 import type { Team } from '@/types'
 
 defineProps<{
-  team: Team
+  team: Team,
+  isSelected: boolean
 }>()
 </script>
-
-<style scoped></style>
