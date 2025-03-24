b4686ab5713e1ed9c0eb0f6456d524a718097c9b
Dasarath G <dasarathg68@gmail.com>
2025-03-24 23:05:43 +0530

    refactor: bank event handlers and update subgraph configuration - Changed the network configuration for the ExpenseAccountEIP712 and Bank templates to 'localhost'. - Refactored event handlers in bank.ts to streamline transaction creation and removed the getOrCreateTransaction utility function.

--- the-graph/src/bank.ts
+++ the-graph/src/bank.ts
@@ -1,173 +1,115 @@
-import { BigInt, Bytes, Address } from "@graphprotocol/graph-ts";
+import { Address } from "@graphprotocol/graph-ts";
 import {
-  Transaction
+  SendTip,
+  SendTokenTip,
+  TipsAddressChanged,
+  TokenAddressChanged,
+  Transaction,
 } from "../generated/schema";
 import {
   Transfer as TransferEvent,
   Deposited as DepositedEvent,
   TokenTransfer as TokenTransferEvent,
   TokenDeposited as TokenDepositedEvent,
+  PushTip as PushTipEvent,
+  SendTip as SendTipEvent,
+  SendTokenTip as SendTokenTipEvent,
+  TipsAddressChanged as TipsAddressChangedEvent,
+  TokenAddressChanged as TokenAddressChangedEvent,
 } from "../generated/templates/Bank/Bank";
-import { getOrCreateTransaction } from "./utils";
 
 export function handleTransfer(event: TransferEvent): void {
-  let id = event.transaction.hash.concatI32(event.logIndex.toI32());
-  let transfer = new Transfer(id);
-  transfer.from = event.params.sender;
-  transfer.to = event.params.to;
-  transfer.contractType = "Bank";
-  transfer.tokenAddress = Bytes.fromHexString(
-    "0x0000000000000000000000000000000000000000"
-  ); // ETH
-  transfer.amount = event.params.amount;
-  transfer.contractAddress = event.address;
-  transfer.blockNumber = event.block.number;
-  transfer.blockTimestamp = event.block.timestamp;
-  transfer.transactionHash = event.transaction.hash;
-  transfer.save();
-
-  getOrCreateTransaction(
-    event.transaction.hash,
-    event.logIndex,
-    event.params.sender,
-    event.params.to,
-    "Bank",
-    Bytes.fromHexString("0x0000000000000000000000000000000000000000"),
-    event.params.amount,
-    event.address,
-    "transfer"
+  let entity = new Transaction(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
   );
+  entity.from = event.params.sender;
+  entity.to = event.params.to;
+  entity.amount = event.params.amount;
+
+  entity.contractType = "Bank";
+  entity.contractAddress = event.address;
+  entity.transactionType = "transfer";
+  entity.tokenAddress = Address.zero();
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
 
 export function handleDeposited(event: DepositedEvent): void {
-  let id = event.transaction.hash.concatI32(event.logIndex.toI32());
-  let deposited = new Deposited(id);
-  deposited.from = event.params.depositor;
-  deposited.to = event.address;
-  deposited.contractType = "Bank";
-  deposited.amount = event.params.amount;
-  deposited.contractAddress = event.address;
-  deposited.blockNumber = event.block.number;
-  deposited.blockTimestamp = event.block.timestamp;
-  deposited.transactionHash = event.transaction.hash;
-  deposited.save();
-
-  getOrCreateTransaction(
-    event.transaction.hash,
-    event.logIndex,
-    event.params.depositor,
-    event.address,
-    "Bank",
-    Bytes.fromHexString("0x0000000000000000000000000000000000000000"),
-    event.params.amount,
-    event.address,
-    "deposit"
+  let entity = new Transaction(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
   );
-}
+  entity.from = event.params.depositor;
+  entity.to = event.address;
+  entity.amount = event.params.amount;
 
-export function handleTokenTransfer(event: TokenTransferEvent): void {
-  let id = event.transaction.hash.concatI32(event.logIndex.toI32());
-  let transfer = new TokenTransfer(id);
-  transfer.from = event.params.sender;
-  transfer.to = event.params.to;
-  transfer.token = event.params.token;
-  transfer.amount = event.params.amount;
-  transfer.contractAddress = event.address;
-  transfer.blockNumber = event.block.number;
-  transfer.blockTimestamp = event.block.timestamp;
-  transfer.transactionHash = event.transaction.hash;
-  transfer.save();
+  entity.contractType = "Bank";
+  entity.contractAddress = event.address;
+  entity.transactionType = "deposit";
+  entity.tokenAddress = Address.zero();
 
-  getOrCreateTransaction(
-    event.transaction.hash,
-    event.logIndex,
-    event.params.sender,
-    event.params.to,
-    "Bank",
-    event.params.token,
-    event.params.amount,
-    event.address,
-    "transfer"
-  );
-}
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-export function handleTokenDeposited(event: TokenDepositedEvent): void {
-  let id = event.transaction.hash.concatI32(event.logIndex.toI32());
-  let deposited = new TokenDeposited(id);
-  deposited.depositor = event.params.depositor;
-  deposited.token = event.params.token;
-  deposited.amount = event.params.amount;
-  deposited.contractAddress = event.address;
-  deposited.blockNumber = event.block.number;
-  deposited.blockTimestamp = event.block.timestamp;
-  deposited.transactionHash = event.transaction.hash;
-  deposited.save();
-
-  getOrCreateTransaction(
-    event.transaction.hash,
-    event.logIndex,
-    event.params.depositor,
-    event.address,
-    "Bank",
-    event.params.token,
-    event.params.amount,
-    event.address,
-    "deposit"
-  );
+  entity.save();
 }
 
 export function handlePushTip(event: PushTipEvent): void {
-  let id = event.transaction.hash.concatI32(event.logIndex.toI32());
-  let pushTip = new PushTip(id);
-  pushTip.addressWhoPushes = event.params.addressWhoPushes;
-  pushTip.teamMembers = event.params.teamMembers;
-  pushTip.totalAmount = event.params.totalAmount;
-  pushTip.contractAddress = event.address;
-  pushTip.blockNumber = event.block.number;
-  pushTip.blockTimestamp = event.block.timestamp;
-  pushTip.transactionHash = event.transaction.hash;
-  pushTip.save();
+  let entity = new Transaction(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.from = event.params.addressWhoPushes;
+  entity.to = event.address;
+  entity.amount = event.params.totalAmount;
+
+  entity.contractType = "Bank";
+  entity.contractAddress = event.address;
+  entity.transactionType = "pushTip";
+  entity.tokenAddress = Address.zero();
 }
 
-export function handlePushTokenTip(event: PushTokenTipEvent): void {
-  let id = event.transaction.hash.concatI32(event.logIndex.toI32());
-  let pushTokenTip = new PushTokenTip(id);
-  pushTokenTip.addressWhoPushes = event.params.addressWhoPushes;
-  pushTokenTip.teamMembers = event.params.teamMembers;
-  pushTokenTip.token = event.params.token;
-  pushTokenTip.totalAmount = event.params.totalAmount;
-  pushTokenTip.contractAddress = event.address;
-  pushTokenTip.blockNumber = event.block.number;
-  pushTokenTip.blockTimestamp = event.block.timestamp;
-  pushTokenTip.transactionHash = event.transaction.hash;
-  pushTokenTip.save();
+export function handleTokenTransfer(event: TokenTransferEvent): void {
+  let entity = new Transaction(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.from = event.params.sender;
+  entity.to = event.params.to;
+  entity.amount = event.params.amount;
+
+  entity.contractType = "Bank";
+  entity.contractAddress = event.address;
+  entity.transactionType = "transfer";
+  entity.tokenAddress = event.params.token;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
 
-export function handleSendTip(event: SendTipEvent): void {
-  let id = event.transaction.hash.concatI32(event.logIndex.toI32());
-  let sendTip = new SendTip(id);
-  sendTip.addressWhoSends = event.params.addressWhoSends;
-  sendTip.teamMembers = event.params.teamMembers;
-  sendTip.totalAmount = event.params.totalAmount;
-  sendTip.contractAddress = event.address;
-  sendTip.blockNumber = event.block.number;
-  sendTip.blockTimestamp = event.block.timestamp;
-  sendTip.transactionHash = event.transaction.hash;
-  sendTip.save();
-}
+export function handleTokenDeposited(event: TokenDepositedEvent): void {
+  let entity = new Transaction(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.from = event.params.depositor;
+  entity.to = event.address;
+  entity.amount = event.params.amount;
 
-export function handleSendTokenTip(event: SendTokenTipEvent): void {
-  let id = event.transaction.hash.concatI32(event.logIndex.toI32());
-  let sendTokenTip = new SendTokenTip(id);
-  sendTokenTip.addressWhoSends = event.params.addressWhoSends;
-  sendTokenTip.teamMembers = event.params.teamMembers;
-  sendTokenTip.token = event.params.token;
-  sendTokenTip.totalAmount = event.params.totalAmount;
-  sendTokenTip.contractAddress = event.address;
-  sendTokenTip.blockNumber = event.block.number;
-  sendTokenTip.blockTimestamp = event.block.timestamp;
-  sendTokenTip.transactionHash = event.transaction.hash;
-  sendTokenTip.save();
+  entity.contractType = "Bank";
+  entity.contractAddress = event.address;
+  entity.transactionType = "deposit";
+  entity.tokenAddress = event.params.token;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
 
 export function handleTipsAddressChanged(event: TipsAddressChangedEvent): void {
