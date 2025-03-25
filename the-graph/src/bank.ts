0cb49c471a9f7d371dc54e35129cbca08b42ddac
Dasarath G <dasarathg68@gmail.com>
2025-03-25 15:30:32 +0530

    refactor: simplify bank event handlers by removing unused functions and imports - Removed unused event handlers for PushTip, TipsAddressChanged, and TokenAddressChanged. - Streamlined imports in bank.ts to include only necessary components for transaction handling.

--- the-graph/src/bank.ts
+++ the-graph/src/bank.ts
@@ -1,21 +1,10 @@
 import { Address } from "@graphprotocol/graph-ts";
-import {
-  SendTip,
-  SendTokenTip,
-  TipsAddressChanged,
-  TokenAddressChanged,
-  Transaction,
-} from "../generated/schema";
+import { Transaction } from "../generated/schema";
 import {
   Transfer as TransferEvent,
   Deposited as DepositedEvent,
   TokenTransfer as TokenTransferEvent,
   TokenDeposited as TokenDepositedEvent,
-  PushTip as PushTipEvent,
-  SendTip as SendTipEvent,
-  SendTokenTip as SendTokenTipEvent,
-  TipsAddressChanged as TipsAddressChangedEvent,
-  TokenAddressChanged as TokenAddressChangedEvent,
 } from "../generated/templates/Bank/Bank";
 
 export function handleTransfer(event: TransferEvent): void {
@@ -58,20 +47,6 @@ export function handleDeposited(event: DepositedEvent): void {
   entity.save();
 }
 
-export function handlePushTip(event: PushTipEvent): void {
-  let entity = new Transaction(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.from = event.params.addressWhoPushes;
-  entity.to = event.address;
-  entity.amount = event.params.totalAmount;
-
-  entity.contractType = "Bank";
-  entity.contractAddress = event.address;
-  entity.transactionType = "pushTip";
-  entity.tokenAddress = Address.zero();
-}
-
 export function handleTokenTransfer(event: TokenTransferEvent): void {
   let entity = new Transaction(
     event.transaction.hash.concatI32(event.logIndex.toI32())
@@ -110,33 +85,4 @@ export function handleTokenDeposited(event: TokenDepositedEvent): void {
   entity.transactionHash = event.transaction.hash;
 
   entity.save();
-}
-
-export function handleTipsAddressChanged(event: TipsAddressChangedEvent): void {
-  let id = event.transaction.hash.concatI32(event.logIndex.toI32());
-  let tipsAddressChanged = new TipsAddressChanged(id);
-  tipsAddressChanged.addressWhoChanged = event.params.addressWhoChanged;
-  tipsAddressChanged.oldAddress = event.params.oldAddress;
-  tipsAddressChanged.newAddress = event.params.newAddress;
-  tipsAddressChanged.contractAddress = event.address;
-  tipsAddressChanged.blockNumber = event.block.number;
-  tipsAddressChanged.blockTimestamp = event.block.timestamp;
-  tipsAddressChanged.transactionHash = event.transaction.hash;
-  tipsAddressChanged.save();
-}
-
-export function handleTokenAddressChanged(
-  event: TokenAddressChangedEvent
-): void {
-  let id = event.transaction.hash.concatI32(event.logIndex.toI32());
-  let tokenAddressChanged = new TokenAddressChanged(id);
-  tokenAddressChanged.addressWhoChanged = event.params.addressWhoChanged;
-  tokenAddressChanged.tokenSymbol = event.params.tokenSymbol;
-  tokenAddressChanged.oldAddress = event.params.oldAddress;
-  tokenAddressChanged.newAddress = event.params.newAddress;
-  tokenAddressChanged.contractAddress = event.address;
-  tokenAddressChanged.blockNumber = event.block.number;
-  tokenAddressChanged.blockTimestamp = event.block.timestamp;
-  tokenAddressChanged.transactionHash = event.transaction.hash;
-  tokenAddressChanged.save();
 }
