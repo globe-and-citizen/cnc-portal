a42eae4463f649361933336d15529f25be0ee3d9
Hermann EHO <hermannleboss@gmail.com>
2025-04-04 11:41:24 +0000

    chore: recreate migrations

--- backend/prisma/migrations/20250404113929_v4/migration.sql
+++ backend/prisma/migrations/20250404113929_v4/migration.sql
@@ -3,6 +3,8 @@ CREATE TABLE "User" (
     "address" TEXT NOT NULL,
     "name" TEXT,
     "nonce" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("address")
 );
@@ -13,14 +15,9 @@ CREATE TABLE "Team" (
     "ownerAddress" TEXT NOT NULL,
     "description" TEXT,
     "name" TEXT NOT NULL,
-    "bankAddress" TEXT,
-    "votingAddress" TEXT,
-    "boardOfDirectorsAddress" TEXT,
-    "expenseAccountAddress" TEXT,
     "officerAddress" TEXT,
-    "investorsAddress" TEXT,
-    "expenseAccountEip712Address" TEXT,
-    "cashRemunerationEip712Address" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
 );
@@ -32,20 +29,51 @@ CREATE TABLE "MemberTeamsData" (
     "teamId" INTEGER NOT NULL,
     "expenseAccountData" TEXT,
     "expenseAccountSignature" TEXT,
-    "hourlyRate" INTEGER,
-    "maxHoursPerWeek" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "MemberTeamsData_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
+CREATE TABLE "Expense" (
+    "id" SERIAL NOT NULL,
+    "userAddress" TEXT NOT NULL,
+    "teamId" INTEGER NOT NULL,
+    "signature" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Expense_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Wage" (
+    "id" SERIAL NOT NULL,
+    "teamId" INTEGER NOT NULL,
+    "userAddress" TEXT NOT NULL,
+    "cashRatePerHour" DOUBLE PRECISION NOT NULL,
+    "tokenRatePerHour" DOUBLE PRECISION NOT NULL,
+    "maximumHoursPerWeek" INTEGER NOT NULL,
+    "nextWageId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Wage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Claim" (
     "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "status" TEXT NOT NULL,
     "hoursWorked" INTEGER,
-    "cashRemunerationSignature" TEXT,
-    "memberTeamsDataId" INTEGER NOT NULL,
+    "signature" TEXT,
+    "tokenTx" TEXT,
+    "wageId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Claim_pkey" PRIMARY KEY ("id")
 );
@@ -57,9 +85,10 @@ CREATE TABLE "Notification" (
     "message" TEXT NOT NULL,
     "isRead" BOOLEAN NOT NULL DEFAULT false,
     "userAddress" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "author" TEXT,
     "resource" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
 );
@@ -70,8 +99,9 @@ CREATE TABLE "TeamContract" (
     "address" TEXT NOT NULL,
     "type" TEXT NOT NULL,
     "deployer" TEXT NOT NULL,
-    "admins" TEXT[],
     "teamId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "TeamContract_pkey" PRIMARY KEY ("id")
 );
@@ -87,6 +117,7 @@ CREATE TABLE "BoardOfDirectorActions" (
     "data" TEXT NOT NULL,
     "teamId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "BoardOfDirectorActions_pkey" PRIMARY KEY ("id")
 );
@@ -105,6 +136,12 @@ CREATE UNIQUE INDEX "Team_name_key" ON "Team"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "MemberTeamsData_userAddress_teamId_key" ON "MemberTeamsData"("userAddress", "teamId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Wage_nextWageId_key" ON "Wage"("nextWageId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Wage_teamId_userAddress_nextWageId_key" ON "Wage"("teamId", "userAddress", "nextWageId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "TeamContract_address_key" ON "TeamContract"("address");
@@ -128,7 +165,22 @@ ALTER TABLE "MemberTeamsData" ADD CONSTRAINT "MemberTeamsData_userAddress_fkey" 
 ALTER TABLE "MemberTeamsData" ADD CONSTRAINT "MemberTeamsData_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Claim" ADD CONSTRAINT "Claim_memberTeamsDataId_fkey" FOREIGN KEY ("memberTeamsDataId") REFERENCES "MemberTeamsData"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_userAddress_fkey" FOREIGN KEY ("userAddress") REFERENCES "User"("address") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Wage" ADD CONSTRAINT "Wage_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Wage" ADD CONSTRAINT "Wage_userAddress_fkey" FOREIGN KEY ("userAddress") REFERENCES "User"("address") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Wage" ADD CONSTRAINT "Wage_nextWageId_fkey" FOREIGN KEY ("nextWageId") REFERENCES "Wage"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Claim" ADD CONSTRAINT "Claim_wageId_fkey" FOREIGN KEY ("wageId") REFERENCES "Wage"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userAddress_fkey" FOREIGN KEY ("userAddress") REFERENCES "User"("address") ON DELETE RESTRICT ON UPDATE CASCADE;
