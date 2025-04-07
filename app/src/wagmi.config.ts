843ac01c64930f4c9d1cd22dac4c500021cbcf1f
Dasarath G <dasarathg68@gmail.com>
2025-04-07 16:59:13 +0530

    fix: adjust balance computation logic

--- app/src/wagmi.config.ts
+++ app/src/wagmi.config.ts
@@ -5,7 +5,7 @@ export const config = createConfig({
   chains: [mainnet, sepolia, polygon, hardhat, polygonAmoy],
   transports: {
     [mainnet.id]: http(),
-    [sepolia.id]: http('https://sepolia.drpc.org'),
+    [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/-LTvlR0N2abdEj4jbo-BkX_FUmZqwSc9'),
     [polygon.id]: http(),
     [polygonAmoy.id]: http(),
     [hardhat.id]: http()
