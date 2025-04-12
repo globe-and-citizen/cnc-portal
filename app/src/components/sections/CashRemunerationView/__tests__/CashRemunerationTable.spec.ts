51527d951d17b38414d7755f2fa0c31926993a79
farrellh1 <farrell.hauzan@gmail.com>
2025-04-12 15:47:49 +0700

    test: add tests

--- app/src/components/sections/CashRemunerationView/__tests__/CashRemunerationTable.spec.ts
+++ app/src/components/sections/CashRemunerationView/__tests__/CashRemunerationTable.spec.ts
@@ -1,32 +1,9 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import { describe, expect, it, vi } from 'vitest'
 import CashRemunerationTable from '../CashRemunerationTable.vue'
 import { createTestingPinia } from '@pinia/testing'
 import { ref } from 'vue'
-import { useUserDataStore } from '@/stores/__mocks__/user'
-import { useToastStore } from '@/stores/__mocks__/useToastStore'
 
-const signClaimMock = vi.fn()
-const withdrawMock = vi.fn()
-const withdrawSuccess = ref(false)
-const withdrawalLoading = ref(false)
-vi.mock('@/composables/useClaim', () => {
-  return {
-    useWithdrawClaim: vi.fn(() => ({
-      execute: withdrawMock,
-      isLoading: withdrawalLoading,
-      error: ref(undefined),
-      isSuccess: withdrawSuccess
-    })),
-    useSignWageClaim: vi.fn(() => ({
-      execute: signClaimMock,
-      isLoading: ref(false),
-      signature: ref(undefined)
-    }))
-  }
-})
-
-const statusCodeMock = ref<number | undefined>(undefined)
 const errorMock = ref<unknown>(undefined)
 const mockFetch = vi.fn()
 vi.mock('@/composables/useCustomFetch', async (importOriginal) => {
@@ -34,120 +11,91 @@ vi.mock('@/composables/useCustomFetch', async (importOriginal) => {
   return {
     ...original,
     useCustomFetch: vi.fn(() => ({
-      get: vi.fn(() => ({
-        json: vi.fn(() => ({
-          data: ref([
-            {
-              id: 1,
-              hourlyRate: '10',
-              hoursWorked: 20,
-              name: 'John Doe',
-              status: 'pending',
-              cashRemunerationSignature: null,
-              createdAt: '2021-09-01',
-              address: ' 0x123'
-            },
-            {
-              id: 2,
-              hourlyRate: '20',
-              hoursWorked: 10,
-              name: null,
-              status: 'approved',
-              cashRemunerationSignature: '0x123',
-              createdAt: '2021-09-02',
-              address: ' 0x456'
-            }
-          ]),
-          error: ref(undefined),
-          execute: mockFetch,
-          isFetching: ref(false)
-        }))
-      })),
-      post: vi.fn(() => ({
-        json: vi.fn(() => ({
-          data: ref({}),
-          error: ref(undefined),
-          execute: vi.fn(),
-          isFetching: ref(false)
-        }))
-      })),
       json: vi.fn(() => ({
-        data: ref({}),
-        error: ref(undefined),
-        execute: vi.fn(),
+        data: ref([
+          {
+            id: 1,
+            hoursWorked: 20,
+            wage: {
+              cashRatePerHour: 1,
+              user: {
+                address: '0x123',
+                name: 'John Doe'
+              }
+            },
+            status: 'pending',
+            cashRemunerationSignature: null,
+            createdAt: '2021-09-01',
+            address: ' 0x123'
+          },
+          {
+            id: 2,
+            hoursWorked: 10,
+            wage: {
+              cashRatePerHour: 1,
+              user: {
+                address: '0x123',
+                name: 'John Doe'
+              }
+            },
+            status: 'approved',
+            cashRemunerationSignature: '0x123',
+            createdAt: '2021-09-02',
+            address: ' 0x456'
+          }
+        ]),
+        error: errorMock,
+        execute: mockFetch,
         isFetching: ref(false)
-      })),
-      put: vi.fn(() => ({
-        json: vi.fn(() => ({
-          error: errorMock,
-          execute: vi.fn(),
-          loading: ref(false),
-          statusCode: statusCodeMock
-        }))
       }))
     }))
   }
 })
 
-vi.mock('@/stores/user')
-vi.mock('@/stores/useToastStore')
+const mockErrorToast = vi.fn()
+vi.mock('@/stores', async (importOriginal) => {
+  const original: object = await importOriginal()
+  return {
+    ...original,
+    useCurrencyStore: vi.fn(() => ({
+      currency: ref({
+        code: 'USD',
+        symbol: '$'
+      }),
+      nativeTokenPrice: ref(1000)
+    })),
+    useToastStore: vi.fn(() => ({
+      addErrorToast: mockErrorToast
+    }))
+  }
+})
 
 interface ComponentData {
-  withdrawLoading: {
-    [key: string]: boolean
-  }
+  statusUrl: string
 }
 
-describe.skip('CashRemunerationTable', () => {
+describe('CashRemunerationTable', () => {
   const createComponent = () => {
-    return mount(CashRemunerationTable, {
+    return shallowMount(CashRemunerationTable, {
       global: {
         plugins: [createTestingPinia({ createSpy: vi.fn })]
       },
-      props: {
-        ownerAddress: '0x123'
+      stubs: {
+        CRSigne: {
+          name: 'CRSigne',
+          template: '<div />'
+        },
+        CRWithdrawClaim: {
+          name: 'CRWithdrawClaim',
+          template: '<div />'
+        }
       }
     })
   }
-  it('should be able to see and click approve button when current user is the owner', async () => {
-    const userStore = useUserDataStore()
-    userStore.address = '0x123'
+
+  it('renders correctly', () => {
     const wrapper = createComponent()
-    const button = wrapper.find('[data-test="approve-button"]')
-
-    statusCodeMock.value = 200
-    expect(button.exists()).toBeTruthy()
-    await button.trigger('click')
-
-    expect(signClaimMock).toHaveBeenCalled()
-    expect(mockFetch).toHaveBeenCalled()
-  })
-
-  it('should be able to see and click withdraw button when current user is the claim owner', async () => {
-    const userStore = useUserDataStore()
-    userStore.address = '0x123'
-    const wrapper = createComponent()
-    const button = wrapper.find('[data-test="withdraw-button"]')
-
-    expect(button.exists()).toBeTruthy()
-    await button.trigger('click')
-
-    expect(withdrawMock).toHaveBeenCalled()
-    withdrawSuccess.value = true
-    await wrapper.vm.$nextTick()
-    expect(mockFetch).toHaveBeenCalled()
-  })
-
-  it('should show error if add approval error', async () => {
-    const { addErrorToast } = useToastStore()
-    const wrapper = createComponent()
-    const button = wrapper.find('[data-test="approve-button"]')
-
-    await button.trigger('click')
-    errorMock.value = new Error('Error')
-    await wrapper.vm.$nextTick()
-
-    expect(addErrorToast).toHaveBeenCalled()
+    expect(wrapper.exists()).toBeTruthy()
   })
 
   it('should emits fetchClaims when radio button changed', async () => {
@@ -158,14 +106,30 @@ describe.skip('CashRemunerationTable', () => {
     expect(mockFetch).toHaveBeenCalled()
   })
 
-  it('should set withdrawLoading correctly', async () => {
+  it('calls fetchTeamClaimData when SubmitClaims emits refetch-claims', async () => {
     const wrapper = createComponent()
-    const button = wrapper.find('[data-test="withdraw-button"]')
+    const submitClaimsStub = wrapper.findComponent({ name: 'SubmitClaims' })
 
-    await button.trigger('click')
-    withdrawalLoading.value = true
+    await submitClaimsStub.vm.$emit('refetch-claims')
+    expect(mockFetch).toHaveBeenCalled()
+  })
+  it('computes statusUrl correctly when selectedRadio is "all"', () => {
+    const wrapper = createComponent()
+    expect((wrapper.vm as unknown as ComponentData).statusUrl).toBe('')
+  })
+
+  it('computes statusUrl correctly when selectedRadio is "pending"', async () => {
+    const wrapper = createComponent()
+    const radio = wrapper.find('input[data-test="radio-pending"]')
+    await radio.setValue()
+    expect((wrapper.vm as unknown as ComponentData).statusUrl).toBe('&status=pending')
+  })
+
+  it('shows error toast when error occurs', async () => {
+    errorMock.value = new Error('Test error')
+    const wrapper = createComponent()
+
     await wrapper.vm.$nextTick()
-
-    expect((wrapper.vm as unknown as ComponentData).withdrawLoading['2']).toBe(true)
+    expect(mockErrorToast).toHaveBeenCalledWith('Failed to fetch team wage data')
   })
 })
