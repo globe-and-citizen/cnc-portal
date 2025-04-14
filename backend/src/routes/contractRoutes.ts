7fa53592e61725985ffac2563a1bee993df780d4
Hermann EHO <hermannleboss@gmail.com>
2025-04-14 15:11:25 +0000

    refactor: remove contract routes from teamRoutes

---
+++ backend/src/routes/contractRoutes.ts
@@ -0,0 +1,200 @@
+import express from "express";
+
+import {
+  getContracts,
+  syncContracts,
+  addContract,
+} from "../controllers/contractController";
+
+const contractRoutes = express.Router();
+
+/**
+ * @openapi
+ * /contract:
+ *  post:
+ *   summary: Add a new contract to a team
+ *   requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *             teamId:
+ *               type: integer
+ *               description: The ID of the team
+ *               minimum: 1
+ *             contractAddress:
+ *               type: string
+ *               description: The address of the contract
+ *             contractType:
+ *               type: string
+ *               description: The type of the contract
+ *   responses:
+ *     200:
+ *       description: Contract added successfully
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 description: The ID of the created contract
+ *     400:
+ *       description: Bad request
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 description: Error message
+ *     403:
+ *       description: Forbidden
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 description: Error message
+ *     500:
+ *       description: Internal server error
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 description: Error message
+ */
+contractRoutes.post("/contract", addContract);
+
+/**
+ * @openapi
+ * /contract:
+ *  get:
+ *   summary: Retrieve contracts for a specific team
+ *   parameters:
+ *     - in: query
+ *       name: teamId
+ *       required: true
+ *       schema:
+ *         type: integer
+ *         description: The ID of the team
+ *         minimum: 1
+ *   responses:
+ *     200:
+ *       description: List of contracts retrieved successfully
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: The ID of the contract
+ *                 address:
+ *                   type: string
+ *                   description: The address of the contract
+ *                 type:
+ *                   type: string
+ *                   description: The type of the contract
+ *     400:
+ *       description: Bad request
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 description: Error message
+ *     404:
+ *       description: Not found
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 description: Error message
+ *     500:
+ *       description: Internal server error
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 description: Error message
+ */
+contractRoutes.get("/contract", getContracts);
+
+/**
+ * @openapi
+ * /contract/sync:
+ *  put:
+ *   summary: Sync contracts for a specific team
+ *   requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *           type: object
+ *           properties:
+ *             teamId:
+ *               type: integer
+ *               description: The ID of the team
+ *               minimum: 1
+ *   responses:
+ *     200:
+ *       description: Contracts synced successfully
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               count:
+ *                 type: integer
+ *                 description: Number of contracts synced
+ *     400:
+ *       description: Bad request
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 description: Error message
+ *     403:
+ *       description: Forbidden
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 description: Error message
+ *     500:
+ *       description: Internal server error
+ *       content:
+ *         application/json
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 description: Error message
+ */
+contractRoutes.put("/contract/sync", syncContracts);
