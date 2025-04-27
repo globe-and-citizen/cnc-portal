a2cb61525ef42d9b39912f89452c3b2a7f08a642
farrellh1 <farrell.hauzan@gmail.com>
2025-04-27 17:41:38 +0700

    feat: add new summary expense approval

---
+++ app/src/components/forms/ApproveExpenseSummaryForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <h3 class="font-bold mb-4">
+    You are about to approve
+    {{
+      teamStore.currentTeam?.members.find((member) => {
+        console.log(teamStore.currentTeam?.members, budgetLimit.approvedAddress)
+        member.address == budgetLimit.approvedAddress
+      })?.name || budgetLimit.approvedAddress
+    }}
+    with the following limits:
+  </h3>
+
+  <h3 :key="limit.budgetType" v-for="limit in budgetLimit.budgetData">
+    {{ limitType(limit.budgetType as 0 | 1 | 2) }}:
+    <span class="font-bold">{{ limit.value }} {{ getToken(limit.budgetType as 0 | 1 | 2) }}</span>
+  </h3>
+
+  <h3 v-if="budgetLimit.expiry">
+    Valid Until:
+    <span class="font-bold">{{ new Date(budgetLimit.expiry * 1000).toLocaleString() }}</span>
+  </h3>
+
+  <div class="divider" />
+
+  <div class="flex justify-between">
+    <ButtonUI
+      :disabled="loading"
+      :loading="loading"
+      variant="primary"
+      @click="$emit('submit', budgetLimit)"
+      >Confirm Approval</ButtonUI
+    >
+
+    <ButtonUI outline variant="error" :disabled="loading" @click="$emit('close')">Cancel</ButtonUI>
+  </div>
+</template>
+<script setup lang="ts">
+import { NETWORK, USDC_ADDRESS, USDT_ADDRESS } from '@/constant'
+import { zeroAddress } from 'viem'
+import ButtonUI from '@/components/ButtonUI.vue'
+import type { BudgetLimit } from '@/types'
+import { useTeamStore } from '@/stores'
+
+defineEmits(['submit', 'close'])
+
+const props = defineProps<{
+  budgetLimit: BudgetLimit
+  loading: boolean
+}>()
+
+const teamStore = useTeamStore()
+function limitType(type: 0 | 1 | 2): string {
+  const budgetTypes = {
+    0: 'Max Transaction per Period',
+    1: 'Max Amount per Period',
+    2: 'Max Amount per Transaction'
+  }
+  return budgetTypes[type]
+}
+
+function getToken(budgetType: 0 | 1 | 2): string {
+  if (budgetType == 0) return ''
+
+  const tokens = {
+    [zeroAddress]: NETWORK.currencySymbol,
+    [USDC_ADDRESS]: 'USDC',
+    [USDT_ADDRESS]: 'USDT'
+  }
+  return tokens[props.budgetLimit.tokenAddress]
+}
+</script>
