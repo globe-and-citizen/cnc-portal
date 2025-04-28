49c33f083e2aac39387204645642a79db47d3875
aliassan <alimkwananzi@gmail.com>
2025-04-28 16:23:27 +0200

    ci: add stores mocks to global scope

---
+++ app/src/tests/mocks/store.mock.ts
@@ -0,0 +1,20 @@
+export const mockTeamStore = {
+  currentTeam: {
+    id: '1',
+    name: 'Team Name',
+    description: 'Team Description',
+    members: [],
+    teamContracts: [
+      {
+        address: '0xTeamContractAddress',
+        admins: [],
+        type: 'ContractType',
+        deployer: '0xDeployerAddress'
+      }
+    ],
+    ownerAddress: '0xOwnerAddress'
+  },
+  currentTeamMeta: {
+    teamIsFetching: false
+  }
+}
---
+++ app/src/tests/setup/store.setup.ts
@@ -0,0 +1,19 @@
+import { vi } from 'vitest'
+import * as mocks from '@/tests/mocks/store.mock'
+
+vi.mock('@/stores', async (importOriginal) => {
+  const actual: object = await importOriginal()
+  return {
+    ...actual,
+    useToastStore: vi.fn(() => ({ addErrorToast: vi.fn(), addSuccessToast: vi.fn() })),
+    useTeamStore: vi.fn(() => ({ ...mocks.mockTeamStore })),
+    useExpenseDataStore: vi.fn(),
+    useCryptoPrice: vi.fn(),
+    useCurrencyStore: vi.fn(() => ({
+      currency: {
+        code: 'USD',
+        symbol: '$'
+      }
+    }))
+  }
+})
