59f12a4230dd44237138473b49a6c677370aa6d2
farrellh1 <farrell.hauzan@gmail.com>
2025-05-04 19:24:06 +0700

    feat: create safe wallet interfaces

---
+++ contract/contracts/interfaces/ISafe.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface ISafe {
+ function setup(
+    address[] calldata _owners,
+    uint256 _threshold,
+    address to,
+    bytes calldata data,
+    address fallbackHandler,
+    address paymentToken,
+    uint256 payment,
+    address payable paymentReceiver
+  ) external;
+}
