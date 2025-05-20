9f310f146601269d2ad2290840dcb7f874c58e9f
Hermann EHO <hermannleboss@gmail.com>
2025-05-20 08:02:26 +0000

    refactor: enhance balance structure and formatting in useContractBalance

--- app/src/composables/useContractBalance.ts
+++ app/src/composables/useContractBalance.ts
@@ -1,9 +1,10 @@
-import { computed, watch } from 'vue'
+import { computed } from 'vue'
 import { useBalance, useReadContract, useChainId } from '@wagmi/vue'
 import { formatEther, type Address } from 'viem'
 import { USDC_ADDRESS } from '@/constant'
 import ERC20ABI from '@/artifacts/abi/erc20.json'
 import { useCurrencyStore } from '@/stores/currencyStore'
+import { formatCurrencyShort } from '@/utils/currencyUtil'
 
 export interface ContractBalance {
   nativeToken: {
@@ -22,8 +23,14 @@ export interface ContractBalance {
 interface Balance {
   code: string
   amount: number
-  valueInUSD: number
-  valueInLocalCurrency: number
+  valueInUSD: {
+    value: number
+    formated: string
+  }
+  valueInLocalCurrency: {
+    value: number
+    formated: string
+  }
 }
 
 export function useContractBalance(address: Address | undefined) {
@@ -34,7 +41,6 @@ export function useContractBalance(address: Address | undefined) {
     data: nativeBalance,
     isLoading: isLoadingNative,
     error: nativeError
-    // refetch: fetchNativeBalance
   } = useBalance({
     address,
     chainId,
@@ -58,39 +64,10 @@ export function useContractBalance(address: Address | undefined) {
     }
   })
 
-  // const formattedNativeBalance = computed(() =>
-  //   nativeBalance.value ? formatEther(nativeBalance.value.value) : '0'
-  // )
-
-  // const formattedUsdcBalance = computed(() =>
-  //   usdcBalance.value ? (Number(usdcBalance.value) / 1e6).toString() : '0'
-  // )
-
-  // const totalValueUSD = computed(() => {
-  //   const nativeValue =
-  //     Number(formattedNativeBalance.value) * (currencyStore.nativeToken.priceInUSD || 0)
-  //   const usdcValue = Number(formattedUsdcBalance.value)
-  //   return (nativeValue + usdcValue).toFixed(2)
-  // })
-
-  // const totalValueInLocalCurrency = computed(() => {
-  //   const nativeValue =
-  //     Number(formattedNativeBalance.value) * (currencyStore.nativeToken.priceInLocal || 0)
-  //   const usdcValue = Number(formattedUsdcBalance.value) * (currencyStore.usdc.priceInLocal || 0)
-  //   return (nativeValue + usdcValue).toFixed(2)
-  // })
-
   const isLoading = computed(() => isLoadingNative.value || isLoadingUsdc.value)
 
   const error = computed(() => nativeError.value || usdcError.value)
 
-  // const refetch = async () => {
-  //   try {
-  //     await Promise.all([fetchNativeBalance(), fetchUsdcBalance()])
-  //   } catch (err) {
-  //     console.error('Error refetching balances:', err)
-  //   }
-  // }
   const newBalance = computed<Array<Balance>>(() => {
     const nativeAmount = nativeBalance.value ? Number(formatEther(nativeBalance.value.value)) : 0
     const usdcAmount = usdcBalance.value ? Number(usdcBalance.value) / 1e6 : 0
@@ -98,51 +75,40 @@ export function useContractBalance(address: Address | undefined) {
       {
         amount: nativeAmount,
         code: nativeBalance.value?.symbol || 'ETH',
-        valueInUSD: Number((nativeAmount * (currencyStore.nativeToken.priceInUSD || 0)).toFixed(2)),
-        valueInLocalCurrency: Number(
-          (nativeAmount * (currencyStore.nativeToken.priceInLocal || 0)).toFixed(2)
-        )
+        valueInUSD: {
+          value: Number((nativeAmount * (currencyStore.nativeToken.priceInUSD || 0)).toFixed(2)),
+          formated: formatCurrencyShort(
+            Number((nativeAmount * (currencyStore.nativeToken.priceInUSD || 0)).toFixed(2))
+          )
+        },
+        valueInLocalCurrency: {
+          value: Number((nativeAmount * (currencyStore.nativeToken.priceInLocal || 0)).toFixed(2)),
+          formated: formatCurrencyShort(
+            Number((nativeAmount * (currencyStore.nativeToken.priceInLocal || 0)).toFixed(2)),
+            currencyStore.localCurrency.code
+          )
+        }
       },
       {
-        amount: usdcBalance.value ? Number(usdcBalance.value) / 1e6 : 0,
+        amount: usdcAmount,
         code: 'USDC',
-        valueInUSD: Number((usdcAmount * (currencyStore.usdc.priceInUSD || 0)).toFixed(2)),
-        valueInLocalCurrency: Number(
-          (usdcAmount * (currencyStore.usdc.priceInLocal || 0)).toFixed(2)
-        )
+        valueInUSD: {
+          value: Number((usdcAmount * (currencyStore.usdc.priceInUSD || 0)).toFixed(2)),
+          formated: formatCurrencyShort(
+            Number((usdcAmount * (currencyStore.usdc.priceInUSD || 0)).toFixed(2))
+          )
+        },
+        valueInLocalCurrency: {
+          value: Number((usdcAmount * (currencyStore.usdc.priceInLocal || 0)).toFixed(2)),
+          formated: formatCurrencyShort(
+            Number((usdcAmount * (currencyStore.usdc.priceInLocal || 0)).toFixed(2)),
+            currencyStore.localCurrency.code
+          )
+        }
       }
     ]
-    // console.log('comuted newBalance')
-    // console.log('newBalance', tab)
     return tab
   })
-
-  // watch(
-  //   newBalance,
-  //   () => {
-  //     console.log('newBalance Value', newBalance.value)
-  //   },
-  //   { deep: true }
-  // )
-  // watch(usdcBalance, () => {
-  //   console.log('usdcBalance Value', usdcBalance.value)
-  // })
-
-  // watch(nativeBalance, () => {
-  //   console.log('nativeBalance Value', nativeBalance.value)
-  // })
-  // const balances = reactive({
-  //   nativeToken: {
-  //     balance: computed(() => nativeBalance.value?.value),
-  //     formatted: computed(() => formattedNativeBalance.value)
-  //   },
-  //   usdc: {
-  //     balance: computed(() => usdcBalance.value),
-  //     formatted: computed(() => formattedUsdcBalance.value)
-  //   },
-  //   totalValueUSD: computed(() => totalValueUSD.value),
-  //   totalValueInLocalCurrency: computed(() => totalValueInLocalCurrency.value)
-  // })
 
   return {
     balances: newBalance,
@@ -151,14 +117,14 @@ export function useContractBalance(address: Address | undefined) {
         usdBalance: Number(
           newBalance.value
             .reduce((acc, balance) => {
-              return acc + (balance.valueInUSD || 0)
+              return acc + (balance.valueInUSD.value || 0)
             }, 0)
             .toFixed(2)
         ),
         localCurrencyBalance: Number(
           newBalance.value
             .reduce((acc, balance) => {
-              return acc + (balance.valueInLocalCurrency || 0)
+              return acc + (balance.valueInLocalCurrency.value || 0)
             }, 0)
             .toFixed(2)
         )
@@ -166,6 +132,5 @@ export function useContractBalance(address: Address | undefined) {
     }),
     isLoading,
     error
-    // refetch
   }
 }
