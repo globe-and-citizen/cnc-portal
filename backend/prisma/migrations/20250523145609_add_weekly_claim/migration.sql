53bbe02f3f8c7d59e75b241e69c2c50815c53d72
Georginio-prod <etonameklou6@gmail.com>
2025-05-26 10:50:58 +0000

    feat: Remove outdated weekly claim migrations and update schema for new relationships

--- backend/prisma/migrations/20250523145609_add_weekly_claim/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `claimId` on the `WeeklyClaim` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "WeeklyClaim" DROP COLUMN "claimId";
