22d2c4afffc1406bd95bc41501a63a08e82bb4f8
Georginio-prod <etonameklou6@gmail.com>
2025-05-29 15:53:14 +0000

    feat: Integrate TableComponent into WeeklyClaimComponent for enhanced data display

--- app/src/components/WeeklyClaimComponent.vue
+++ app/src/components/WeeklyClaimComponent.vue
@@ -1,108 +1,53 @@
 <template>
-  <pre>
+  <!-- <pre>
     {{ data }}
     {{ error }}
-</pre
-  >
+</pre> -->
   <div class="card bg-base-100 shadow-xl w-full pb-7">
     <div class="card-body overflow-x-auto">
-      <table class="table w-full text-sm">
-        <!-- En-tête -->
-        <thead class="text-gray-700 text-lg font-semibold">
-          <tr>
-            <th>Date</th>
-            <th>Member</th>
-            <th>Hour Worked</th>
-            <!-- <th>Memo</th> -->
-            <th>Hourly Rate</th>
-            <th class="text-center">Action</th>
-          </tr>
-        </thead>
-
-        <!-- Corps du tableau -->
-        <transition-group tag="tbody" name="slide" class="relative">
-          <tr
-            v-for="(notif, idx) in notifications"
-            :key="notif.id"
-            class="bg-white text-gray-800 hover:bg-gray-100 transition-all cursor-pointer"
-            :style="{ zIndex: notifications.length - idx }"
-            @click="moveToBack(idx)"
-          >
-            <!-- Date -->
-            <td class="py-2 px-4">
-              {{ notif.createdAt }}
-            </td>
-
-            <!-- Member -->
-            <td class="py-2 px-4">
-              <UserComponent :user="notif.user" />
-            </td>
-
-            <!-- Hour Worked -->
-            <td class="py-2 px-4">
-              <div class="font-semibold">{{ notif.hoursWorked }} / {{ notif.totalHours }} h</div>
-              <div class="text-xs text-gray-500">{{ notif.weeklyLimit }} h/week</div>
-            </td>
-
-            <!-- Memo -->
-            <!-- <td class="py-2 px-4">
-              {{ notif.memo }}
-            </td> -->
-
-            <!-- Hourly Rate -->
-            <td class="py-2 px-4">
-              <div class="font-semibold">{{ notif.hourlyRate }} SepoliaETH / h</div>
-              <div class="text-xs text-gray-500">{{ notif.usdRate }} USD</div>
-            </td>
-
-            <!-- Action -->
-            <td class="py-2 px-4 text-center">
-              <ButtonUI
-                class="btn btn-sm btn-primary"
-                :data-test="`approve-button-${notif.id}`"
-                variant="success"
-                :disabled="loading"
-                size="sm"
-                @click.stop="async () => await approveClaim(notif)"
-              >
-                {{ notif.action }}
-              </ButtonUI>
-            </td>
-          </tr>
-        </transition-group>
-      </table>
+      <slot />
+      <!-- Corps du tableau -->
+      <transition-group tag="tbody" name="slide" class="relative">
+        <tr
+          v-for="(notif, idx) in notifications"
+          :key="notif.id"
+          class="bg-white text-gray-800 hover:bg-gray-100 transition-all cursor-pointer"
+          :style="{ zIndex: notifications.length - idx }"
+          @click="moveToBack(idx)"
+        ></tr
+      ></transition-group>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import UserComponent from '@/components/UserComponent.vue'
-import ButtonUI from '@/components/ButtonUI.vue'
-import { useCustomFetch } from '@/composables'
+// import { ref } from 'vue'
+// import UserComponent from '@/components/UserComponent.vue'
+// import ButtonUI from '@/components/ButtonUI.vue'
+// import { useCustomFetch } from '@/composables'
 
-const notifications = ref([
-  {
-    id: 1,
-    createdAt: 'lundi 12 juin - dimanche 18 juin',
-    user: {
-      name: 'georges',
-      address: '0x098C1234ABCD9DFB',
-      imageUrl: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
-    },
-    hourlyRate: 5,
-    usdRate: 100.25,
-    hoursWorked: 10,
-    totalHours: 10,
-    weeklyLimit: 10,
-    // memo: 'georges',
-    // status: 'Submitted',
-    action: 'Approve',
-    shadow: 'shadow-lg'
-  }
-])
+// const notifications = ref([
+//   {
+//     id: 1,
+//     createdAt: 'lundi 12 juin - dimanche 18 juin',
+//     user: {
+//       name: 'georges',
+//       address: '0x098C1234ABCD9DFB',
+//       imageUrl: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
+//     },
+//     hourlyRate: 5,
+//     usdRate: 100.25,
+//     hoursWorked: 10,
+//     totalHours: 10,
+//     weeklyLimit: 10,
+//     // memo: 'georges',
+//     // status: 'Submitted',
+//     action: 'Approve',
+//     shadow: 'shadow-lg'
+//   }
+// ])
 
-const { data, error } = useCustomFetch('/weeklyClaim/?teamId=1').get().json()
+// const { data, error } = useCustomFetch('/weeklyClaim/?teamId=1').get().json()
 
 function moveToBack(index: number) {
   const notif = notifications.value.splice(index, 1)[0]
--- app/src/components/sections/CashRemunerationView/CashRemunerationWeeklyClaim.vue
+++ app/src/components/sections/CashRemunerationView/CashRemunerationWeeklyClaim.vue
@@ -1,10 +1,63 @@
 <template>
   <CardComponent title="Weekly Claim" class="w-full pb-7">
-    <WeeklyClaimComponent> </WeeklyClaimComponent>
+    <WeeklyClaimComponent> 
+     <TableComponent
+        :rows="teamClaimData ?? undefined"
+        :columns="columns"
+        :loading="isTeamClaimDataFetching"
+      >
+        <template v-for="col in columns" #[`${col.key}-data`]="{ row }" :key="col.key">
+          <span>
+            {{ row[col.key] }}
+          </span>
+        </template>
+      </TableComponent>
+    </WeeklyClaimComponent>
   </CardComponent>
 </template>
 
 <script setup lang="ts">
 import CardComponent from '@/components/CardComponent.vue'
 import WeeklyClaimComponent from '@/components/WeeklyClaimComponent.vue'
+import TableComponent, { type TableColumn, type TableRow } from '@/components/TableComponent.vue'
+
+// Table columns configuration
+const columns = [
+  {
+    key: 'createdAt',
+    label: 'Date',
+    sortable: true,
+    class: 'text-black text-base'
+  },
+  {
+    key: 'member',
+    label: 'Member',
+    sortable: false,
+    class: 'text-black text-base'
+  },
+  {
+    key: 'hoursWorked',
+    label: 'Hour Worked',
+    sortable: false,
+    class: 'text-black text-base'
+  },
+  {
+    key: 'hourlyRate',
+    label: 'Hourly Rate',
+    sortable: false,
+    class: 'text-black text-base'
+  },
+  {
+    key: 'status',
+    label: 'Status',
+    sortable: false,
+    class: 'text-black text-base'
+  },
+  {
+    key: 'action',
+    label: 'Action',
+    sortable: false,
+    class: 'text-black text-base'
+  }
+] as TableColumn[]
 </script>
