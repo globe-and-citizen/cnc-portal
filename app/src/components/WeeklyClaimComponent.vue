0a8be59531a8f8b2fba841c92764de85d1c18ca6
Georginio-prod <etonameklou6@gmail.com>
2025-06-08 08:02:16 +0000

    refactor: remove commented-out code and unused imports from WeeklyClaimComponent

--- app/src/components/WeeklyClaimComponent.vue
+++ app/src/components/WeeklyClaimComponent.vue
@@ -16,40 +16,7 @@
   </div>
 </template>
 
-<script setup lang="ts">
-// import { ref } from 'vue'
-// import UserComponent from '@/components/UserComponent.vue'
-// import ButtonUI from '@/components/ButtonUI.vue'
-// import { useCustomFetch } from '@/composables'
-
-// const notifications = ref([
-//   {
-//     id: 1,
-//     createdAt: 'lundi 12 juin - dimanche 18 juin',
-//     user: {
-//       name: 'georges',
-//       address: '0x098C1234ABCD9DFB',
-//       imageUrl: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
-//     },
-//     hourlyRate: 5,
-//     usdRate: 100.25,
-//     hoursWorked: 10,
-//     totalHours: 10,
-//     weeklyLimit: 10,
-//     // memo: 'georges',
-//     // status: 'Submitted',
-//     action: 'Approve',
-//     shadow: 'shadow-lg'
-//   }
-// ])
-
-// const { data, error } = useCustomFetch('/weeklyClaim/?teamId=1').get().json()
-
-// function moveToBack(index: number) {
-//   const notif = notifications.value.splice(index, 1)[0]
-//   notifications.value.push(notif)
-// }
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 .slide-move {
