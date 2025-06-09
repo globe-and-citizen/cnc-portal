d5235cd2741d31d3f2be7533732b51194bcba398
Hermann EHO <hermannleboss@gmail.com>
2025-06-09 16:47:56 +0000

    refactor: update DepositBankForm to improve token handling and simplify logic

--- app/src/components/forms/DepositBankForm.vue
+++ app/src/components/forms/DepositBankForm.vue
@@ -1,19 +1,16 @@
 <template>
   <span class="font-bold text-2xl">Deposit to Team Bank Contract</span>
 
-  <div v-if="tokenList[selectedTokenId].symbol === 'USDC'" class="steps w-full my-4">
+  <div v-if="selectedToken?.token.id !== 'native'" class="steps w-full my-4">
     <a class="step" :class="{ 'step-primary': currentStep >= 1 }">Amount</a>
     <a class="step" :class="{ 'step-primary': currentStep >= 2 }">Approval</a>
     <a class="step" :class="{ 'step-primary': currentStep >= 3 }">Deposit</a>
   </div>
 
-  <label
-    class="form-control w-full"
-    :class="{ 'mt-4': tokenList[selectedTokenId].symbol !== 'USDC' }"
-  >
+  <label class="form-control w-full" :class="{ 'mt-4': selectedToken?.token.id !== 'native' }">
     <div class="label">
       <span class="label-text">Deposit</span>
-      <span class="label-text-alt">Balance: {{ formattedBalance }}</span>
+      <span class="label-text-alt">Balance: {{ selectedToken?.amount }}</span>
     </div>
     <div class="input input-bordered flex items-center">
       <input
@@ -47,15 +44,15 @@
       <div>
         <SelectComponent
           :options="
-            tokenList.map((token, id) => ({
-              label: token.name,
-              value: `${id}`
+            tokenList.map((token) => ({
+              label: token.symbol,
+              value: token.tokenId
             }))
           "
           :disabled="isLoadingBalance"
           @change="
             (value) => {
-              selectedTokenId = parseInt(value)
+              selectedTokenId = value
             }
           "
           :format-value="
@@ -91,12 +88,12 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed, watch } from 'vue'
+import { ref, computed, watch } from 'vue'
+
 import { required, numeric, helpers } from '@vuelidate/validators'
 import { useVuelidate } from '@vuelidate/core'
-import { onClickOutside } from '@vueuse/core'
 
-import { NETWORK, USDC_ADDRESS } from '@/constant'
+import { SUPPORTED_TOKENS, type TokenId } from '@/constant'
 import ERC20ABI from '@/artifacts/abi/erc20.json'
 import BankABI from '@/artifacts/abi/bank.json'
 
@@ -105,282 +102,219 @@ import { useCurrencyStore, useToastStore, useUserDataStore } from '@/stores'
 import SelectComponent from '@/components/SelectComponent.vue'
 import ButtonUI from '../ButtonUI.vue'
 
-import {
-  useBalance,
-  useChainId,
-  useReadContract,
-  useSendTransaction,
-  useWriteContract,
-  useWaitForTransactionReceipt
-} from '@wagmi/vue'
+import { useSendTransaction, useWriteContract, useWaitForTransactionReceipt } from '@wagmi/vue'
 import { readContract } from '@wagmi/core'
 import { config } from '@/wagmi.config'
-import { formatEther, parseEther, type Address } from 'viem'
+import { parseEther, type Address } from 'viem'
+import { useContractBalance } from '@/composables/useContractBalance'
 
+import { formatCurrencyShort } from '@/utils/currencyUtil'
+
+const emits = defineEmits(['closeModal'])
 const props = defineProps<{
   loading?: boolean
   loadingText?: string
   bankAddress: Address
 }>()
 
-const { addErrorToast, addSuccessToast } = useToastStore()
-
 // Component state
 const amount = ref<string>('')
-const selectedTokenId = ref(0)
-const isDropdownOpen = ref<boolean>(false)
+const selectedTokenId = ref<TokenId>('native') // Default to native token (ETH)
 const depositAmount = ref<string>('')
+const currentStep = ref(1)
+
+// Stores
 const currencyStore = useCurrencyStore()
 const userDataStore = useUserDataStore()
-const chainId = useChainId()
+const { addErrorToast, addSuccessToast } = useToastStore()
 
-// Contract interactions
-const { sendTransaction, isPending: depositLoading, data: depositHash } = useSendTransaction()
+// Reactive state for balances: composable that fetches address balances
+const { balances, isLoading } = useContractBalance(userDataStore.address as Address)
 
-const { isLoading: isConfirmingDeposit } = useWaitForTransactionReceipt({
+// Native token desposit
+const { sendTransactionAsync: sendTransaction, data: depositHash } = useSendTransaction()
+
+// Wait for transaction receipt for Native token deposit
+const { status: nativeTokenDespositStatus } = useWaitForTransactionReceipt({
   hash: depositHash
 })
 
-const {
-  writeContract: writeTokenDeposit,
-  isPending: tokenDepositLoading,
-  data: tokenDepositHash
-} = useWriteContract()
+// Write contract for ERC20 token deposit
+const { writeContractAsync: writeTokenDeposit, data: tokenDepositHash } = useWriteContract()
 
-const { isLoading: isConfirmingTokenDeposit } = useWaitForTransactionReceipt({
+// Wait for transaction receipt for ERC20 token deposit
+const { status: erc20TokenDespositStatus } = useWaitForTransactionReceipt({
   hash: tokenDepositHash
 })
 
+// Write contract for ERC20 token spend cap approval
 const {
-  writeContract: approve,
-  isPending: isPendingApprove,
+  writeContractAsync: approve,
+  // isPending: isPendingApprove,
   data: approveHash
 } = useWriteContract()
 
-const { isLoading: isConfirmingApprove } = useWaitForTransactionReceipt({
+// Wait for transaction receipt for ERC20 token spend cap approval
+const { status: erc20ApprovaleSatus } = useWaitForTransactionReceipt({
   hash: approveHash
 })
 
-watch(amount, () => {
-  $v.value.$touch()
+// Computed properties
+// Token list derived from SUPPORTED_TOKENS
+const tokenList = computed(() =>
+  SUPPORTED_TOKENS.map((token) => ({
+    symbol: token.symbol,
+    tokenId: token.id,
+    name: token.name
+  }))
+)
+
+// computed propertie for selected token
+const selectedToken = computed(() =>
+  balances.value.find((b) => b.token.id === selectedTokenId.value)
+)
+// TODO, toFixed(4) is giving sometimes a value > to the actual balance, need to fix this
+const estimatedPrice = computed(() => {
+  const tokenInfo = currencyStore.getTokenInfo(selectedTokenId.value)
+  const priceObj = tokenInfo?.prices.find((p) => p.id === 'local')
+  const price = priceObj?.price ?? 0
+  const value = (Number(amount.value) || 0) * price
+  return formatCurrencyShort(value, priceObj?.code ?? 'USD')
 })
 
-const { data: nativeBalance, isLoading: isLoadingNativeBalance } = useBalance({
-  address: userDataStore.address as Address,
-  chainId
-})
+const isLoadingBalance = computed(() => isLoading.value)
 
-const { data: usdcBalance, isLoading: isLoadingUsdcBalance } = useReadContract({
-  address: USDC_ADDRESS as Address,
-  abi: ERC20ABI,
-  functionName: 'balanceOf',
-  args: [userDataStore.address as Address]
-})
+// Methods
 
-const tokenList = [
-  { name: NETWORK.currencySymbol, symbol: 'ETH' },
-  { name: 'USDC', symbol: 'USDC' }
-]
+// wait until a value is true
 
-const emits = defineEmits(['deposit', 'closeModal'])
-
-const target = ref<HTMLElement | null>(null)
-onMounted(() => {
-  onClickOutside(target, () => {
-    isDropdownOpen.value = false
+const waitForCondition = (condition: () => boolean, timeout = 5000) => {
+  return new Promise((resolve, reject) => {
+    const startTime = Date.now()
+    const interval = setInterval(() => {
+      console.log('Checking condition...')
+      if (condition()) {
+        clearInterval(interval)
+        resolve(true)
+      } else if (Date.now() - startTime > timeout) {
+        clearInterval(interval)
+        reject(new Error('Condition not met within timeout'))
+      }
+    }, 1000)
   })
-  // Fetch the current price when component mounts
-  // currencyStore.fetchNativeTokenPrice()
-})
-
-const formattedBalance = computed(() => {
-  if (selectedTokenId.value === 0) {
-    return nativeBalance.value ? Number(formatEther(nativeBalance.value.value)).toFixed(4) : '0.00'
-  }
-  return usdcBalance.value ? (Number(usdcBalance.value) / 1e6).toFixed(4) : '0.00'
-})
-
-const isLoadingBalance = computed(() => isLoadingNativeBalance.value || isLoadingUsdcBalance.value)
-
+}
 const useMaxBalance = () => {
-  amount.value = formattedBalance.value
+  amount.value = selectedToken.value?.amount.toString() ?? '0.00'
 }
-
 const usePercentageOfBalance = (percentage: number) => {
-  const balance = parseFloat(formattedBalance.value)
-  amount.value = ((balance * percentage) / 100).toFixed(4)
+  amount.value = (((selectedToken.value?.amount ?? 0) * percentage) / 100).toFixed(4)
 }
 
+// Validation rules
 const notZero = helpers.withMessage('Amount must be greater than 0', (value: string) => {
   return parseFloat(value) > 0
 })
 
 const notExceedingBalance = helpers.withMessage('Amount exceeds your balance', (value: string) => {
   if (!value || parseFloat(value) <= 0) return true
-  return parseFloat(value) <= parseFloat(formattedBalance.value)
+  const amountValue = selectedToken.value?.amount ?? 0
+  return parseFloat(value) <= amountValue
 })
 
-const validDecimals = helpers.withMessage(
-  'Amount must have at most 4 decimal places',
-  (value: string) => {
-    if (!value) return true
-    const parts = value.split('.')
-    return parts.length === 1 || parts[1].length <= 4
-  }
-)
+// const validDecimals = helpers.withMessage(
+//   'Amount must have at most 4 decimal places',
+//   (value: string) => {
+//     if (!value) return true
+//     const parts = value.split('.')
+//     return parts.length === 1 || parts[1].length <= 4
+//   }
+// )
 
 const rules = {
   amount: {
     required,
     numeric,
     notZero,
-    notExceedingBalance,
-    validDecimals
+    notExceedingBalance
+    // validDecimals
   }
 }
 
 const $v = useVuelidate(rules, { amount })
-
-const estimatedPrice = computed(() => {
-  const amountValue = parseFloat(amount.value)
-  if (isNaN(amountValue) || amountValue <= 0) return 0
-
-  if (selectedTokenId.value === 0) {
-    return Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: currencyStore.localCurrency.code,
-      minimumFractionDigits: 2
-    }).format((currencyStore.nativeToken.priceInLocal || 0) * amountValue)
-  }
-
-  return Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: currencyStore.localCurrency.code,
-    minimumFractionDigits: 2
-  }).format((currencyStore.usdc.priceInLocal || 0) * amountValue)
-})
-
-// Add currentStep ref
-const currentStep = ref(1)
-
-watch(
-  [
-    () => tokenList[selectedTokenId.value].symbol,
-    isPendingApprove,
-    isConfirmingApprove,
-    tokenDepositLoading,
-    isConfirmingTokenDeposit
-  ],
-  ([
-    symbol,
-    isPendingApprove,
-    isConfirmingApprove,
-    tokenDepositLoading,
-    isConfirmingTokenDeposit
-  ]) => {
-    // Reset step when switching tokens
-    if (symbol !== 'USDC') {
-      currentStep.value = 1
-      return
-    }
-
-    // Update steps for USDC
-    if (isPendingApprove || isConfirmingApprove) {
-      currentStep.value = 2
-    } else if (tokenDepositLoading || isConfirmingTokenDeposit) {
-      currentStep.value = 3
-    } else {
-      currentStep.value = 1
-    }
-  }
-)
-
-watch(isConfirmingDeposit, (newIsConfirming, oldIsConfirming) => {
-  if (!newIsConfirming && oldIsConfirming) {
-    addSuccessToast('ETH deposited successfully')
-    emits('closeModal')
-  }
-})
-
-watch(isConfirmingTokenDeposit, (newIsConfirming, oldIsConfirming) => {
-  if (!newIsConfirming && oldIsConfirming) {
-    addSuccessToast('USDC deposited successfully')
-    emits('closeModal')
-    depositAmount.value = '' // Clear stored amount
-  }
-})
-
-watch(isConfirmingApprove, async (newIsConfirming, oldIsConfirming) => {
-  if (!newIsConfirming && oldIsConfirming) {
-    addSuccessToast('Token approved successfully')
-    if (depositAmount.value) {
-      await handleUsdcDeposit(depositAmount.value)
-    }
-  }
-})
 
 const submitForm = async () => {
   await $v.value.$touch()
   if ($v.value.$invalid) return
 
   try {
-    if (tokenList[selectedTokenId.value].symbol === 'ETH') {
-      sendTransaction({
+    if (selectedTokenId.value === 'native') {
+      await sendTransaction({
         to: props.bankAddress,
         value: parseEther(amount.value)
       })
-    } else if (tokenList[selectedTokenId.value].symbol === 'USDC') {
+
+      await waitForCondition(() => nativeTokenDespositStatus.value === 'success', 15000)
+      addSuccessToast(`${selectedToken.value?.token.code} deposited successfully`)
+      emits('closeModal')
+      depositAmount.value = '' // Clear stored amount
+    } else {
       const tokenAmount = BigInt(Number(amount.value) * 1e6)
       depositAmount.value = amount.value // Store amount for after approval
 
-      const allowance = await readContract(config, {
-        address: USDC_ADDRESS as Address,
-        abi: ERC20ABI,
-        functionName: 'allowance',
-        args: [userDataStore.address as Address, props.bankAddress]
-      })
-
-      const currentAllowance = allowance ? allowance.toString() : 0n
-      if (Number(currentAllowance) < Number(tokenAmount)) {
-        approve({
-          address: USDC_ADDRESS as Address,
+      if (selectedToken.value) {
+        const allowance = await readContract(config, {
+          address: selectedToken.value.token.address as Address,
           abi: ERC20ABI,
-          functionName: 'approve',
-          args: [props.bankAddress, tokenAmount]
+          functionName: 'allowance',
+          args: [userDataStore.address as Address, props.bankAddress]
         })
+
+        const currentAllowance = allowance ? allowance.toString() : 0n
+        if (Number(currentAllowance) < Number(tokenAmount)) {
+          // If allowance is less than token amount, approve the token
+          currentStep.value = 2
+          await approve({
+            address: selectedToken.value.token.address as Address,
+            abi: ERC20ABI,
+            functionName: 'approve',
+            args: [props.bankAddress, tokenAmount]
+          })
+
+          // wait for 3s, timeout for approval transaction
+          await new Promise((resolve) => setTimeout(resolve, 3000))
+
+          await waitForCondition(() => erc20ApprovaleSatus.value === 'success', 15000)
+
+          // wait for transaction receipt
+          addSuccessToast('Token approved successfully')
+        }
+
+        // Directly deposit the token
+        currentStep.value = 3
+        await writeTokenDeposit({
+          address: props.bankAddress,
+          abi: BankABI,
+          functionName: 'depositToken',
+          args: [selectedToken.value.token.address as Address, tokenAmount]
+        })
+        // TODO wait for transaction receipt
+
+        await waitForCondition(() => erc20TokenDespositStatus.value === 'success', 15000)
+        depositAmount.value = ''
+        addSuccessToast('USDC deposited successfully')
+        emits('closeModal')
       } else {
-        // If already approved, deposit directly
-        await handleUsdcDeposit(amount.value)
+        addErrorToast('Selected token is not valid')
+        return
       }
     }
   } catch (error) {
     console.error(error)
-    addErrorToast(`Failed to deposit ${tokenList[selectedTokenId.value].symbol}`)
+    addErrorToast(`Failed to deposit ${selectedTokenId.value}`)
   }
+
+  currentStep.value = 1
 }
-
-const handleUsdcDeposit = async (amount: string) => {
-  const tokenAmount = BigInt(Number(amount) * 1e6)
-
-  writeTokenDeposit({
-    address: props.bankAddress,
-    abi: BankABI,
-    functionName: 'depositToken',
-    args: [USDC_ADDRESS as Address, tokenAmount]
-  })
-}
-
-const isLoading = computed(() => {
-  return Boolean(
-    props.loading ||
-      depositLoading.value ||
-      isConfirmingDeposit.value ||
-      isPendingApprove.value ||
-      isConfirmingApprove.value ||
-      tokenDepositLoading.value ||
-      isConfirmingTokenDeposit.value
-  )
-})
 
 const handleAmountInput = (event: Event) => {
   const input = event.target as HTMLInputElement
@@ -392,4 +326,8 @@ const handleAmountInput = (event: Event) => {
     amount.value = value
   }
 }
+
+watch(amount, () => {
+  $v.value.$touch()
+})
 </script>
