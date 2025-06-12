f635c4e3b24c9b432ad87325ebed76ea59585829
aliassan <alimkwananzi@gmail.com>
2025-06-12 17:26:30 +0200

    refactor: update to use CRSigneClaim type

--- app/src/components/sections/CashRemunerationView/CashRemunerationTable.vue
+++ app/src/components/sections/CashRemunerationView/CashRemunerationTable.vue
@@ -37,7 +37,7 @@
         </template>
         <template #action-data="{ row }">
           <CRSigne :claim="formatRow(row) as CRSignClaim" @claim-signed="fetchTeamClaimData()" />
-          <CRWithdrawClaim :claim="formatRow(row)" @claim-withdrawn="fetchTeamClaimData()" />
+          <CRWithdrawClaim :claim="formatRow(row) as CRSignClaim" @claim-withdrawn="fetchTeamClaimData()" />
           <!-- <ButtonUI
             v-if="row.status == 'pending' && ownerAddress == userDataStore.address"
             variant="success"
