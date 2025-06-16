23967fd1ffd0e8308a9941bba61fc24ac8643e86
Hermann EHO <hermannleboss@gmail.com>
2025-06-16 09:09:57 +0000

    refactor: replace typedBankAddress with bankAddress  for consistency  and use getContractAddressByType in BankView component

--- app/src/views/team/[id]/BankView.vue
+++ app/src/views/team/[id]/BankView.vue
@@ -1,30 +1,25 @@
 <template>
   <div class="flex flex-col gap-6">
     <BankBalanceSection
-      v-if="typedBankAddress"
+      v-if="bankAddress"
       ref="bankBalanceSection"
-      :bank-address="typedBankAddress"
+      :bank-address="bankAddress!"
       @balance-updated="$forceUpdate()"
     />
-    <GenericTokenHoldingsSection v-if="typedBankAddress" :address="typedBankAddress" />
+    <GenericTokenHoldingsSection v-if="bankAddress" :address="bankAddress!" />
     <!-- <TransactionsHistorySection /> -->
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import { type Address } from 'viem'
+import { computed, ref } from 'vue'
+// import { type Address } from 'viem'
 import BankBalanceSection from '@/components/sections/BankView/BankBalanceSection.vue'
 import GenericTokenHoldingsSection from '@/components/GenericTokenHoldingsSection.vue'
 // import TransactionsHistorySection from '@/components/sections/BankView/TransactionsHistorySection.vue'
 import { useTeamStore } from '@/stores'
 
 const teamStore = useTeamStore()
-const typedBankAddress = computed<Address | undefined>(() => {
-  const bankContract = teamStore.currentTeam?.teamContracts.find(
-    (contract) => contract.type === 'Bank'
-  )
-  return bankContract?.address as Address | undefined
-})
+const bankAddress = computed(() => teamStore.getContractAddressByType('Bank'))
 const bankBalanceSection = ref<InstanceType<typeof BankBalanceSection> | null>(null)
 </script>
