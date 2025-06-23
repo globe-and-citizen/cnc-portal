64f650242ad9b516d6f76d03e1eccb1cf7b9c73f
Georginio-prod <etonameklou6@gmail.com>
2025-06-23 19:22:14 +0000

    feat: add CRWeeklyClaimOwnerHeader component for displaying pending weekly claims

--- app/src/components/sections/CashRemunerationView/CRWeeklyClaimOwnerHeader.vue
+++ app/src/components/sections/CashRemunerationView/CRWeeklyClaimOwnerHeader.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
+  <div v-if="isTeamOwner">
     <div class="px-8 pb-4 flex items-end" :class="{ 'justify-between': isTeamOwner }">
       <span class="card-title" v-if="isTeamOwner">Pending Weekly Claim</span>
       <div class="card-actions justify-end">
-        <CRAddERC20Support v-if="isTeamOwner" />
-        <SubmitClaims v-if="hasWage" />
+        <CRAddERC20Support />
       </div>
     </div>
   </div>
@@ -15,7 +14,6 @@ import { useCustomFetch } from '@/composables/useCustomFetch'
 import { computed, watch } from 'vue'
 import { useUserDataStore, useTeamStore, useToastStore } from '@/stores'
 import { type WageResponse } from '@/types'
-import SubmitClaims from './SubmitClaims.vue'
 
 import CRAddERC20Support from './CRAddERC20Support.vue'
 
@@ -26,18 +24,11 @@ const toastStore = useToastStore()
 const isTeamOwner = computed(() => {
   return teamStore.currentTeam?.ownerAddress === userStore.address
 })
-const { data: teamWageData, error: teamWageDataError } = useCustomFetch(
+const { error: teamWageDataError } = useCustomFetch(
   computed(() => `/wage/?teamId=${teamStore.currentTeam?.id}`)
 )
   .get()
   .json<Array<WageResponse>>()
-
-const hasWage = computed(() => {
-  const userWage = teamWageData.value?.find((wage) => wage.userAddress === userStore.address)
-  if (!userWage) return false
-
-  return true
-})
 
 watch(teamWageDataError, (newVal) => {
   if (newVal) {
