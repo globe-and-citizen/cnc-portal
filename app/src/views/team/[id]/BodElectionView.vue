0fdf172502cc914d78e7bd78f1803b8bff93903d
aliassan <alimkwananzi@gmail.com>
2025-06-25 15:17:32 +0200

    feat: add bod members section

--- app/src/views/team/[id]/BodElectionView.vue
+++ app/src/views/team/[id]/BodElectionView.vue
@@ -1,10 +1,12 @@
 <template>
+  <CurrentBoDSection />
   <BoDElectionSection v-if="team" :team="team" />
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 import BoDElectionSection from '@/components/sections/AdministrationView/BoDElectionSection.vue'
+import CurrentBoDSection from '@/components/sections/AdministrationView/CurrentBoDSection.vue'
 import { useTeamStore } from '@/stores'
 
 const teamStore = useTeamStore()
