01e6631e4c222de5ac9600c977a294267832d1c7
aliassan <alimkwananzi@gmail.com>
2025-06-26 11:52:49 +0200

    feat: add current election section

--- app/src/views/team/[id]/BodElectionView.vue
+++ app/src/views/team/[id]/BodElectionView.vue
@@ -1,5 +1,6 @@
 <template>
   <CurrentBoDSection />
+  <CurrentBoDElectionSection />
   <BoDElectionSection v-if="team" :team="team" />
 </template>
 
@@ -7,6 +8,7 @@
 import { computed } from 'vue'
 import BoDElectionSection from '@/components/sections/AdministrationView/BoDElectionSection.vue'
 import CurrentBoDSection from '@/components/sections/AdministrationView/CurrentBoDSection.vue'
+import CurrentBoDElectionSection from '@/components/sections/AdministrationView/CurrentBoDElectionSection.vue'
 import { useTeamStore } from '@/stores'
 
 const teamStore = useTeamStore()
