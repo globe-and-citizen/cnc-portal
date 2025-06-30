b09cc6a40bda2372555cd22243b22be233e072c4
Georginio-prod <etonameklou6@gmail.com>
2025-06-30 09:56:34 +0000

    feat: add ClaimHistoryView component to display claim history

---
+++ app/src/views/team/[id]/ClaimHistoryView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <ClaimHistory />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ClaimHistory from '@/components/sections/ClaimHistoryView/ClaimHistory.vue'
+</script>
