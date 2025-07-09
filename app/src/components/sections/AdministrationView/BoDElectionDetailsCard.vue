7423c38780937fa38577a7fcde92d656d138a7cf
aliassan <alimkwananzi@gmail.com>
2025-07-09 17:16:49 +0200

    feat: add bod election details card

---
+++ app/src/components/sections/AdministrationView/BoDElectionDetailsCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-base-100 card border border-gray-200 flex flex-col">
+    <div class="card-body">
+      <UserComponent layout="alternate" :user="election.user" />
+
+      <!-- View Results Button -->
+      <ButtonUI variant="success" :outline="true"> View Results </ButtonUI>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ButtonUI from '@/components/ButtonUI.vue'
+import UserComponent from './UserComponent.vue'
+
+defineProps({
+  election: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+/* Optional custom styles */
+</style>
