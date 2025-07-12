abaf31131bdc06f6bb840df4539b5a5e47815ebc
farrellh1 <farrell.hauzan@gmail.com>
2025-07-12 17:40:12 +0700

    feature: past elections

--- app/src/components/sections/AdministrationView/PastBoDElectionCard.vue
+++ app/src/components/sections/AdministrationView/PastBoDElectionCard.vue
@@ -15,42 +15,85 @@
       <!-- Candidates Count -->
       <div class="flex justify-between items-center mb-2">
         <span class="text-gray-600">Candidates:</span>
-        <span class="font-semibold">{{ election.candidates }}</span>
+        <span class="font-semibold">{{ election.seatCount }}</span>
       </div>
 
       <!-- Votes Count -->
       <div class="flex justify-between items-center mb-4">
         <span class="text-gray-600">Total Votes:</span>
-        <span class="font-semibold">{{ election.totalVotes }}</span>
+        <span class="font-semibold">{{ voteCount }}</span>
       </div>
 
       <div class="flex-grow"></div>
       <!-- Spacer -->
 
       <!-- Elected Members -->
-      <div class="mb-2 mb-5">
+      <div class="mb-5">
         <p class="text-gray-600 mb-2">Elected Members:</p>
         <div class="flex flex-wrap gap-2">
-          <span v-for="(member, i) in election.electedMembers" :key="i" class="badge badge-info">
-            {{ member }}
+          <span v-for="(member, i) in electionResults" :key="i" class="badge badge-info">
+            {{
+              teamStore.currentTeam?.members.find((m) => m.address === member)?.name || 'Unknown'
+            }}
           </span>
         </div>
       </div>
 
       <!-- View Results Button -->
-      <ButtonUI variant="success" :outline="true"> View Results </ButtonUI>
+      <ButtonUI variant="success" :outline="true" @click="electionResultModal = true">
+        View Results
+      </ButtonUI>
+      <ModalComponent
+        v-if="electionResultModal"
+        v-model="electionResultModal"
+        data-test="election-result-modal"
+      >
+        <ElectionResultModal :id="election.id" @closeModal="electionResultModal = false" />
+      </ModalComponent>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ELECTIONS_ABI } from '@/artifacts/abi/elections'
 import ButtonUI from '@/components/ButtonUI.vue'
+import { useTeamStore, useToastStore } from '@/stores'
+import type { Election } from '@/types'
+import { parseError } from '@/utils'
+import { useReadContract } from '@wagmi/vue'
+import type { Address } from 'viem'
+import { watch } from 'vue'
+import { computed, ref } from 'vue'
+import ElectionResultModal from './modals/ElectionResultModal.vue'
+import ModalComponent from '@/components/ModalComponent.vue'
 
-defineProps({
-  election: {
-    type: Object,
-    required: true
-  }
+const electionResultModal = ref(false)
+const { election } = defineProps<{
+  election: Election
+}>()
+const teamStore = useTeamStore()
+const toastStore = useToastStore()
+const electionsAddress = computed(() => {
+  const address = teamStore.currentTeam?.teamContracts?.find((c) => c.type === 'Elections')?.address
+  return address as Address
+})
+
+const {
+  data: voteCount,
+  // isLoading: isLoadingVoteCount,
+  error: errorGetVoteCount
+} = useReadContract({
+  functionName: 'getVoteCount',
+  address: electionsAddress.value,
+  abi: ELECTIONS_ABI,
+  args: [BigInt(election.id)] // Supply currentElectionId as an argument
+})
+
+const { data: electionResults, error: errorGetElectionResults } = useReadContract({
+  functionName: 'getElectionResults',
+  address: electionsAddress.value,
+  abi: ELECTIONS_ABI,
+  args: [BigInt(election.id)] // Supply currentElectionId as an argument
 })
 
 const formatDate = (date: Date) => {
@@ -60,8 +103,17 @@ const formatDate = (date: Date) => {
     year: 'numeric'
   })
 }
-</script>
 
-<style scoped>
-/* Optional custom styles */
-</style>
+watch(errorGetVoteCount, (newError) => {
+  if (newError) {
+    console.error('Error fetching vote count:', parseError(newError))
+    toastStore.addErrorToast('Failed to fetch vote count')
+  }
+})
+watch(errorGetElectionResults, (newError) => {
+  if (newError) {
+    console.error('Error fetching election results:', parseError(newError))
+    toastStore.addErrorToast('Failed to fetch election results')
+  }
+})
+</script>
