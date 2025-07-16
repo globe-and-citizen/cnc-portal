368f374a141bc256344f6346e47447e7320cc2f3
aliassan <alimkwananzi@gmail.com>
2025-07-16 12:32:39 +0200

    feat: add elected bod to details

--- app/src/views/team/[id]/BodElectionDetailsView.vue
+++ app/src/views/team/[id]/BodElectionDetailsView.vue
@@ -1,6 +1,6 @@
 <template>
-  <CurrentBoDElectionSection />
-  <CurrentBoDSection v-if="formattedElection?.resultsPublished" />
+  <CurrentBoDElectionSection :election-id="currentElectionId" />
+  <CurrentBoDSection v-if="formattedElection?.resultsPublished" :election-id="currentElectionId" />
   <ElectionDetailsSection />
 </template>
 
@@ -9,12 +9,19 @@ import CurrentBoDElectionSection from '@/components/sections/AdministrationView/
 import ElectionDetailsSection from '@/components/sections/AdministrationView/BoDElectionDetailsSection.vue'
 import CurrentBoDSection from '@/components/sections/AdministrationView/CurrentBoDSection.vue'
 import { useTeamStore } from '@/stores'
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useReadContract } from '@wagmi/vue'
 import { ELECTIONS_ABI } from '@/artifacts/abi/elections'
 import type { Address } from 'viem'
+import { useRouter } from 'vue-router'
 
 const teamStore = useTeamStore()
+const router = useRouter()
+const electionId = BigInt(
+  typeof router.currentRoute.value.query.electionId === 'string'
+    ? router.currentRoute.value.query.electionId
+    : 0
+)
 const electionsAddress = computed(() => {
   const address = teamStore.currentTeam?.teamContracts?.find((c) => c.type === 'Elections')?.address
   return address as Address
@@ -32,6 +39,8 @@ const {
 
 // Compute current election ID
 const currentElectionId = computed(() => {
+  if (typeof router.currentRoute.value.query.electionId === 'string')
+    return BigInt(router.currentRoute.value.query.electionId)
   if (
     nextElectionId.value &&
     (typeof nextElectionId.value === 'number' || typeof nextElectionId.value === 'bigint')
@@ -64,5 +73,8 @@ const formattedElection = computed(() => {
     seatCount: Number(raw[6]),
     resultsPublished: raw[7]
   }
+})
+onMounted(() => {
+  console.log('Election ID: ', electionId)
 })
 </script>
