38c510d38444f2cfff28ea5884254f76525b082d
aliassan <alimkwananzi@gmail.com>
2025-07-17 15:01:15 +0200

    chore: remove comments

--- app/src/components/sections/AdministrationView/ElectionStats.vue
+++ app/src/components/sections/AdministrationView/ElectionStats.vue
@@ -8,17 +8,6 @@
       color="blue"
       title="Candidates"
     />
-    <!-- <div class="flex-1 flex gap-4 p-4 bg-white rounded-lg shadow-sm border border-gray-300">
-      <div class="p-3 bg-blue-50 rounded-full">
-        <IconifyIcon icon="heroicons:user-group" class="h-6 w-6 text-blue-600" />
-      </div>
-      <div>
-        <p class="text-sm font-medium text-gray-500">Candidates</p>
-        <p class="text-xl font-semibold text-gray-900">
-          {{ formattedElection?.candidates }}
-        </p>
-      </div>
-    </div> -->
 
     <!-- Start Date Stat -->
     <ElectionStatsCard
@@ -27,19 +16,6 @@
       color="green"
       title="Starts"
     />
-    <!-- <div
-      class="flex-1 flex items-center gap-4 p-4 bg-white rounded-lg shadow-sm border border-gray-300"
-    >
-      <div class="p-3 bg-green-50 rounded-full">
-        <IconifyIcon icon="heroicons:calendar" class="h-6 w-6 text-green-600" />
-      </div>
-      <div>
-        <p class="text-sm font-medium text-gray-500">Starts</p>
-        <p class="text-xl font-semibold text-gray-900">
-          {{ formatDate(formattedElection?.startDate ?? new Date()) }}
-        </p>
-      </div>
-    </div> -->
 
     <!-- End Date Stat -->
     <ElectionStatsCard
@@ -48,19 +24,6 @@
       color="red"
       title="Ends"
     />
-    <!-- <div
-      class="flex-1 flex items-center gap-4 p-4 bg-white rounded-lg shadow-sm border border-gray-300"
-    >
-      <div class="p-3 bg-red-50 rounded-full">
-        <IconifyIcon icon="heroicons:calendar-days" class="h-6 w-6 text-red-600" />
-      </div>
-      <div>
-        <p class="text-sm font-medium text-gray-500">Ends</p>
-        <p class="text-xl font-semibold text-gray-900">
-          {{ formatDate(formattedElection?.endDate ?? new Date()) }}
-        </p>
-      </div>
-    </div> -->
 
     <!-- Votes Stat -->
     <ElectionStatsCard
@@ -69,19 +32,6 @@
       color="purple"
       title="Votes Cast"
     />
-    <!-- <div
-      class="flex-1 flex items-center gap-4 p-4 bg-white rounded-lg shadow-sm border border-gray-300"
-    >
-      <div class="p-3 bg-purple-50 rounded-full">
-        <IconifyIcon icon="heroicons:chart-bar" class="h-6 w-6 text-purple-600" />
-      </div>
-      <div>
-        <p class="text-sm font-medium text-gray-500">Votes Cast</p>
-        <p class="text-xl font-semibold text-gray-900">
-          {{ formattedElection?.votesCast }}
-        </p>
-      </div>
-    </div> -->
   </div>
 </template>
 <script setup lang="ts">
