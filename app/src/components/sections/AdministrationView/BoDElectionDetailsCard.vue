0422ccd24e0155a23a030778c8be6da0a2689e3a
aliassan <alimkwananzi@gmail.com>
2025-07-24 16:31:02 +0200

    refactor: remove svg icon

--- app/src/components/sections/AdministrationView/BoDElectionDetailsCard.vue
+++ app/src/components/sections/AdministrationView/BoDElectionDetailsCard.vue
@@ -6,23 +6,9 @@
     <!-- Winner Badge (aligned to straddle border) -->
     <div
       v-if="isElectionWinner"
-      class="absolute top-0 -translate-y-1/2 right-0 -translate-x-1/4 z-10"
+      class="absolute top-0 -translate-y-1/2 right-0 -translate-x-1/4 z-10 badge badge-warning badge-lg gap-2 shadow-lg border-2 border-base-100"
     >
-      <span class="badge badge-warning badge-lg gap-2 shadow-lg border-2 border-base-100">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-4 w-4"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-            clip-rule="evenodd"
-          />
-        </svg>
-        Winner
-      </span>
+      <span class=""> Winner </span>
     </div>
     <div class="card-body">
       <!-- User Component -->
