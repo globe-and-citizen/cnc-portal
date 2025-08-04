8dd1e3daf687cfe1a817cd8e1a9ec4ab3ac72910
Georginio-prod <etonameklou6@gmail.com>
2025-08-04 21:48:05 +0000

    feat: add TotalSupply component to display total supply information

---
+++ app/src/components/sections/SherTokenView/TotalSupply.vue
@@ -0,0 +1,28 @@
+<template>
+  <OverviewCard
+    :title="
+      totalSupply != null && tokenSymbol ? formatUnits(totalSupply, 6) + ' ' + tokenSymbol : '...'
+    "
+    subtitle="Total Supply"
+    variant="warning"
+    :card-icon="bagIcon"
+    :loading="!team.name"
+  >
+  </OverviewCard>
+</template>
+<script setup lang="ts">
+import { formatUnits } from 'viem'
+import OverviewCard from '@/components/OverviewCard.vue'
+import bagIcon from '@/assets/bag.svg'
+import type { Team } from '@/types'
+
+defineProps<{
+  team: Partial<Team>
+  tokenSymbol: string | undefined
+  totalSupply: bigint | undefined
+  tokenSymbolLoading: boolean
+  totalSupplyLoading: boolean
+  tokenBalance: bigint | undefined
+  loadingTokenBalance: boolean
+}>()
+</script>
