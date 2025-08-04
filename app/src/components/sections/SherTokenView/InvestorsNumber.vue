08ae411c4cf2a150aa31f3199299a689a334d83f
Georginio-prod <etonameklou6@gmail.com>
2025-08-04 20:50:00 +0000

    feat: add InvestorsNumber component to display investor count

---
+++ app/src/components/sections/SherTokenView/InvestorsNumber.vue
@@ -0,0 +1,27 @@
+<template>
+  <OverviewCard
+    :title="`${investorsCount} `"
+    subtitle="Investors"
+    variant="warning"
+    :card-icon="bagIcon"
+    :loading="!team.name"
+  >
+  </OverviewCard>
+</template>
+<script setup lang="ts">
+import OverviewCard from '@/components/OverviewCard.vue'
+import bagIcon from '@/assets/bag.svg'
+
+const props = defineProps<{
+  team: Partial<Team>
+  tokenSymbol: string | undefined
+  totalSupply: bigint | undefined
+  tokenSymbolLoading: boolean
+  totalSupplyLoading: boolean
+  tokenBalance: bigint | undefined
+  loadingTokenBalance: boolean
+  investorsCount: number
+}>()
+
+const investorsCount = props.investorsCount
+</script>
