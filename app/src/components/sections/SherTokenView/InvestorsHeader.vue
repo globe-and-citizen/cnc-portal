1438955761c298101e7a16fe7d75037f235022b2
Georginio-prod <etonameklou6@gmail.com>
2025-08-07 09:43:16 +0000

    refactor: replace CardComponent with OverviewCard for improved layout and consistency

--- app/src/components/sections/SherTokenView/InvestorsHeader.vue
+++ app/src/components/sections/SherTokenView/InvestorsHeader.vue
@@ -1,38 +1,45 @@
 <template>
-  <CardComponent title="Token Information">
-    <div class="flex justify-between">
-      <div class="flex flex-col text-right">
-        <h2>Balance</h2>
-        <p v-if="!loadingTokenBalance && !tokenSymbolLoading" data-test="token-balance">
-          <span class="text-3xl">{{ formatUnits(tokenBalance!, 6) }}</span>
-          {{ tokenSymbol }}
-        </p>
-        <span
-          v-if="loadingTokenBalance && tokenSymbolLoading"
-          class="loading loading-dots loading-sm self-end"
-          data-test="token-balance-loading"
-        ></span>
-      </div>
-      <div class="flex flex-col text-right gap-y-4">
-        <h3>Total Supply</h3>
-        <p v-if="!tokenSymbolLoading && !totalSupplyLoading" data-test="total-supply">
-          <span class="text-3xl">{{ formatUnits(totalSupply!, 6) }}</span>
-          {{ tokenSymbol }}
-        </p>
-        <span
-          v-if="tokenSymbolLoading && totalSupplyLoading"
-          class="loading loading-dots loading-sm self-end"
-          data-test="total-supply-loading"
-        ></span>
-      </div>
-    </div>
-  </CardComponent>
+  <div class="flex gap-10">
+    <OverviewCard
+      :title="`${investorsCount} Investors`"
+      subtitle="Investors"
+      variant="info"
+      :card-icon="personIcon"
+      :loading="!team.name"
+    >
+    </OverviewCard>
+    <OverviewCard
+      :title="
+        tokenBalance != null && tokenSymbol
+          ? formatUnits(tokenBalance, 6) + ' ' + tokenSymbol
+          : '...'
+      "
+      subtitle="Balance"
+      variant="success"
+      :card-icon="bagIcon"
+      :loading="!team.name"
+    >
+    </OverviewCard>
+    <OverviewCard
+      :title="
+        totalSupply != null && tokenSymbol ? formatUnits(totalSupply, 6) + ' ' + tokenSymbol : '...'
+      "
+      subtitle="Total Supply"
+      variant="warning"
+      :card-icon="cartIcon"
+      :loading="!team.name"
+    >
+    </OverviewCard>
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { Team } from '@/types'
 import { formatUnits } from 'viem'
-import CardComponent from '@/components/CardComponent.vue'
+import OverviewCard from '@/components/OverviewCard.vue'
+import cartIcon from '@/assets/cart.svg'
+import bagIcon from '@/assets/bag.svg'
+import personIcon from '@/assets/person.svg'
 
 defineProps<{
   team: Partial<Team>
@@ -42,5 +49,6 @@ defineProps<{
   totalSupplyLoading: boolean
   tokenBalance: bigint | undefined
   loadingTokenBalance: boolean
+  investorsCount: number
 }>()
 </script>
