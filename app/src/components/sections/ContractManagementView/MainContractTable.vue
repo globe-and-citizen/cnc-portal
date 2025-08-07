875c337510aa657c1d8a2457a34b906c826e616f
aliassan <alimkwananzi@gmail.com>
2025-08-07 14:43:41 +0200

    refactor: add main contract actions component

--- app/src/components/sections/ContractManagementView/MainContractTable.vue
+++ app/src/components/sections/ContractManagementView/MainContractTable.vue
@@ -2,7 +2,7 @@
   <div id="team-contracts" class="overflow-x-auto mt-4">
     <TableComponent
       :rows="
-        contracts.map((contract, index) => ({
+        teamContracts.map((contract, index) => ({
           ...contract,
           index: index + 1
         }))
@@ -12,43 +12,35 @@
         { key: 'type', label: 'Type' },
         { key: 'address', label: 'Contract Address' },
         { key: 'owner', label: 'Owner' },
-        { key: 'actions', label: 'Actions' } /*,
-        { key: 'events', label: 'Events' }*/
+        { key: 'actions', label: 'Actions' }
       ]"
     >
       <template #address-data="{ row }">
         <AddressToolTip :address="row.address" class="text-xs" />
       </template>
 
-      <template #owner-data>
+      <template #owner-data="{ row }">
         <UserComponent
           :user="
-            teamStore.currentTeam?.members.find(
-              (member) => member.address == userStore.address
-            ) as User
+            teamStore.currentTeam?.members.find((member) => member.address == row.owner) as User
           "
         />
       </template>
 
-      <template #actions-data>
-        <div class="flex items-center gap-2">
-          <ButtonUI variant="info" size="sm">
-            <IconifyIcon :icon="`heroicons:play-solid`" />
-          </ButtonUI>
-          <ButtonUI
-            variant="success"
-            :outline="true"
-            size="sm"
-            @click="() => console.log('Transferring ownership...')"
-            >Transfer Ownership</ButtonUI
-          >
-        </div>
+      <template #actions-data="{ row }">
+        <MainContractActions
+          @contract-status-changed="
+            async () => (teamContracts = (await getTeamContracts(contracts)) || [])
+          "
+          :row="row"
+        />
       </template>
     </TableComponent>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref, watch } from 'vue'
 import { Icon as IconifyIcon } from '@iconify/vue'
 import TableComponent from '@/components/TableComponent.vue'
 import ButtonUI from '@/components/ButtonUI.vue'
@@ -56,9 +48,47 @@ import UserComponent from '@/components/UserComponent.vue'
 import { useTeamStore, useUserDataStore } from '@/stores/'
 import { type TeamContract, type User } from '@/types'
 import AddressToolTip from '@/components/AddressToolTip.vue'
+import MainContractActions from './MainContractActions.vue'
+import { readContract } from '@wagmi/core'
+import { config } from '@/wagmi.config'
+import ExpenseAccountAbi from '@/artifacts/abi/expense-account-eip712.json'
+import { log, parseError, getTeamContracts } from '@/utils'
+import type { Abi } from 'viem'
 
 // Define props
-defineProps<{ contracts: TeamContract[]; teamId: string }>()
+const props = defineProps<{ contracts: TeamContract[]; teamId: string }>()
 const teamStore = useTeamStore()
 const userStore = useUserDataStore()
+const teamContracts = ref<Object[]>([])
+// const getTeamContracts = async () => {
+//   try {
+//     return Promise.all(
+//       props.contracts.filter(contract => contract.type === "ExpenseAccountEIP712").map(async (contract) => {
+//         const owner = await readContract(config, {
+//           address: contract.address,
+//           abi: ExpenseAccountAbi,
+//           functionName: 'owner'
+//         })
+
+//         return {
+//           ...contract,
+//           owner
+//         }
+//       })
+//     )
+//   } catch (error) {
+//     log.error('Error fetching contract owners: ', parseError(error, ExpenseAccountAbi as Abi))
+//   }
+// }
+
+watch(
+  () => props.contracts,
+  async (newContracts) => {
+    if (newContracts.length > 0) {
+      teamContracts.value = (await getTeamContracts(props.contracts)) || []
+      console.log('teamContracts.value: ', teamContracts.value)
+    }
+  },
+  { immediate: true }
+)
 </script>
