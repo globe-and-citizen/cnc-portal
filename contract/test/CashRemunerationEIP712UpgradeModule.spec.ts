60fbf2ec8655392d155b614d0a39157ae991a007
aliassan <alimkwananzi@gmail.com>
2025-08-13 12:31:52 +0200

    test: provide commented out template tests

--- contract/test/CashRemunerationEIP712UpgradeModule.spec.ts
+++ contract/test/CashRemunerationEIP712UpgradeModule.spec.ts
@@ -1,27 +1,23 @@
 // test/beaconUpgrade.test.ts
-import { ethers } from 'hardhat'
+// import { ethers } from 'hardhat'
 // import { expect } from 'chai'
-import { /*CashRemunerationEIP712,*/ FactoryBeacon } from '../typechain-types'
+// import { CashRemunerationEIP712, FactoryBeacon } from '../typechain-types'
 
 describe('CashRemunerationEIP712 Upgrade Test', function () {
   it('Should verify the implementation address after upgrade', async function () {
     // 1. Get the Beacon contract instance
-    const beaconAddress = '<beacon_address>' //use correct address for the network
-    const Beacon = (await ethers.getContractFactory('FactoryBeacon')) as unknown as FactoryBeacon
-    const beacon = Beacon.attach(beaconAddress) as unknown as FactoryBeacon
-
+    // const beaconAddress = '<beacon_address>' //use correct address for the network
+    // const Beacon = (await ethers.getContractFactory('FactoryBeacon')) as unknown as FactoryBeacon
+    // const beacon = Beacon.attach(beaconAddress) as unknown as FactoryBeacon
     // 2. Get the implementation address from the beacon
-    const implementationAddress = await beacon.implementation()
-
+    // const implementationAddress = await beacon.implementation()
     // 3. Verify the implementation address is correct
-    console.log('Current CashRemunerationEIP712implementation address:', implementationAddress)
-
+    // console.log('Current CashRemunerationEIP712implementation address:', implementationAddress)
     // Optional: Verify the implementation is the expected contract
     // const Implementation = await ethers.getContractFactory('CashRemunerationEIP712')
     // const implementation = Implementation.attach(
     //   implementationAddress
     // ) as unknown as CashRemunerationEIP712
-
     // Test some function to verify it's the right contract
     // Replace with an actual function from your implementation
     // const version = await implementation.getVersion();
--- contract/test/OfficerUpgradeModule.spec.ts
+++ contract/test/OfficerUpgradeModule.spec.ts
@@ -1,25 +1,20 @@
-// test/beaconUpgrade.test.ts
-import { ethers } from 'hardhat'
-import { expect } from 'chai'
-import { Officer, FactoryBeacon } from '../typechain-types'
+// import { ethers } from 'hardhat'
+// import { expect } from 'chai'
+// import { Officer, FactoryBeacon } from '../typechain-types'
 
 describe('Officer Upgrade Test', function () {
   it('Should verify the implementation address after upgrade', async function () {
     // 1. Get the Beacon contract instance
-    const beaconAddress = '<beacon_address>'
-    const Beacon = (await ethers.getContractFactory('FactoryBeacon')) as unknown as FactoryBeacon
-    const beacon = Beacon.attach(beaconAddress) as unknown as FactoryBeacon
-
+    // const beaconAddress = '<beacon_address>'
+    // const Beacon = (await ethers.getContractFactory('FactoryBeacon')) as unknown as FactoryBeacon
+    // const beacon = Beacon.attach(beaconAddress) as unknown as FactoryBeacon
     // 2. Get the implementation address from the beacon
-    const implementationAddress = await beacon.implementation()
-
+    // const implementationAddress = await beacon.implementation()
     // 3. Verify the implementation address is correct
-    console.log('Current Officer implementation address:', implementationAddress)
-
+    // console.log('Current Officer implementation address:', implementationAddress)
     // Optional: Verify the implementation is the expected contract
     // const Implementation = await ethers.getContractFactory('Officer')
     // const implementation = Implementation.attach(implementationAddress) as unknown as Officer
-
     // Test some function to verify it's the right contract
     // Replace with an actual function from your implementation
     // const version = await implementation.getVersion();
