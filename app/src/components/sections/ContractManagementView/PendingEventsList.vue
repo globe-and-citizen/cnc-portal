8d0f6d7fc792965ad68c8dc7ca5797957f521920
aliassan <alimkwananzi@gmail.com>
2025-08-18 16:20:27 +0200

    feat: add heading

--- app/src/components/sections/ContractManagementView/PendingEventsList.vue
+++ app/src/components/sections/ContractManagementView/PendingEventsList.vue
@@ -1,41 +1,57 @@
 <template>
-	<TableComponent 
-		:rows="pendingEvents"
-		:columns="columns"
-	>
-		<template #description-data="{ row }">
-			{{  row.description }}
-		</template>
+  <span class="text-lg font-semibold">Pending Actions</span>
+  <TableComponent :rows="pendingEvents" :columns="columns">
+    <template #description-data="{ row }">
+      {{ row.description }}
+    </template>
 
-		<template #requestedBy-data="{ row }">
-			{{ row.requestedBy }}
-		</template>
+    <template #requestedBy-data="{ row }">
+      {{ row.requestedBy }}
+    </template>
 
-		<template #dateCreated-data="{ row }">
-			{{ row.dateCreated }}
-		</template>
+    <template #dateCreated-data="{ row }">
+      {{ row.dateCreated }}
+    </template>
 
-		<template #actions-data="{ row }">
-			<ButtonUI variant="success" size="sm">Approve</ButtonUI>
-		</template>
-	</TableComponent>
+    <template #actions-data="{ row }">
+      <ButtonUI @click="emits('view-details', row)" variant="success" size="sm">Approve</ButtonUI>
+    </template>
+  </TableComponent>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
-import TableComponent from '@/components/TableComponent.vue';
-import ButtonUI from '@/components/ButtonUI.vue';
+import { ref } from 'vue'
+import TableComponent from '@/components/TableComponent.vue'
+import ButtonUI from '@/components/ButtonUI.vue'
+
+const emits = defineEmits(['view-details'])
 
 const pendingEvents = ref([
-	{ index: 1, description: 'Transfer ownership to BOD', requestedBy: 'User1', dateCreated: '2023-01-01' },
-	{ index: 2, description: 'Transfer ownership to Member', requestedBy: 'User2', dateCreated: '2023-01-02' },
-	{ index: 3, description: 'Transfer ownership to BOD', requestedBy: 'User3', dateCreated: '2023-01-03', }
+  {
+    index: 1,
+    description: 'Transfer ownership to BOD',
+    requestedBy: 'User1',
+    dateCreated: '2023-01-01'
+  },
+  {
+    index: 2,
+    description: 'Transfer ownership to Member',
+    requestedBy: 'User2',
+    dateCreated: '2023-01-02'
+  },
+  {
+    index: 3,
+    description: 'Transfer ownership to BOD',
+    requestedBy: 'User3',
+    dateCreated: '2023-01-03'
+  },
+  { index: 3, description: 'Transfer from bank', requestedBy: 'User3', dateCreated: '2023-01-03' }
 ])
 
-const columns=[
-	{ key: 'index', label: '#' },
-	{ key: 'description', label: 'Description' },
-	{ key: 'requestedBy', label: 'Requested By' },
-	{ key: 'dateCreated', label: 'Date Created' },
-	{ key: 'actions', label: 'Actions' }
+const columns = [
+  { key: 'index', label: '#' },
+  { key: 'description', label: 'Description' },
+  { key: 'requestedBy', label: 'Requested By' },
+  { key: 'dateCreated', label: 'Date Created' },
+  { key: 'actions', label: 'Actions' }
 ]
 </script>
