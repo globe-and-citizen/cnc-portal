828a08d5111ac7c4663c708676efd154b2f2f37e
aliassan <alimkwananzi@gmail.com>
2025-08-19 15:19:21 +0200

    feat add action to contract and backend

--- app/src/components/sections/ContractManagementView/MainContractActions.vue
+++ app/src/components/sections/ContractManagementView/MainContractActions.vue
@@ -5,7 +5,7 @@
       size="sm"
       @click="changeContractStatus(row.paused)"
       :loading="isLoadingPauseContract || isLoadingUnpauseContract"
-      :disabled="row.owner !== userDataStore.address"
+      :disabled="row.owner !== userDataStore.address && !isBodAction"
     >
       <IconifyIcon
         v-if="!isLoadingPauseContract && !isLoadingUnpauseContract"
@@ -17,7 +17,7 @@
       :outline="true"
       size="sm"
       @click="showModal = true"
-      :disabled="row.owner !== userDataStore.address"
+      :disabled="row.owner !== userDataStore.address && !isBodAction"
       >Transfer Ownership</ButtonUI
     >
     <ButtonUI
@@ -38,9 +38,14 @@
       <ModalComponent v-model="showModal">
         <TransferOwnershipForm
           v-if="showModal"
-          :is-bod-action="true"
+          :is-bod-action="isBodAction"
           @transfer-ownership="transferOwnership"
-          :loading="isLoadingTransferOwnership || isConfirmingTransferOwnership"
+          :loading="
+            isLoadingTransferOwnership ||
+            isConfirmingTransferOwnership ||
+            isLoadingAddAction ||
+            isConfirmingAddAction
+          "
         />
       </ModalComponent>
       <ModalComponent v-model="showApprovalModal" :modal-width="modalWidth">
@@ -61,16 +66,20 @@
 <script setup lang="ts">
 import { Icon as IconifyIcon } from '@iconify/vue'
 import ButtonUI from '@/components/ButtonUI.vue'
-import type { Abi, Address } from 'viem'
+import { encodeFunctionData, type Abi, type Address } from 'viem'
 import type { TableRow } from '@/components/TableComponent.vue'
 import { useWriteContract, useWaitForTransactionReceipt } from '@wagmi/vue'
-import { watch, ref, computed } from 'vue'
+import { watch, ref, computed, onMounted } from 'vue'
 import { useToastStore, useTeamStore, useUserDataStore } from '@/stores'
 import TransferOwnershipForm from './forms/TransferOwnershipForm.vue'
 import ModalComponent from '@/components/ModalComponent.vue'
 import { log, parseError } from '@/utils'
 import PendingEventsList from './PendingEventsList.vue'
 import BodApprovalModal from './BodApprovalModal.vue'
+import { readContract } from '@wagmi/core'
+import { config } from '@/wagmi.config'
+import BOD_ABI from '@/artifacts/abi/bod.json'
+import { useCustomFetch } from '@/composables'
 
 const props = defineProps<{
   row: TableRow
@@ -82,20 +91,32 @@ const teamStore = useTeamStore()
 const { addSuccessToast, addErrorToast } = useToastStore()
 
 const showModal = ref(false)
-// const showPendingActionsModal = ref(false)
 const showApprovalModal = ref(false)
 const selectedRow = ref<TableRow>({})
 const currentStep = ref<0 | 1 | 2>(0)
 const modalWidth = computed(() => {
   return currentStep.value === 1 ? 'w-1/2 max-w-4xl' : ''
 })
-
-// const
-// const isBodAction = computed(() => {
-//   return teamStore.
-// })
+const isBodAction = ref(false)
+const action = ref({})
 
 const userDataStore = useUserDataStore()
+
+const { error: errorSaveAction, execute: executeSaveAction } = useCustomFetch('actions/', {
+  immediate: false
+}).post(action)
+
+const {
+  data: hashAddAction,
+  writeContract: executeAddAction,
+  isPending: isLoadingAddAction,
+  error: errorAddAction
+} = useWriteContract()
+
+const { isLoading: isConfirmingAddAction, isSuccess: isConfirmedAddAction } =
+  useWaitForTransactionReceipt({
+    hash: hashAddAction
+  })
 
 const {
   data: hashTransferOwnership,
@@ -133,13 +154,44 @@ const { isLoading: isConfirmingUnpauseContract, isSuccess: isConfirmedUnpauseCon
     hash: hashUnpauseContract
   })
 
-const transferOwnership = (address: Address) => {
-  executeTransferOwnership({
-    address: props.row.address as Address,
-    abi: props.row.abi as Abi,
-    functionName: 'transferOwnership',
-    args: [address]
-  })
+const transferOwnership = async (address: Address) => {
+  if (isBodAction.value) {
+    const bodAddress = teamStore.getContractAddressByType('BoardOfDirectors')
+    if (!bodAddress) return
+    const actionId = await readContract(config, {
+      address: bodAddress,
+      abi: BOD_ABI,
+      functionName: 'actionCount'
+    })
+    const data = encodeFunctionData({
+      abi: props.row.abi as Abi,
+      functionName: 'transferOwnership',
+      args: [address]
+    })
+    const description = JSON.stringify({
+      text: `Transfer ownership of ${props.row.type} to ${address}`,
+      title: `Ownership Transfer Request`
+    })
+    executeAddAction({
+      address: teamStore.getContractAddressByType('BoardOfDirectors') as Address,
+      abi: BOD_ABI,
+      functionName: 'addAction',
+      args: [props.row.address, description, data]
+    })
+    action.value = {
+      teamId: teamStore.currentTeamId,
+      actionId: Number(actionId),
+      targetAddress: props.row.address,
+      description,
+      data
+    }
+  } else
+    executeTransferOwnership({
+      address: props.row.address as Address,
+      abi: props.row.abi as Abi,
+      functionName: 'transferOwnership',
+      args: [address]
+    })
 }
 
 const changeContractStatus = async (paused: boolean) => {
@@ -157,6 +209,13 @@ const changeContractStatus = async (paused: boolean) => {
     })
   }
 }
+
+watch(isConfirmingAddAction, async (isConfirming, wasConfirming) => {
+  if (wasConfirming && !isConfirming && isConfirmedAddAction.value) {
+    await executeSaveAction()
+    addSuccessToast('Action added successfully!')
+  }
+})
 
 watch(isConfirmingTransferOwnership, async (isConfirming, wasConfirming) => {
   if (wasConfirming && !isConfirming && isConfirmedTransferOwnership.value) {
@@ -198,5 +257,16 @@ watch(errorUnpauseContract, (error) => {
     addErrorToast(parseError(error, props.row.abi as Abi))
     log.error('errorUnpauseContract.value: ', error)
   }
+})
+
+onMounted(async () => {
+  const bodAddress = teamStore.getContractAddressByType('BoardOfDirectors')
+  const isMember = await readContract(config, {
+    address: bodAddress as Address,
+    abi: BOD_ABI,
+    functionName: 'isMember',
+    args: [userDataStore.address]
+  })
+  isBodAction.value = props.row.owner === bodAddress && isMember ? true : false
 })
 </script>
