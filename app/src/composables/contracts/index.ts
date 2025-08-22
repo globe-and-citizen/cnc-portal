4c74925e026063f58dd24bd5d7f576fc7555dcf1
K. Hermann EHO <hermannleboss@gmail.com>
2025-08-22 10:42:37 +0000

    feat: add generic and contract-specific writes composables

---
+++ app/src/composables/contracts/index.ts
@@ -0,0 +1,6 @@
+// Generic contract writes composable - can be used for any contract
+export { useContractWrites, type ContractWriteOptions, type ContractWriteConfig } from './useContractWrites'
+
+// Contract-specific writes composables
+export { useBankWrites } from '../bank/useBankWrites'
+export { useVestingWrites } from './useVestingWrites'
