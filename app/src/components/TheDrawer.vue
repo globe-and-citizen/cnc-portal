77ac01aaf7efe7e4d0aa5eed59dc74af3a9d8e6b
Georginio-prod <etonameklou6@gmail.com>
2025-08-24 13:02:42 +0000

    fix: correct cash remuneration address reference in TheDrawer component

--- app/src/components/TheDrawer.vue
+++ app/src/components/TheDrawer.vue
@@ -304,7 +304,7 @@ const cashRemunerationAddress = computed(() =>
 
 const { data: cashRemunerationOwner, error: cashRemunerationOwnerError } = useReadContract({
   functionName: 'owner',
-  address: cashRemunerationAddress.value,
+  address: cashRemunerationAddress,
   abi: CashRemuneration_ABI,
   enabled: computed(() => !!cashRemunerationAddress.value)
 })
