2da27870f4bc2718ed4e1cfe40171bce7b91ea4e
K. Hermann EHO <hermannleboss@gmail.com>
2025-09-15 12:48:52 +0000

    feat: enhance Sentry integration with session replay configuration

--- app/src/main.ts
+++ app/src/main.ts
@@ -39,10 +39,19 @@ export function setupApp() {
       Sentry.feedbackIntegration({
         // Additional SDK configuration goes in here, for example:
         colorScheme: 'system'
+      }),
+
+      Sentry.replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
       })
     ],
     tracesSampleRate: 0.1,
-    tracePropagationTargets: ['localhost', /^https:\/\/cncportal\.io/]
+    tracePropagationTargets: ['localhost', /^https:\/\/cncportal\.io/],
+    // Session Replay
+    replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+
   })
 
   return app
