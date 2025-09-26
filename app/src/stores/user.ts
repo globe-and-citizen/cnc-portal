aa3aea47fbda89ca3127f81d9b71d41384ec5689
Georginio-prod <etonameklou6@gmail.com>
2025-09-26 20:36:43 +0000

    feat: remove isNameGenerated handling from user data store

--- app/src/stores/user.ts
+++ app/src/stores/user.ts
@@ -7,14 +7,12 @@ export const useUserDataStore = defineStore('user', () => {
   const userNonce = useStorage('nonce', '')
   const userImageUrl = useStorage('imageUrl', '')
   const isAuth = useStorage('isAuth', false)
-  const isNameGenerated = useStorage('isNameGenerated', false)
 
   function setUserData(name: string, address: string, nonce: string, imageUrl: string) {
     userName.value = name
     userAddress.value = address
     userNonce.value = nonce
     userImageUrl.value = imageUrl
-    isNameGenerated.value = false
   }
 
   function clearUserData() {
@@ -22,7 +20,6 @@ export const useUserDataStore = defineStore('user', () => {
     userAddress.value = ''
     userNonce.value = ''
     isAuth.value = false // Reset authentication status as well if clearing user data
-    isNameGenerated.value = false
   }
 
   function setAuthStatus(status: boolean) {
@@ -34,7 +31,6 @@ export const useUserDataStore = defineStore('user', () => {
     address: userAddress,
     nonce: userNonce,
     imageUrl: userImageUrl,
-    isNameGenerated,
     isAuth,
     setUserData,
     clearUserData,
