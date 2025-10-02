cfdc0ea69fd26800a0d2044bc694cbf2445e2765
aliassan <alimkwananzi@gmail.com>
2025-10-02 17:57:28 +0200

    refactor: create approval details card

--- app/src/components/sections/ContractManagementView/BodApprovalDetails.vue
+++ app/src/components/sections/ContractManagementView/BodApprovalDetails.vue
@@ -2,7 +2,7 @@
   <!-- Details cards grid -->
   <div class="grid grid-cols-2 gap-3 auto-rows-fr">
     <!-- Amount Card -->
-    <div class="flex flex-col">
+    <!-- <div class="flex flex-col">
       <p class="text-sm font-medium text-blue-gray-700 mb-1">Amount</p>
       <div class="rounded-lg border border-blue-gray-200 bg-gray-50 p-3 flex-1">
         <p class="text-lg font-semibold text-gray-700">
@@ -12,10 +12,18 @@
           {{ NETWORK.currencySymbol }}
         </p>
       </div>
-    </div>
+    </div> -->
+    <BodApprovalDetailsCard title="Amount">
+      <p class="text-lg font-semibold text-gray-700">
+        {{
+          formatCryptoAmount(row.description.split(' ')[type === 'Pay Dividends Request' ? 3 : 1])
+        }}
+        {{ NETWORK.currencySymbol }}
+      </p>
+    </BodApprovalDetailsCard>
 
     <!-- Recipient Card -->
-    <div class="flex flex-col">
+    <!-- <div class="flex flex-col">
       <p class="text-sm font-medium text-blue-gray-700 mb-1">Recipient</p>
       <div class="rounded-lg border border-blue-gray-200 bg-gray-50 p-3 flex-1">
         <UserComponent
@@ -27,23 +35,39 @@
           "
         />
       </div>
-    </div>
+    </div> -->
+    <BodApprovalDetailsCard title="Recipient">
+      <UserComponent
+        :user="
+          getUser(
+            row.description.split(' ')[type === 'Pay Dividends Request' ? 5 : 4].trim(),
+            teamStore.currentTeam?.members || []
+          )
+        "
+      />
+    </BodApprovalDetailsCard>
 
     <!-- Requestor Card -->
-    <div class="flex flex-col">
+    <!-- <div class="flex flex-col">
       <p class="text-sm font-medium text-blue-gray-700 mb-1">Requestor</p>
       <div class="rounded-lg border border-blue-gray-200 bg-gray-50 p-3 flex-1">
         <UserComponent :user="row.requestedBy" />
       </div>
-    </div>
+    </div> -->
+    <BodApprovalDetailsCard title="Requestor">
+      <UserComponent :user="row.requestedBy" />
+    </BodApprovalDetailsCard>
 
     <!-- Request Date Card -->
-    <div class="flex flex-col">
+    <!-- <div class="flex flex-col">
       <p class="text-sm font-medium text-blue-gray-700 mb-1">Request Date</p>
       <div class="rounded-lg border border-blue-gray-200 bg-gray-50 p-3 flex-1">
         <p class="text-lg font-semibold text-gray-600">{{ row.dateCreated }}</p>
       </div>
-    </div>
+    </div> -->
+    <BodApprovalDetailsCard title="Request Date">
+      <p class="text-lg font-semibold text-gray-600">{{ row.dateCreated }}</p>
+    </BodApprovalDetailsCard>
   </div>
 </template>
 <script setup lang="ts">
@@ -52,6 +76,7 @@ import { useTeamStore } from '@/stores'
 import type { TableRow } from '@/components/TableComponent.vue'
 import { getUser, formatCryptoAmount } from '@/utils'
 import { NETWORK } from '@/constant'
+import BodApprovalDetailsCard from './BodApprovalDetailsCard.vue'
 
 defineProps<{ row: TableRow; type: 'Bank Transfer Request' | 'Pay Dividends Request' }>()
 const teamStore = useTeamStore()
