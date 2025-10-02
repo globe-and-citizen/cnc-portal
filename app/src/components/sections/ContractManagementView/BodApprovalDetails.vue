d1c062bddc62a4f8c00a71ab4f11162778df363e
aliassan <alimkwananzi@gmail.com>
2025-10-02 18:03:07 +0200

    chore: remove comments

--- app/src/components/sections/ContractManagementView/BodApprovalDetails.vue
+++ app/src/components/sections/ContractManagementView/BodApprovalDetails.vue
@@ -1,18 +1,6 @@
 <template>
-  <!-- Details cards grid -->
   <div class="grid grid-cols-2 gap-3 auto-rows-fr">
-    <!-- Amount Card -->
-    <!-- <div class="flex flex-col">
-      <p class="text-sm font-medium text-blue-gray-700 mb-1">Amount</p>
-      <div class="rounded-lg border border-blue-gray-200 bg-gray-50 p-3 flex-1">
-        <p class="text-lg font-semibold text-gray-700">
-          {{
-            formatCryptoAmount(row.description.split(' ')[type === 'Pay Dividends Request' ? 3 : 1])
-          }}
-          {{ NETWORK.currencySymbol }}
-        </p>
-      </div>
-    </div> -->
+    <!-- First Card -->
     <BodApprovalDetailsCard title="Amount">
       <p class="text-lg font-semibold text-gray-700">
         {{
@@ -22,20 +10,7 @@
       </p>
     </BodApprovalDetailsCard>
 
-    <!-- Recipient Card -->
-    <!-- <div class="flex flex-col">
-      <p class="text-sm font-medium text-blue-gray-700 mb-1">Recipient</p>
-      <div class="rounded-lg border border-blue-gray-200 bg-gray-50 p-3 flex-1">
-        <UserComponent
-          :user="
-            getUser(
-              row.description.split(' ')[type === 'Pay Dividends Request' ? 5 : 4].trim(),
-              teamStore.currentTeam?.members || []
-            )
-          "
-        />
-      </div>
-    </div> -->
+    <!-- Second Card -->
     <BodApprovalDetailsCard title="Recipient">
       <UserComponent
         :user="
@@ -47,24 +22,12 @@
       />
     </BodApprovalDetailsCard>
 
-    <!-- Requestor Card -->
-    <!-- <div class="flex flex-col">
-      <p class="text-sm font-medium text-blue-gray-700 mb-1">Requestor</p>
-      <div class="rounded-lg border border-blue-gray-200 bg-gray-50 p-3 flex-1">
-        <UserComponent :user="row.requestedBy" />
-      </div>
-    </div> -->
+    <!-- Third Card -->
     <BodApprovalDetailsCard title="Requestor">
       <UserComponent :user="row.requestedBy" />
     </BodApprovalDetailsCard>
 
-    <!-- Request Date Card -->
-    <!-- <div class="flex flex-col">
-      <p class="text-sm font-medium text-blue-gray-700 mb-1">Request Date</p>
-      <div class="rounded-lg border border-blue-gray-200 bg-gray-50 p-3 flex-1">
-        <p class="text-lg font-semibold text-gray-600">{{ row.dateCreated }}</p>
-      </div>
-    </div> -->
+    <!-- Fourth Card -->
     <BodApprovalDetailsCard title="Request Date">
       <p class="text-lg font-semibold text-gray-600">{{ row.dateCreated }}</p>
     </BodApprovalDetailsCard>
