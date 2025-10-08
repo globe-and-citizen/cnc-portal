2c62671151de34826a64ea08d75750ca4627b3ac
achille <achillegley@gmail.com>
2025-10-08 13:49:42 +0700

    correct format check

--- app/src/artifacts/abi/officer.ts
+++ app/src/artifacts/abi/officer.ts
@@ -1,446 +1,446 @@
-import type { Abi } from "viem";
+import type { Abi } from 'viem'
 
 export const OFFICER_ABI = [
   {
-    "inputs": [],
-    "name": "EnforcedPause",
-    "type": "error"
+    inputs: [],
+    name: 'EnforcedPause',
+    type: 'error'
   },
   {
-    "inputs": [],
-    "name": "ExpectedPause",
-    "type": "error"
+    inputs: [],
+    name: 'ExpectedPause',
+    type: 'error'
   },
   {
-    "inputs": [],
-    "name": "InvalidInitialization",
-    "type": "error"
+    inputs: [],
+    name: 'InvalidInitialization',
+    type: 'error'
   },
   {
-    "inputs": [],
-    "name": "NotInitializing",
-    "type": "error"
+    inputs: [],
+    name: 'NotInitializing',
+    type: 'error'
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "owner",
-        "type": "address"
+        internalType: 'address',
+        name: 'owner',
+        type: 'address'
       }
     ],
-    "name": "OwnableInvalidOwner",
-    "type": "error"
+    name: 'OwnableInvalidOwner',
+    type: 'error'
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "account",
-        "type": "address"
+        internalType: 'address',
+        name: 'account',
+        type: 'address'
       }
     ],
-    "name": "OwnableUnauthorizedAccount",
-    "type": "error"
+    name: 'OwnableUnauthorizedAccount',
+    type: 'error'
   },
   {
-    "inputs": [],
-    "name": "ReentrancyGuardReentrantCall",
-    "type": "error"
+    inputs: [],
+    name: 'ReentrancyGuardReentrantCall',
+    type: 'error'
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": false,
-        "internalType": "string",
-        "name": "contractType",
-        "type": "string"
+        indexed: false,
+        internalType: 'string',
+        name: 'contractType',
+        type: 'string'
       },
       {
-        "indexed": false,
-        "internalType": "address",
-        "name": "beaconAddress",
-        "type": "address"
+        indexed: false,
+        internalType: 'address',
+        name: 'beaconAddress',
+        type: 'address'
       }
     ],
-    "name": "BeaconConfigured",
-    "type": "event"
+    name: 'BeaconConfigured',
+    type: 'event'
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": false,
-        "internalType": "address[]",
-        "name": "beaconProxies",
-        "type": "address[]"
+        indexed: false,
+        internalType: 'address[]',
+        name: 'beaconProxies',
+        type: 'address[]'
       }
     ],
-    "name": "BeaconProxiesDeployed",
-    "type": "event"
+    name: 'BeaconProxiesDeployed',
+    type: 'event'
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": false,
-        "internalType": "string",
-        "name": "contractType",
-        "type": "string"
+        indexed: false,
+        internalType: 'string',
+        name: 'contractType',
+        type: 'string'
       },
       {
-        "indexed": false,
-        "internalType": "address",
-        "name": "deployedAddress",
-        "type": "address"
+        indexed: false,
+        internalType: 'address',
+        name: 'deployedAddress',
+        type: 'address'
       }
     ],
-    "name": "ContractDeployed",
-    "type": "event"
+    name: 'ContractDeployed',
+    type: 'event'
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": false,
-        "internalType": "uint64",
-        "name": "version",
-        "type": "uint64"
+        indexed: false,
+        internalType: 'uint64',
+        name: 'version',
+        type: 'uint64'
       }
     ],
-    "name": "Initialized",
-    "type": "event"
+    name: 'Initialized',
+    type: 'event'
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": true,
-        "internalType": "address",
-        "name": "previousOwner",
-        "type": "address"
+        indexed: true,
+        internalType: 'address',
+        name: 'previousOwner',
+        type: 'address'
       },
       {
-        "indexed": true,
-        "internalType": "address",
-        "name": "newOwner",
-        "type": "address"
+        indexed: true,
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address'
       }
     ],
-    "name": "OwnershipTransferred",
-    "type": "event"
+    name: 'OwnershipTransferred',
+    type: 'event'
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": false,
-        "internalType": "address",
-        "name": "account",
-        "type": "address"
+        indexed: false,
+        internalType: 'address',
+        name: 'account',
+        type: 'address'
       }
     ],
-    "name": "Paused",
-    "type": "event"
+    name: 'Paused',
+    type: 'event'
   },
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": false,
-        "internalType": "address",
-        "name": "account",
-        "type": "address"
+        indexed: false,
+        internalType: 'address',
+        name: 'account',
+        type: 'address'
       }
     ],
-    "name": "Unpaused",
-    "type": "event"
+    name: 'Unpaused',
+    type: 'event'
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "string",
-        "name": "contractType",
-        "type": "string"
+        internalType: 'string',
+        name: 'contractType',
+        type: 'string'
       },
       {
-        "internalType": "address",
-        "name": "beaconAddress",
-        "type": "address"
+        internalType: 'address',
+        name: 'beaconAddress',
+        type: 'address'
       }
     ],
-    "name": "configureBeacon",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: 'configureBeacon',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
+        internalType: 'string',
+        name: '',
+        type: 'string'
       }
     ],
-    "name": "contractBeacons",
-    "outputs": [
+    name: 'contractBeacons',
+    outputs: [
       {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
+        internalType: 'address',
+        name: '',
+        type: 'address'
       }
     ],
-    "stateMutability": "view",
-    "type": "function"
+    stateMutability: 'view',
+    type: 'function'
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256'
       }
     ],
-    "name": "contractTypes",
-    "outputs": [
+    name: 'contractTypes',
+    outputs: [
       {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
+        internalType: 'string',
+        name: '',
+        type: 'string'
       }
     ],
-    "stateMutability": "view",
-    "type": "function"
+    stateMutability: 'view',
+    type: 'function'
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "components": [
+        components: [
           {
-            "internalType": "string",
-            "name": "contractType",
-            "type": "string"
+            internalType: 'string',
+            name: 'contractType',
+            type: 'string'
           },
           {
-            "internalType": "bytes",
-            "name": "initializerData",
-            "type": "bytes"
+            internalType: 'bytes',
+            name: 'initializerData',
+            type: 'bytes'
           }
         ],
-        "internalType": "struct DeploymentData[]",
-        "name": "deployments",
-        "type": "tuple[]"
+        internalType: 'struct DeploymentData[]',
+        name: 'deployments',
+        type: 'tuple[]'
       }
     ],
-    "name": "deployAllContracts",
-    "outputs": [
+    name: 'deployAllContracts',
+    outputs: [
       {
-        "internalType": "address[]",
-        "name": "",
-        "type": "address[]"
+        internalType: 'address[]',
+        name: '',
+        type: 'address[]'
       }
     ],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    stateMutability: 'nonpayable',
+    type: 'function'
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "string",
-        "name": "contractType",
-        "type": "string"
+        internalType: 'string',
+        name: 'contractType',
+        type: 'string'
       },
       {
-        "internalType": "bytes",
-        "name": "initializerData",
-        "type": "bytes"
+        internalType: 'bytes',
+        name: 'initializerData',
+        type: 'bytes'
       }
     ],
-    "name": "deployBeaconProxy",
-    "outputs": [
+    name: 'deployBeaconProxy',
+    outputs: [
       {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
+        internalType: 'address',
+        name: '',
+        type: 'address'
       }
     ],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    stateMutability: 'nonpayable',
+    type: 'function'
   },
   {
-    "inputs": [],
-    "name": "getConfiguredContractTypes",
-    "outputs": [
+    inputs: [],
+    name: 'getConfiguredContractTypes',
+    outputs: [
       {
-        "internalType": "string[]",
-        "name": "",
-        "type": "string[]"
+        internalType: 'string[]',
+        name: '',
+        type: 'string[]'
       }
     ],
-    "stateMutability": "view",
-    "type": "function"
+    stateMutability: 'view',
+    type: 'function'
   },
   {
-    "inputs": [],
-    "name": "getDeployedContracts",
-    "outputs": [
+    inputs: [],
+    name: 'getDeployedContracts',
+    outputs: [
       {
-        "components": [
+        components: [
           {
-            "internalType": "string",
-            "name": "contractType",
-            "type": "string"
+            internalType: 'string',
+            name: 'contractType',
+            type: 'string'
           },
           {
-            "internalType": "address",
-            "name": "contractAddress",
-            "type": "address"
+            internalType: 'address',
+            name: 'contractAddress',
+            type: 'address'
           }
         ],
-        "internalType": "struct Officer.DeployedContract[]",
-        "name": "",
-        "type": "tuple[]"
+        internalType: 'struct Officer.DeployedContract[]',
+        name: '',
+        type: 'tuple[]'
       }
     ],
-    "stateMutability": "view",
-    "type": "function"
+    stateMutability: 'view',
+    type: 'function'
   },
   {
-    "inputs": [],
-    "name": "getTeam",
-    "outputs": [
+    inputs: [],
+    name: 'getTeam',
+    outputs: [
       {
-        "components": [
+        components: [
           {
-            "internalType": "string",
-            "name": "contractType",
-            "type": "string"
+            internalType: 'string',
+            name: 'contractType',
+            type: 'string'
           },
           {
-            "internalType": "address",
-            "name": "contractAddress",
-            "type": "address"
+            internalType: 'address',
+            name: 'contractAddress',
+            type: 'address'
           }
         ],
-        "internalType": "struct Officer.DeployedContract[]",
-        "name": "",
-        "type": "tuple[]"
+        internalType: 'struct Officer.DeployedContract[]',
+        name: '',
+        type: 'tuple[]'
       }
     ],
-    "stateMutability": "view",
-    "type": "function"
+    stateMutability: 'view',
+    type: 'function'
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "_owner",
-        "type": "address"
+        internalType: 'address',
+        name: '_owner',
+        type: 'address'
       },
       {
-        "components": [
+        components: [
           {
-            "internalType": "string",
-            "name": "beaconType",
-            "type": "string"
+            internalType: 'string',
+            name: 'beaconType',
+            type: 'string'
           },
           {
-            "internalType": "address",
-            "name": "beaconAddress",
-            "type": "address"
+            internalType: 'address',
+            name: 'beaconAddress',
+            type: 'address'
           }
         ],
-        "internalType": "struct Officer.BeaconConfig[]",
-        "name": "beaconConfigs",
-        "type": "tuple[]"
+        internalType: 'struct Officer.BeaconConfig[]',
+        name: 'beaconConfigs',
+        type: 'tuple[]'
       },
       {
-        "components": [
+        components: [
           {
-            "internalType": "string",
-            "name": "contractType",
-            "type": "string"
+            internalType: 'string',
+            name: 'contractType',
+            type: 'string'
           },
           {
-            "internalType": "bytes",
-            "name": "initializerData",
-            "type": "bytes"
+            internalType: 'bytes',
+            name: 'initializerData',
+            type: 'bytes'
           }
         ],
-        "internalType": "struct DeploymentData[]",
-        "name": "_deployments",
-        "type": "tuple[]"
+        internalType: 'struct DeploymentData[]',
+        name: '_deployments',
+        type: 'tuple[]'
       },
       {
-        "internalType": "bool",
-        "name": "_isDeployAllContracts",
-        "type": "bool"
+        internalType: 'bool',
+        name: '_isDeployAllContracts',
+        type: 'bool'
       }
     ],
-    "name": "initialize",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: 'initialize',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
   },
   {
-    "inputs": [],
-    "name": "owner",
-    "outputs": [
+    inputs: [],
+    name: 'owner',
+    outputs: [
       {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
+        internalType: 'address',
+        name: '',
+        type: 'address'
       }
     ],
-    "stateMutability": "view",
-    "type": "function"
+    stateMutability: 'view',
+    type: 'function'
   },
   {
-    "inputs": [],
-    "name": "pause",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    inputs: [],
+    name: 'pause',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
   },
   {
-    "inputs": [],
-    "name": "paused",
-    "outputs": [
+    inputs: [],
+    name: 'paused',
+    outputs: [
       {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
+        internalType: 'bool',
+        name: '',
+        type: 'bool'
       }
     ],
-    "stateMutability": "view",
-    "type": "function"
+    stateMutability: 'view',
+    type: 'function'
   },
   {
-    "inputs": [],
-    "name": "renounceOwnership",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    inputs: [],
+    name: 'renounceOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "newOwner",
-        "type": "address"
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address'
       }
     ],
-    "name": "transferOwnership",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: 'transferOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
   },
   {
-    "inputs": [],
-    "name": "unpause",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    inputs: [],
+    name: 'unpause',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
   }
-] as const satisfies Abi;
+] as const satisfies Abi
