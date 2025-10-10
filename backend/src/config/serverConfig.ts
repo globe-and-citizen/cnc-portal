643949f8e7612335a81ffebad3c6d2d1fb3a8b3b
aliassan <alimkwananzi@gmail.com>
2025-10-10 17:14:03 +0200

    feat: create elections router

--- backend/src/config/serverConfig.ts
+++ backend/src/config/serverConfig.ts
@@ -7,6 +7,7 @@ import rateLimit from 'express-rate-limit';
 //#endregion networking modules
 
 //#region routing modules
+
 import teamRoutes from '../routes/teamRoutes';
 import userRoutes from '../routes/userRoutes';
 import authRoutes from '../routes/authRoutes';
@@ -18,6 +19,7 @@ import weeklyClaimRoutes from '../routes/weeklyClaimRoute';
 import expenseRoutes from '../routes/expenseRoute';
 import uploadRoute from '../routes/uploadRoute';
 import contractRoutes from '../routes/contractRoutes';
+import electionsRoute from '../routes/electionsRoute';
 //#endregion routing modules
 
 import { authorizeUser } from '../middleware/authMiddleware';
@@ -65,6 +67,7 @@ class Server {
       claim: '/api/claim/',
       upload: '/api/upload/',
       constract: '/api/contract/',
+      elections: '/api/elections/',
     };
     const limiter = rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
@@ -112,6 +115,8 @@ class Server {
     this.app.use(this.paths.upload, authorizeUser, uploadRoute);
     this.app.use(this.paths.weeklyClaim, authorizeUser, weeklyClaimRoutes);
     this.app.use(this.paths.constract, authorizeUser, contractRoutes);
+
+    this.app.use(this.paths.elections, authorizeUser, electionsRoute);
     this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     // The error handler must be registered before any other error middleware and after all controllers
     Sentry.setupExpressErrorHandler(this.app);
