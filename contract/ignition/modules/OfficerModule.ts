2e4186e2bc93a1fe5b1df1e20d9721bab2eb2cac
Hermann EHO <hermannleboss@gmail.com>
2025-10-19 10:24:09 +0000

    refactor: Remove unused modules and commented-out code in OfficerModule

--- contract/ignition/modules/OfficerModule.ts
+++ contract/ignition/modules/OfficerModule.ts
@@ -1,7 +1,7 @@
 import boardOfDirectorsBeaconModule from './BoardOfDirectorsBeaconModule'
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
 import bankBeaconModule from './BankBeaconModule'
-import ExpenseAccountModule from './ExpenseAccountModule'
+// import ExpenseAccountModule from './ExpenseAccountModule'
 import ExpenseAccountEIP712Module from './ExpenseAccountEIP712Module'
 import investorsV1BeaconModule from './InvestorsV1BeaconModule'
 import CashRemunerationEIP712Module from './CashRemunerationEIP712Module'
@@ -16,7 +16,7 @@ export default buildModule('Officer', (m) => {
   const { beacon: proposalBeacon } = m.useModule(proposalBeaconModule)
   const { beacon: electionsBeacon } = m.useModule(electionsBeaconModule)
   const { beacon: investorsV1Beacon } = m.useModule(investorsV1BeaconModule)
-  const { expenseAccountFactoryBeacon } = m.useModule(ExpenseAccountModule)
+  // const { expenseAccountFactoryBeacon } = m.useModule(ExpenseAccountModule)
   const { expenseAccountEip712FactoryBeacon } = m.useModule(ExpenseAccountEIP712Module)
   const { cashRemunerationEip712FactoryBeacon } = m.useModule(CashRemunerationEIP712Module)
 
@@ -25,7 +25,7 @@ export default buildModule('Officer', (m) => {
     { beaconType: 'Elections', beaconAddress: electionsBeacon },
     { beaconType: 'Proposals', beaconAddress: proposalBeacon },
     { beaconType: 'BoardOfDirectors', beaconAddress: BoDBeacon },
-    { beaconType: 'ExpenseAccount', beaconAddress: expenseAccountFactoryBeacon },
+    // { beaconType: 'ExpenseAccount', beaconAddress: expenseAccountFactoryBeacon },
     { beaconType: 'ExpenseAccountEIP712', beaconAddress: expenseAccountEip712FactoryBeacon },
     { beaconType: 'InvestorsV1', beaconAddress: investorsV1Beacon },
     { beaconType: 'CashRemunerationEIP712', beaconAddress: cashRemunerationEip712FactoryBeacon }
