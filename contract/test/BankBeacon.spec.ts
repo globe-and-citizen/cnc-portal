bab49824b5bb7527c472a96979192d4d7ef916ec
Hermann EHO <hermannleboss@gmail.com>
2025-10-20 08:17:02 +0000

    refactor: remove unused imports in BankBeacon test file

--- contract/test/BankBeacon.spec.ts
+++ contract/test/BankBeacon.spec.ts
@@ -1,4 +1,4 @@
-import { ethers, upgrades } from 'hardhat'
+import { ethers } from 'hardhat'
 import { expect } from 'chai'
 import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers'
 
