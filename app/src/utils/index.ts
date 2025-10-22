af9933ad78f5ce7945bb3e75df9eccb3844850c7
Hermann EHO <hermannleboss@gmail.com>
2025-10-22 14:58:27 +0000

    feat: add waitForCondition utility function for condition checking

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -4,3 +4,24 @@ export * from './constantUtil'
 export * from './currencyUtil'
 export * from './expenseUtil'
 export * from './contractManagementUtil'
+/**
+ * Utility function to wait for a condition to be met
+ * @description This function repeatedly checks a condition until it returns true or a timeout occurs.
+ * @param condition () => boolean - A function that returns a boolean indicating whether the condition is met.
+ * @param timeout
+ */
+export const waitForCondition = (condition: () => boolean, timeout = 5000) => {
+  return new Promise((resolve, reject) => {
+    const startTime = Date.now()
+    const interval = setInterval(() => {
+      console.log('Checking condition...')
+      if (condition()) {
+        clearInterval(interval)
+        resolve(true)
+      } else if (Date.now() - startTime > timeout) {
+        clearInterval(interval)
+        reject(new Error('Condition not met within timeout'))
+      }
+    }, 1000)
+  })
+}
