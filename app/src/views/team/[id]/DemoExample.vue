1870280a95965f9174186b47ec77e070999e8aaa
Hermann EHO <hermannleboss@gmail.com>
2025-11-07 10:27:18 +0000

    refactor: update pause and unpause button functionality to use correct result references and improve status display

--- app/src/views/team/[id]/DemoExample.vue
+++ app/src/views/team/[id]/DemoExample.vue
@@ -1,52 +1,51 @@
 <template>
   <div>
     <div class="flex gap-8">
-      <ButtonUI variant="primary" @click="executeWrite()">Pause Bank</ButtonUI>
-      <ButtonUI variant="secondary" @click="executeUnPause()">Unpause Bank</ButtonUI>
+      <ButtonUI variant="primary" @click="pauseResult.executeWrite()">Pause Bank</ButtonUI>
+      <ButtonUI variant="secondary" @click="unpauseResult.executeWrite()">Unpause Bank</ButtonUI>
     </div>
     <h2>
-      Bank paused Status is <span class="text-red-500">{{ pauseResult.data }}</span>
+      Bank paused Status is <span class="text-red-500">{{ pauseStatus.data }}</span>
     </h2>
-    <!-- Timeline Demo -->
     <div class="card bg-base-100 shadow-xl">
       <div class="card-body">
         <h2 class="card-title mb-6">Bank Pause Operation</h2>
 
         <div class="flex flex-row">
           <TransactionTimeline
+            v-if="pauseResult.timelineSteps.value"
             :show="true"
-            :steps="pauseTimelineSteps"
+            :steps="pauseResult.timelineSteps.value"
             title="Bank Pause Operation"
           />
           <TransactionTimeline
+            v-if="unpauseResult.timelineSteps.value"
             :show="true"
-            :steps="unpauseTimelineSteps"
+            :steps="unpauseResult.timelineSteps.value"
             title="Bank Un Pause Operation"
           />
         </div>
       </div>
     </div>
     <div class="flex">
-      <pre>{{ pauseResult }}</pre>
+      <pre>{{ pauseStatus }}</pre>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import ButtonUI from '@/components/ButtonUI.vue'
-import TransactionTimeline from '@/components/ui/TransactionTimeline.vue'
 import { useBankReads } from '@/composables/bank'
 import { usePause, useUnpause } from '@/composables/bank/bankWrites'
-// import { useTransactionTimeline } from '@/composables/useTransactionTimeline'
+import ButtonUI from '@/components/ButtonUI.vue'
+import TransactionTimeline from '@/components/ui/TransactionTimeline.vue'
 
 const { useBankPaused } = useBankReads()
 
 // Get contract data with loading and error states
-const pauseResult = useBankPaused()
+const pauseStatus = useBankPaused()
+const pauseResult = usePause()
+const unpauseResult = useUnpause()
 
-const { timelineSteps: pauseTimelineSteps, executeWrite } = usePause()
-
-const { executeWrite: executeUnPause, timelineSteps: unpauseTimelineSteps } = useUnpause()
 </script>
 
 <style scoped></style>
