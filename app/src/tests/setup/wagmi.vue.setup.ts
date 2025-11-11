f8e4c80e8e87d355935cfdb943ad8214afdedbc7
aliassan <alimkwananzi@gmail.com>
2025-11-11 17:14:28 +0200

    test: mock useWiteContract

--- app/src/tests/setup/wagmi.vue.setup.ts
+++ app/src/tests/setup/wagmi.vue.setup.ts
@@ -2,19 +2,13 @@ import { vi } from 'vitest'
 import * as mocks from '@/tests/mocks/wagmi.vue.mock'
 import { ref } from 'vue'
 
-console.log('🟢 GLOBAL WAGMI SETUP FILE EXECUTING') // You should see this
-
 vi.mock('@wagmi/vue', async (importOriginal) => {
-  console.log('🟢 GLOBAL WAGMI MOCK SETUP') // You should see this too
-
   const actual: object = await importOriginal()
   return {
     ...actual,
     useChainId: vi.fn(() => ref(1)),
     useReadContract: vi.fn(() => ({ ...mocks.mockUseReadContract })),
-    useSignTypedData: vi.fn(() => {
-      console.log('🟢 MOCKED WAGMI FUNCTION EXECUTED')
-      return { ...mocks.mockUseSignTypedData }
-    })
+    useSignTypedData: vi.fn(() => ({ ...mocks.mockUseSignTypedData })),
+    useWriteContract: vi.fn(() => ({ ...mocks.mockUseWriteContract }))
   }
 })
