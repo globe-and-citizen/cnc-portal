8b84927439743f9aa667881d63437e8fb762b06d
Hermann EHO <hermannleboss@gmail.com>
2025-11-21 11:15:11 +0000

    feat(dashboard): initialize dashboard project with Nuxt.js setup
    - Add package.json with scripts and dependencies for Nuxt.js
    - Include favicon.ico for branding
    - Create robots.txt to manage web crawler access
    - Set up TypeScript configuration with references to Nuxt.js generated files

---
+++ dashboard/nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/hints',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui'
+  ]
+})
