d560dfb90d592404d06621454181221affe4f2a3
aliassan <alimkwananzi@gmail.com>
2025-11-25 16:59:29 +0200

    feat: update budget limit type

--- backend/src/types/expense-account.ts
+++ backend/src/types/expense-account.ts
@@ -1,11 +1,15 @@
 import { Prisma } from '@prisma/client';
 
 export type BudgetLimit = Prisma.JsonObject & {
+  amount: number | string;
+  frequencyType: number;
+  customFrequency: number | string;
   approvedAddress: string;
-  budgetData: Array<{
-    budgetType: number;
-    value: number;
-  }>;
-  expiry: number;
+  // budgetData: Array<{
+  //   budgetType: number;
+  //   value: number;
+  // }>;
+  startDate: number;
+  endDate: number;
   tokenAddress: string;
 };
