cebe9d345c1e6312e34300a0199cbd310ebba70a
aliassan <alimkwananzi@gmail.com>
2025-11-26 14:37:32 +0200

    feat: update table table to use new limit

--- app/src/components/sections/ExpenseAccountView/ExpenseAccountTable.vue
+++ app/src/components/sections/ExpenseAccountView/ExpenseAccountTable.vue
@@ -52,8 +52,11 @@
       <template #member-data="{ row }">
         <UserComponent v-if="!!row.user" :user="row.user"></UserComponent>
       </template>
-      <template #expiryDate-data="{ row }">
-        <span>{{ new Date(Number(row.expiry) * 1000).toLocaleString('en-US') }}</span>
+      <template #startDate-data="{ row }">
+        <span>{{ new Date(Number(row.startDate) * 1000).toLocaleString('en-US') }}</span>
+      </template>
+      <template #endDate-data="{ row }">
+        <span>{{ new Date(Number(row.endDate) * 1000).toLocaleString('en-US') }}</span>
       </template>
       <template #status-data="{ row }">
         <span
@@ -66,27 +69,11 @@
           >{{ row.status }}</span
         >
       </template>
-      <template #maxAmountPerTx-data="{ row }">
-        <span
-          >{{ row.budgetData.find((item: BudgetData) => item.budgetType === 2)?.value }}
-          {{ tokenSymbol(row.tokenAddress) }}</span
-        >
-      </template>
-      <template #transactions-data="{ row }">
-        <span
-          >{{ row.balances[0] }}/{{
-            row.budgetData.find((item: BudgetData) => item.budgetType === 0)?.value
-          }}
-          TXs</span
-        >
+      <template #frequencyType-data="{ row }">
+        <span>{{ row.frequencyType }}</span>
       </template>
       <template #amountTransferred-data="{ row }">
-        <span
-          >{{ row.balances[1] }}/{{
-            row.budgetData.find((item: BudgetData) => item.budgetType === 1)?.value
-          }}
-          {{ tokenSymbol(row.tokenAddress) }}</span
-        >
+        <span>{{ row.balances[1] }}/{{ row.amount }} {{ tokenSymbol(row.tokenAddress) }}</span>
       </template>
     </TableComponent>
   </div>
@@ -104,7 +91,7 @@ import { EXPENSE_ACCOUNT_EIP712_ABI } from '@/artifacts/abi/expense-account-eip7
 import UserComponent from '@/components/UserComponent.vue'
 import { useQueryClient } from '@tanstack/vue-query'
 import { useTanstackQuery } from '@/composables'
-import type { BudgetData, ExpenseResponse } from '@/types'
+import type { ExpenseResponse } from '@/types'
 
 const teamStore = useTeamStore()
 const { addErrorToast, addSuccessToast } = useToastStore()
@@ -148,18 +135,18 @@ const columns = [
     sortable: false
   },
   {
-    key: 'expiryDate',
-    label: 'Expiry',
+    key: 'startDate',
+    label: 'Start Date',
     sortable: true
   },
   {
-    key: 'maxAmountPerTx',
-    label: 'Max Ammount Per Tx',
-    sortable: false
+    key: 'endDate',
+    label: 'End Date',
+    sortable: true
   },
   {
-    key: 'transactions',
-    label: 'Max Transactions',
+    key: 'frequencyType',
+    label: 'Frequency',
     sortable: false
   },
   {
@@ -291,6 +278,11 @@ watch(errorGetOwner, (newVal) => {
   if (newVal) {
     log.error(parseError(newVal))
     addErrorToast('Error Getting Contract Owner')
+  }
+})
+watch(expenseData, (newVal) => {
+  if (newVal) {
+    console.log('Expense Data Updated: ', newVal)
   }
 })
 //#endregion
