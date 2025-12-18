7b4057327b23d56a47b385551d9bfec498b73ee7
K. Hermann EHO <hermannleboss@gmail.com>
2025-12-18 21:33:30 +0000

    Remove deprecated E2E test files for dashboard, navbar, and teams

--- app/test/e2e/login.spec.ts
+++ app/test/e2e/login.spec.ts
@@ -9,32 +9,32 @@ const test = testWithSynpress(metaMaskFixtures(connectedSetup))
 test.describe('Sign in', () => {
   test('should be able to sign in and redirect to the teams page', async ({ page, metamask }) => {
     // Mock API
-    const address = await metamask.getAccountAddress()
-    await page.route('**/api/user/nonce/*', async (route) => {
-      await route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify({ success: true, nonce: '41vj7bz5Ow8oT5xaE' })
-      })
-    })
-    await page.route('**/api/auth/siwe', async (route) => {
-      await route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify({ success: true, accessToken: 'token' })
-      })
-    })
-    await page.route(`**/api/user/${address}`, async (route) => {
-      await route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify({
-          address: await metamask.getAccountAddress(),
-          name: null,
-          nonce: '41vj7bz5Ow8oT5xaE'
-        })
-      })
-    })
+    // const address = await metamask.getAccountAddress()
+    // await page.route('**/api/user/nonce/*', async (route) => {
+    //   await route.fulfill({
+    //     status: 200,
+    //     contentType: 'application/json',
+    //     body: JSON.stringify({ success: true, nonce: '41vj7bz5Ow8oT5xaE' })
+    //   })
+    // })
+    // await page.route('**/api/auth/siwe', async (route) => {
+    //   await route.fulfill({
+    //     status: 200,
+    //     contentType: 'application/json',
+    //     body: JSON.stringify({ success: true, accessToken: 'token' })
+    //   })
+    // })
+    // await page.route(`**/api/user/${address}`, async (route) => {
+    //   await route.fulfill({
+    //     status: 200,
+    //     contentType: 'application/json',
+    //     body: JSON.stringify({
+    //       address: await metamask.getAccountAddress(),
+    //       name: null,
+    //       nonce: '41vj7bz5Ow8oT5xaE'
+    //     })
+    //   })
+    // })
 
     // Click sign-in button
     await page.getByTestId('sign-in').click()
@@ -42,18 +42,18 @@ test.describe('Sign in', () => {
     await page.waitForLoadState('networkidle')
 
     // Connect to dapp
-    // await metamask.connectToDapp()
+    await metamask.connectToDapp()
 
     // // Switch network
-    // await metamask.approveNewNetwork()
-    // await metamask.approveSwitchNetwork()
+    await metamask.approveNewNetwork()
+    await metamask.approveSwitchNetwork()
 
     // // Confirm signature
-    // await page.waitForTimeout(3000)
-    // await metamask.confirmSignature()
+    await page.waitForTimeout(3000)
+    await metamask.confirmSignature()
 
     // // Wait for redirection
-    // await page.waitForURL('http://localhost:5173/teams')
+    await page.waitForURL('http://localhost:5173/teams')
 
     // // Check redirection
     // expect(page.url()).toBe('http://localhost:5173/teams')
