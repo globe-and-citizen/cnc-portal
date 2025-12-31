6f4510800a710e8663c406281fafcd71e9328b30
K. Hermann EHO <hermannleboss@gmail.com>
2025-12-31 04:01:38 +0000

    feat(migration): remove deprecated columns and add unique constraints for MemberTeamsData and WeeklyClaim

--- backend/prisma/migrations/20251231035854_remove_column/migration.sql
+++ backend/prisma/migrations/20251231035854_remove_column/migration.sql
@@ -1,3 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userAddress` on the `MemberTeamsData` table. All the data in the column will be lost.
+  - You are about to drop the column `cashRatePerHour` on the `Wage` table. All the data in the column will be lost.
+  - You are about to drop the column `tokenRatePerHour` on the `Wage` table. All the data in the column will be lost.
+  - You are about to drop the column `usdcRatePerHour` on the `Wage` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[memberAddress,teamId]` on the table `MemberTeamsData` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[wageId,weekStart]` on the table `WeeklyClaim` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `memberAddress` to the `MemberTeamsData` table without a default value. This is not possible if the table is not empty.
+
+*/
 -- DropForeignKey
 ALTER TABLE "BoardOfDirectorActions" DROP CONSTRAINT "BoardOfDirectorActions_teamId_fkey";
 
@@ -23,6 +35,18 @@ ALTER TABLE "MemberTeamsData" DROP CONSTRAINT "MemberTeamsData_teamId_fkey";
 ALTER TABLE "MemberTeamsData" DROP CONSTRAINT "MemberTeamsData_userAddress_fkey";
 
 -- DropForeignKey
+ALTER TABLE "Notification" DROP CONSTRAINT "Notification_userAddress_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TeamContract" DROP CONSTRAINT "TeamContract_teamId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TeamFunctionOverride" DROP CONSTRAINT "TeamFunctionOverride_functionName_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TeamFunctionOverride" DROP CONSTRAINT "TeamFunctionOverride_teamId_fkey";
+
+-- DropForeignKey
 ALTER TABLE "Wage" DROP CONSTRAINT "Wage_teamId_fkey";
 
 -- DropForeignKey
@@ -37,8 +61,26 @@ ALTER TABLE "WeeklyClaim" DROP CONSTRAINT "WeeklyClaim_teamId_fkey";
 -- DropForeignKey
 ALTER TABLE "WeeklyClaim" DROP CONSTRAINT "WeeklyClaim_wageId_fkey";
 
+-- DropIndex
+DROP INDEX "MemberTeamsData_userAddress_teamId_key";
+
+-- AlterTable
+ALTER TABLE "MemberTeamsData" DROP COLUMN "userAddress",
+ADD COLUMN     "memberAddress" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Wage" DROP COLUMN "cashRatePerHour",
+DROP COLUMN "tokenRatePerHour",
+DROP COLUMN "usdcRatePerHour";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MemberTeamsData_memberAddress_teamId_key" ON "MemberTeamsData"("memberAddress", "teamId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WeeklyClaim_wageId_weekStart_key" ON "WeeklyClaim"("wageId", "weekStart");
+
 -- AddForeignKey
-ALTER TABLE "MemberTeamsData" ADD CONSTRAINT "MemberTeamsData_userAddress_fkey" FOREIGN KEY ("userAddress") REFERENCES "User"("address") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "MemberTeamsData" ADD CONSTRAINT "MemberTeamsData_memberAddress_fkey" FOREIGN KEY ("memberAddress") REFERENCES "User"("address") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "MemberTeamsData" ADD CONSTRAINT "MemberTeamsData_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -71,7 +113,19 @@ ALTER TABLE "WeeklyClaim" ADD CONSTRAINT "WeeklyClaim_teamId_fkey" FOREIGN KEY (
 ALTER TABLE "WeeklyClaim" ADD CONSTRAINT "WeeklyClaim_wageId_fkey" FOREIGN KEY ("wageId") REFERENCES "Wage"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userAddress_fkey" FOREIGN KEY ("userAddress") REFERENCES "User"("address") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamContract" ADD CONSTRAINT "TeamContract_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "BoardOfDirectorActions" ADD CONSTRAINT "BoardOfDirectorActions_userAddress_fkey" FOREIGN KEY ("userAddress") REFERENCES "User"("address") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "BoardOfDirectorActions" ADD CONSTRAINT "BoardOfDirectorActions_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamFunctionOverride" ADD CONSTRAINT "TeamFunctionOverride_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamFunctionOverride" ADD CONSTRAINT "TeamFunctionOverride_functionName_fkey" FOREIGN KEY ("functionName") REFERENCES "GlobalSetting"("functionName") ON DELETE CASCADE ON UPDATE CASCADE;
