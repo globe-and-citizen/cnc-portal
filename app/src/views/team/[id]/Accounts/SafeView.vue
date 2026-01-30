cf4c866004df8cfc247fcb76e99d6379ea367d44
Hermann K. EHO <hermannleboss@gmail.com>
2026-01-30 22:07:57 +0000

    refactor: improve formatting and readability of SafeView layout

--- app/src/views/team/[id]/Accounts/SafeView.vue
+++ app/src/views/team/[id]/Accounts/SafeView.vue
@@ -3,20 +3,20 @@
   <div class="flex flex-col gap-6">
     <SafeBalanceSection />
 
-
     <!-- Optimized layout: Safe Owners 1/3, Transactions 2/3 -->
     <div class="grid grid-cols-1 xl:grid-cols-5 gap-6">
       <div class="xl:col-span-3 min-w-0">
-        <GenericTokenHoldingsSection v-if="teamStore.currentTeam?.safeAddress"
-          :address="teamStore.currentTeam?.safeAddress" class="h-full"/>
+        <GenericTokenHoldingsSection
+          v-if="teamStore.currentTeam?.safeAddress"
+          :address="teamStore.currentTeam?.safeAddress"
+          class="h-full"
+        />
       </div>
       <!-- Safe Owners - takes 1/3 of space on desktop -->
       <div class="xl:col-span-2 min-w-0">
         <SafeOwnersCard v-if="teamStore.currentTeam?.safeAddress" />
       </div>
-
     </div>
-
 
     <SafeTransactions v-if="teamStore.currentTeam?.safeAddress" />
     <!-- Safe Transactions History would go here -->
