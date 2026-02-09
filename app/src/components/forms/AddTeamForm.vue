b8f14b700e3587477b99fc8150b9a44e653b8563
Georginio-prod <etonameklou6@gmail.com>
2026-02-09 16:04:26 +0000

    fix: simplify MultiSelectMemberInput usage in AddTeamForm

--- app/src/components/forms/AddTeamForm.vue
+++ app/src/components/forms/AddTeamForm.vue
@@ -57,11 +57,7 @@
           You can add team members now or invite them later.
         </div>
 
-        <MultiSelectMemberInput
-          v-model="teamData.members"
-          :is-creating-team="true"
-          :disable-team-members="false"
-        />
+        <MultiSelectMemberInput v-model="teamData.members" :disable-team-members="false" />
         <div
           class="pl-4 pt-4 text-sm text-red-500"
           data-test="create-team-error"
