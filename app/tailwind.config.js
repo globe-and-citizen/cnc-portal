15c7752e934fbb87d7b97c1f789fadc286ec59c0
Hermann K. EHO <hermannleboss@gmail.com>
2026-02-13 21:36:49 +0000

    refactor: remove daisyui plugin and comment out import in tailwind.config.js

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
-import daisyui from 'daisyui'
+// import daisyui from 'daisyui'
 
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: [daisyui],
+  plugins: [],
   daisyui: {
     themes: [
       {
