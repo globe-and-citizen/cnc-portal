07d5be235fa1c9eecd28a2e9421ca7426fef5a4a
Hermann K. EHO <hermannleboss@gmail.com>
2026-03-13 17:19:28 +0000

    refactor: remove commented-out dividend logic from useContractBalance

--- app/src/composables/useContractBalance.ts
+++ app/src/composables/useContractBalance.ts
@@ -84,22 +84,8 @@ type TokenBalanceEntry = NativeTokenBalanceEntry | ERC20TokenBalanceEntry
 export function useContractBalance(address: Address | Ref<Address | undefined>) {
   const chainId = useChainId()
   const currencyStore = useCurrencyStore()
-  // const teamStore = useTeamStore()
-
   const supportedToken = computed(() => {
     const tokens = [...SUPPORTED_TOKENS]
-    // const investorsV1Address = teamStore.getContractAddressByType('InvestorV1')
-    // if (investorsV1Address && !tokens.some((t) => t.id === 'sher')) {
-    //   tokens.push({
-    //     id: 'sher',
-    //     name: 'Sher Token',
-    //     symbol: 'SHER',
-    //     code: 'SHER',
-    //     coingeckoId: 'sher-token',
-    //     decimals: 6,
-    //     address: investorsV1Address
-    //   })
-    // }
     return tokens
   })
 
@@ -136,103 +122,19 @@ export function useContractBalance(address: Address | Ref<Address | undefined>) 
     }
   })
 
-  // const dividendEntries: DividendEntry[] = supportedToken.value.map((token) => {
-  //   if (token.id === 'native') {
-  //     const read = useReadContract({
-  //       address: unref(address),
-  //       abi: BANK_ABI,
-  //       functionName: 'totalDividends',
-  //       query: { refetchInterval: 300_000 }
-  //     })
-  //     return {
-  //       token,
-  //       data: read.data,
-  //       isLoading: read.isLoading,
-  //       error: read.error,
-  //       isNative: true
-  //     } as DividendNativeEntry
-  //   } else {
-  //     const read = useReadContract({
-  //       address: unref(address),
-  //       abi: BANK_ABI,
-  //       functionName: 'totalTokenDividends',
-  //       args: [token.address as Address],
-  //       query: { refetchInterval: 300_000 }
-  //     })
-  //     return {
-  //       token,
-  //       data: read.data,
-  //       isLoading: read.isLoading,
-  //       error: read.error,
-  //       isNative: false
-  //     } as DividendErc20Entry
-  //   }
-  // })
-
-  // Computed list of dividend amounts per token (for display)
-  // const dividends = computed<TokenBalance[]>(() => {
-  //   return dividendEntries.map(({ token, data }) => {
-  //     const tokenDecimals = token.decimals ?? 18
-  //     const raw = Number(
-  //       token.id === 'native'
-  //         ? formatEther(((data.value ?? 0n) as bigint) ?? 0n)
-  //         : formatUnits(((data.value ?? 0n) as bigint) ?? 0n, tokenDecimals)
-  //     )
-
-  //     const info = currencyStore.getTokenInfo(token.id as TokenId)
-  //     const values: Record<string, TokenBalanceValue> = {}
-  //     if (info?.prices) {
-  //       for (const price of info.prices) {
-  //         const val = raw * (price.price ?? 0)
-  //         values[price.code] = {
-  //           value: val,
-  //           formated: formatCurrencyShort(val, price.code),
-  //           id: price.id,
-  //           code: price.code,
-  //           symbol: price.symbol,
-  //           price: price.price ?? 0,
-  //           formatedPrice: formatCurrencyShort(price.price ?? 0, price.code)
-  //         }
-  //       }
-  //     }
-
-  //     return {
-  //       amount: raw,
-  //       token,
-  //       values
-  //     }
-  //   })
-  // })
-
   const balances = computed<TokenBalance[]>(() => {
     return tokenBalances.map(({ token, data, isNative }) => {
       let amount = 0
       if (data.value) {
-        // const addr = unref(address)
-        // const bankAddr = teamStore.getContractAddressByType('Bank')
-        // const isBank =
-        //   typeof addr === 'string' &&
-        //   typeof bankAddr === 'string' &&
-        //   addr.toLowerCase() === bankAddr.toLowerCase()
-
         if (isNative) {
           const nativeRecord = data.value as { value: bigint } | undefined
           const raw = Number(formatEther(nativeRecord?.value ?? 0n))
-
-          // // take dividend for native from dividends computed
-          // const div = isBank
-          //   ? (dividends.value.find((d) => d.token.id === 'native')?.amount ?? 0)
-          //   : 0
 
           amount = raw
         } else {
           const tokenDecimals = token.decimals ?? 18
           const erc20Val = (data.value as bigint) ?? 0n
           const raw = Number(formatUnits(erc20Val, tokenDecimals))
-
-          // const div = isBank
-          //   ? (dividends.value.find((d) => d.token.id === token.id)?.amount ?? 0)
-          //   : 0
 
           amount = raw
         }
@@ -284,29 +186,6 @@ export function useContractBalance(address: Address | Ref<Address | undefined>) 
     }
     return totals
   })
-
-  // New: totals for dividends
-  // const dividendsTotal = computed<Record<string, TokenBalanceValue>>(() => {
-  //   const totals: Record<string, TokenBalanceValue> = {}
-  //   if (dividends.value.length > 0 && dividends.value[0]) {
-  //     const allCodes = Object.keys(dividends.value[0].values)
-  //     for (const code of allCodes) {
-  //       const first = dividends.value[0].values[code]
-  //       if (!first) continue
-  //       const sum = dividends.value.reduce((acc, d) => acc + (d.values[code]?.value ?? 0), 0)
-  //       totals[code] = {
-  //         value: sum,
-  //         formated: formatCurrencyShort(sum, code),
-  //         id: first.id,
-  //         code: first.code,
-  //         symbol: first.symbol,
-  //         price: first.price,
-  //         formatedPrice: formatCurrencyShort(first.price, code)
-  //       }
-  //     }
-  //   }
-  //   return totals
-  // })
 
   // Combined loading and error states
   const isLoading = computed(() => tokenBalances.some((tb) => tb.isLoading.value))
