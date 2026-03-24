15b31896e97d498a45c3ec3f08957edc8f264a37
Hermann K. EHO <hermannleboss@gmail.com>
2026-03-24 13:32:50 +0000

    refactor: update MemberSection component with UCard and UTable for improved UI and structure

--- app/src/components/sections/DashboardView/MemberSection.vue
+++ app/src/components/sections/DashboardView/MemberSection.vue
@@ -1,156 +1,150 @@
 <template>
-  <CardComponent title="Team Members List">
-    <template
-      #card-action
-      v-if="teamStore.currentTeamMeta.data?.ownerAddress == userDataStore.address"
-    >
-      <ButtonUI
-        @click="
-          () => {
-            showAddMemberForm = { mount: true, show: true }
-          }
-        "
-        data-test="add-member-button"
-        variant="primary"
-        class="w-max"
-      >
-        <IconifyIcon icon="heroicons-outline:plus-circle" class="size-6" /> Add a new Member
-      </ButtonUI>
-      <ModalComponent
-        v-model="showAddMemberForm.show"
-        @reset="() => (showAddMemberForm = { mount: false, show: false })"
-      >
-        <AddMemberForm
-          v-if="teamStore.currentTeamId && showAddMemberForm.mount"
-          :teamId="teamStore.currentTeamId"
-          @memberAdded="showAddMemberForm = { mount: false, show: false }"
-        />
-      </ModalComponent>
-    </template>
-    <template #default>
-      <div class="divider m-0"></div>
-      <div class="overflow-x-auto">
-        <TableComponent
-          :rows="
-            teamStore.currentTeamMeta.data?.members.map((member: any, index: number) => {
-              return { index: index + 1, ...member }
-            })
-          "
-          :loading="isTeamWageDataFetching"
-          :columns="columns"
-          data-test="members-table"
+  <UCard :ui="{ body: 'p-0' }">
+    <template #header>
+      <div class="flex items-center justify-between">
+        <p class="font-semibold text-lg">Team Members List</p>
+
+        <UModal
+          v-if="teamStore.currentTeamMeta.data?.ownerAddress == userDataStore.address"
+          v-model:open="showAddMemberForm.show"
+          title="Add a new Member"
+          :ui="{ content: 'rounded-2xl' }"
+          @update:open="(v) => !v && (showAddMemberForm = { mount: false, show: false })"
         >
-          <template #wage-header="">
-            <div class="flex flex-col gap-0 w-full pt-7">
-              <div class="text-center pb-1">
-                <span>Hourly Rates</span>
-              </div>
-              <div class="flex flex-row justify-between border-t border-base-400">
-                <span class="w-1/3 text-xs p-1 text-center bg-[#C8FACD]">{{
-                  NETWORK.currencySymbol
-                }}</span>
-                <span class="w-1/3 text-xs p-1 text-center bg-[#FEF3DE]">USDC</span>
-                <span class="w-1/3 text-xs p-1 text-center bg-[#D9F1F6]">SHER</span>
-              </div>
-            </div>
-          </template>
-          <template #member-data="{ row }">
-            <UserComponent
-              :user="{ name: row.name, address: row.address, imageUrl: row.imageUrl }"
+          <UButton
+            icon="i-heroicons-plus-circle"
+            color="success"
+            data-test="add-member-button"
+            label="Add a new Member"
+            @click="showAddMemberForm = { mount: true, show: true }"
+          />
+
+          <template #body>
+            <AddMemberForm
+              v-if="teamStore.currentTeamId && showAddMemberForm.mount"
+              :teamId="teamStore.currentTeamId"
+              @memberAdded="showAddMemberForm = { mount: false, show: false }"
             />
           </template>
-          <template #maxWeeklyHours-data="{ row }">
-            <div v-if="!isTeamWageDataFetching" class="flex flex-col font-semibold">
-              <span>{{ getMemberWageView(row.address).maximumHoursPerWeek }}</span>
-              <span
-                v-if="getMemberWageView(row.address).hasOvertime"
-                class="text-xs text-black font-light"
-              >
-                OT: {{ getMemberWageView(row.address).maximumOvertimeHoursPerWeek }}
-              </span>
-            </div>
-            <div class="skeleton w-24 h-4" v-if="isTeamWageDataFetching"></div>
-          </template>
-          <template #wage-data="{ row }">
-            <div class="flex flex-col gap-1">
-              <div class="flex flex-row gap-2 justify-between font-semibold">
-                <span class="w-1/3 text-right pr-4">
-                  {{
-                    !isTeamWageDataFetching ? getMemberWageView(row.address).cashRatePerHour : ''
-                  }}
-                </span>
-                <span class="w-1/3 text-right pr-4">
-                  {{
-                    !isTeamWageDataFetching ? getMemberWageView(row.address).usdcRatePerHour : ''
-                  }}
-                </span>
-                <span class="w-1/3 text-right pr-4">
-                  {{
-                    !isTeamWageDataFetching ? getMemberWageView(row.address).tokenRatePerHour : ''
-                  }}
-                </span>
-              </div>
-              <div
-                v-if="!isTeamWageDataFetching && getMemberWageView(row.address).hasOvertime"
-                class="flex flex-row gap-2 justify-between text-xs text-black font-light"
-              >
-                <span class="w-1/3 text-right pr-4">
-                  OT: {{ getMemberWageView(row.address).overtimeCashRatePerHour }}
-                </span>
-                <span class="w-1/3 text-right pr-4">
-                  OT: {{ getMemberWageView(row.address).overtimeUsdcRatePerHour }}
-                </span>
-                <span class="w-1/3 text-right pr-4">
-                  OT: {{ getMemberWageView(row.address).overtimeTokenRatePerHour }}
-                </span>
-              </div>
-            </div>
-            <div class="skeleton w-24 h-4" v-if="isTeamWageDataFetching"></div>
-          </template>
-          <template
-            #action-data="{ row }"
-            v-if="teamId && teamStore.currentTeamMeta.data?.ownerAddress === userDataStore.address"
-          >
-            <div class="flex flex-wrap gap-2">
-              <DeleteMemberModal
-                :member="{ name: row.name, address: row.address }"
-                :teamId="teamId"
-              />
-              <ButtonUI
-                :variant="teamWageByAddress.get(row.address)?.disabled ? 'success' : 'warning'"
-                :loading="isToggling"
-                :disabled="!teamWageByAddress.get(row.address) || isToggling"
-                :data-test="teamWageByAddress.get(row.address)?.disabled ? 'resume-wage-button' : 'pause-wage-button'"
-                @click="toggleWageStatus(teamWageByAddress.get(row.address)!)"
-              >
-                <IconifyIcon
-                  :icon="teamWageByAddress.get(row.address)?.disabled ? 'heroicons-outline:play' : 'heroicons-outline:pause'"
-                  class="size-5"
-                />
-              </ButtonUI>
-              <SetMemberWageModal
-                :member="{ name: row.name, address: row.address }"
-                :teamId="teamId"
-                :wage="teamWageByAddress.get(row.address)"
-              />
-            </div>
-          </template>
-        </TableComponent>
+        </UModal>
       </div>
     </template>
-  </CardComponent>
+
+    <UTable
+      :data="
+        teamStore.currentTeamMeta.data?.members.map((member: any, index: number) => ({
+          index: index + 1,
+          ...member
+        }))
+      "
+      :loading="isTeamWageDataFetching"
+      :columns="columns"
+      data-test="members-table"
+    >
+      <template #wage-header>
+        <div class="flex flex-col gap-0 w-full pt-7">
+          <div class="text-center pb-1">
+            <span>Hourly Rates</span>
+          </div>
+          <div class="flex flex-row justify-between border-t border-base-400">
+            <span class="w-1/3 text-xs p-1 text-center bg-[#C8FACD]">{{
+              NETWORK.currencySymbol
+            }}</span>
+            <span class="w-1/3 text-xs p-1 text-center bg-[#FEF3DE]">USDC</span>
+            <span class="w-1/3 text-xs p-1 text-center bg-[#D9F1F6]">SHER</span>
+          </div>
+        </div>
+      </template>
+
+      <template #member-cell="{ row }">
+        <UserComponent
+          :user="{ name: row.original.name, address: row.original.address, imageUrl: row.original.imageUrl }"
+        />
+      </template>
+
+      <template #maxWeeklyHours-cell="{ row }">
+        <div v-if="!isTeamWageDataFetching" class="flex flex-col font-semibold">
+          <span>{{ getMemberWageView(row.original.address).maximumHoursPerWeek }}</span>
+          <span
+            v-if="getMemberWageView(row.original.address).hasOvertime"
+            class="text-xs text-black font-light"
+          >
+            OT: {{ getMemberWageView(row.original.address).maximumOvertimeHoursPerWeek }}
+          </span>
+        </div>
+        <USkeleton v-if="isTeamWageDataFetching" class="h-4 w-24" />
+      </template>
+
+      <template #wage-cell="{ row }">
+        <div v-if="!isTeamWageDataFetching" class="flex flex-col gap-1">
+          <div class="flex flex-row gap-2 justify-between font-semibold">
+            <span class="w-1/3 text-right pr-4">{{
+              getMemberWageView(row.original.address).cashRatePerHour
+            }}</span>
+            <span class="w-1/3 text-right pr-4">{{
+              getMemberWageView(row.original.address).usdcRatePerHour
+            }}</span>
+            <span class="w-1/3 text-right pr-4">{{
+              getMemberWageView(row.original.address).tokenRatePerHour
+            }}</span>
+          </div>
+          <div
+            v-if="getMemberWageView(row.original.address).hasOvertime"
+            class="flex flex-row gap-2 justify-between text-xs text-black font-light"
+          >
+            <span class="w-1/3 text-right pr-4">
+              OT: {{ getMemberWageView(row.original.address).overtimeCashRatePerHour }}
+            </span>
+            <span class="w-1/3 text-right pr-4">
+              OT: {{ getMemberWageView(row.original.address).overtimeUsdcRatePerHour }}
+            </span>
+            <span class="w-1/3 text-right pr-4">
+              OT: {{ getMemberWageView(row.original.address).overtimeTokenRatePerHour }}
+            </span>
+          </div>
+        </div>
+        <USkeleton v-if="isTeamWageDataFetching" class="h-4 w-24" />
+      </template>
+
+      <template #action-cell="{ row }">
+        <div v-if="teamId" class="flex flex-wrap gap-2">
+          <DeleteMemberModal
+            :member="{ name: row.original.name, address: row.original.address }"
+            :teamId="teamId"
+          />
+          <UButton
+            :color="teamWageByAddress.get(row.original.address)?.disabled ? 'success' : 'warning'"
+            :loading="isToggling"
+            :disabled="!teamWageByAddress.get(row.original.address) || isToggling"
+            :icon="
+              teamWageByAddress.get(row.original.address)?.disabled
+                ? 'i-heroicons-play'
+                : 'i-heroicons-pause'
+            "
+            :data-test="
+              teamWageByAddress.get(row.original.address)?.disabled
+                ? 'resume-wage-button'
+                : 'pause-wage-button'
+            "
+            @click="toggleWageStatus(teamWageByAddress.get(row.original.address)!)"
+          />
+          <SetMemberWageModal
+            :member="{ name: row.original.name, address: row.original.address }"
+            :teamId="teamId"
+            :wage="teamWageByAddress.get(row.original.address)"
+          />
+        </div>
+      </template>
+    </UTable>
+  </UCard>
 </template>
 
 <script setup lang="ts">
 import { computed, ref, watch } from 'vue'
-import { Icon as IconifyIcon } from '@iconify/vue'
+import type { TableColumn } from '@nuxt/ui'
 import AddMemberForm from '@/components/sections/DashboardView/forms/AddMemberForm.vue'
-import ModalComponent from '@/components/ModalComponent.vue'
 import { useUserDataStore } from '@/stores/user'
 import { useTeamStore, useToastStore } from '@/stores'
-import ButtonUI from '@/components/ButtonUI.vue'
-import CardComponent from '@/components/CardComponent.vue'
-import TableComponent from '@/components/TableComponent.vue'
 import UserComponent from '@/components/UserComponent.vue'
 import { useGetTeamWagesQuery, useToggleWageStatusMutation } from '@/queries/wage.queries'
 import type { Address } from 'viem'
@@ -159,13 +153,17 @@ import DeleteMemberModal from '@/components/sections/DashboardView/DeleteMemberM
 import type { Wage } from '@/types'
 import SetMemberWageModal from './SetMemberWageModal.vue'
 
+type MemberRow = {
+  index: number
+  name: string
+  address: string
+  imageUrl?: string
+}
+
 const userDataStore = useUserDataStore()
 const toastStore = useToastStore()
 const teamStore = useTeamStore()
-const showAddMemberForm = ref({
-  mount: false,
-  show: false
-})
+const showAddMemberForm = ref({ mount: false, show: false })
 
 const teamId = computed(() => teamStore.currentTeamId)
 
@@ -178,9 +176,7 @@ const {
 watch(
   () => teamWageDataError.value,
   (error) => {
-    if (error) {
-      toastStore.addErrorToast('Failed to fetch team wage data')
-    }
+    if (error) toastStore.addErrorToast('Failed to fetch team wage data')
   }
 )
 
@@ -251,23 +247,19 @@ const buildMemberWageView = (memberWage: Wage): MemberWageView => {
 }
 
 const memberWageViewByAddress = computed(() => {
-  const wageViewByAddress = new Map<string, MemberWageView>()
-
+  const map = new Map<string, MemberWageView>()
   for (const wage of teamWageData.value ?? []) {
-    wageViewByAddress.set(wage.userAddress, buildMemberWageView(wage as Wage))
+    map.set(wage.userAddress, buildMemberWageView(wage as Wage))
   }
-
-  return wageViewByAddress
+  return map
 })
 
 const teamWageByAddress = computed(() => {
-  const wages = new Map<string, Wage>()
-
+  const map = new Map<string, Wage>()
   for (const wage of teamWageData.value ?? []) {
-    wages.set(wage.userAddress, wage as Wage)
+    map.set(wage.userAddress, wage as Wage)
   }
-
-  return wages
+  return map
 })
 
 const { mutate: executeToggleStatus, isPending: isToggling } = useToggleWageStatusMutation()
@@ -287,16 +279,16 @@ const getMemberWageView = (memberAddress: Address): MemberWageView => {
   return memberWageViewByAddress.value.get(memberAddress) ?? EMPTY_MEMBER_WAGE_VIEW
 }
 
-const columns = computed(() => {
-  const columns = [
-    { key: 'index', label: '#' },
-    { key: 'member', label: 'Member' },
-    { key: 'maxWeeklyHours', label: 'Max Weekly Hours' },
-    { key: 'wage', label: `Hourly Rate` }
+const columns = computed((): TableColumn<MemberRow>[] => {
+  const cols: TableColumn<MemberRow>[] = [
+    { accessorKey: 'index', header: '#' },
+    { id: 'member', header: 'Member' },
+    { id: 'maxWeeklyHours', header: 'Max Weekly Hours' },
+    { id: 'wage', header: 'Hourly Rate' }
   ]
   if (teamStore.currentTeamMeta.data?.ownerAddress == userDataStore.address) {
-    columns.push({ key: 'action', label: 'Action' })
+    cols.push({ id: 'action', header: 'Action' })
   }
-  return columns
+  return cols
 })
 </script>
