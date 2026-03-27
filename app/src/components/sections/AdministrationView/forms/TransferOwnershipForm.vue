6539c17a256b67619a89e977cdfa785798ec34f9
Hermann EHO <hermannleboss@gmail.com>
2026-03-27 10:20:13 +0000

    refactor: migrate AdministrationView TransferOwnershipForm to UForm + Zod
    Replace vuelidate with zod schema, native inputs with UInput inside
    UFormField, and update test to use flushPromises for async validation.

--- app/src/components/sections/AdministrationView/forms/TransferOwnershipForm.vue
+++ app/src/components/sections/AdministrationView/forms/TransferOwnershipForm.vue
@@ -1,66 +1,46 @@
 <template>
-  <div class="flex flex-col gap-2">
-    <h1 class="font-bold text-2xl">Transfer Ownership</h1>
-    <h3 class="font-bold text-red-600">This will create an board of director action</h3>
-  </div>
-  <hr class="" />
-  <div class="flex flex-col">
-    <label class="input input-bordered flex items-center gap-2 input-md mt-4">
-      <span class="w-28">New Owner</span>
-      <input
-        type="text"
-        class="grow"
-        placeholder="0x123"
-        data-test="new-owner-input"
-        v-model="newOwner"
-      />
-    </label>
-    <div
-      class="pl-4 text-red-500 text-sm w-full text-left"
-      v-for="error of $v.newOwner.$errors"
-      :key="error.$uid"
-    >
-      {{ error.$message }}
+  <UForm :schema="schema" :state="state" @submit="submitForm">
+    <div class="flex flex-col gap-2">
+      <h1 class="font-bold text-2xl">Transfer Ownership</h1>
+      <h3 class="font-bold text-red-600">This will create an board of director action</h3>
     </div>
-    <label class="input input-bordered flex items-center gap-2 input-md mt-4">
-      <span class="w-28">Description</span>
-      <input
-        type="text"
-        class="grow"
-        placeholder="Description"
-        data-test="new-owner-input"
-        v-model="description"
-      />
-    </label>
-    <div
-      class="pl-4 text-red-500 text-sm w-full text-left"
-      v-for="error of $v.description.$errors"
-      :key="error.$uid"
-    >
-      {{ error.$message }}
+    <hr class="" />
+    <div class="flex flex-col gap-4 mt-4">
+      <UFormField name="newOwner" label="New Owner">
+        <UInput
+          v-model="state.newOwner"
+          placeholder="0x123"
+          data-test="new-owner-input"
+          class="w-full"
+        />
+      </UFormField>
+      <UFormField name="description" label="Description">
+        <UInput
+          v-model="state.description"
+          placeholder="Description"
+          data-test="description-input"
+          class="w-full"
+        />
+      </UFormField>
     </div>
-  </div>
-
-  <div class="modal-action justify-center">
-    <UButton
-      color="primary"
-      :loading="transferOwnershipLoading"
-      :disabled="transferOwnershipLoading"
-      data-test="submit-button"
-      @click="submitForm(newOwner, description)"
-      label="Submit"
-    />
-  </div>
+    <div class="modal-action justify-center">
+      <UButton
+        type="submit"
+        color="primary"
+        :loading="transferOwnershipLoading"
+        :disabled="transferOwnershipLoading"
+        data-test="submit-button"
+        label="Submit"
+      />
+    </div>
+  </UForm>
 </template>
+
 <script setup lang="ts">
-import { required, helpers } from '@vuelidate/validators'
-import { useVuelidate } from '@vuelidate/core'
-import { ref } from 'vue'
+import { z } from 'zod'
+import { reactive, computed } from 'vue'
 import { isAddress } from 'viem'
 
-const newOwner = ref('')
-const description = ref('')
-const emits = defineEmits(['transferOwnership'])
 const props = defineProps({
   transferOwnershipLoading: {
     type: Boolean,
@@ -73,25 +53,20 @@ const props = defineProps({
   }
 })
 
-const validAddress = helpers.withMessage('Invalid address', (address) =>
-  isAddress(address as string)
-)
-const rules = {
-  newOwner: {
-    required,
-    validAddress
-  },
-  description: {
-    required: helpers.withMessage('Description is required', (value: string) => {
-      return props.asBod ? value.length > 0 : true
-    })
-  }
-}
+const emits = defineEmits(['transferOwnership'])
 
-const $v = useVuelidate(rules, { newOwner, description })
-const submitForm = (newOwner: string, description: string) => {
-  $v.value.$touch()
-  if ($v.value.$invalid) return
-  emits('transferOwnership', newOwner, description)
+const schema = computed(() =>
+  z.object({
+    newOwner: z.string().refine((v) => isAddress(v), { message: 'Invalid address' }),
+    description: props.asBod
+      ? z.string().min(1, 'Description is required')
+      : z.string()
+  })
+)
+
+const state = reactive({ newOwner: '', description: '' })
+
+const submitForm = () => {
+  emits('transferOwnership', state.newOwner, state.description)
 }
 </script>
