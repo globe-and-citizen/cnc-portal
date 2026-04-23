85fdd1569c4bd51f855a3577d40e3a6537ce147e
Achille Gley <achillegley@gmail.com>
2026-04-23 22:52:43 +0700

    refactor doc structure

--- app/src/views/team/[id]/Accounts/WeeklyClaimView.vue
+++ app/src/views/team/[id]/Accounts/WeeklyClaimView.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="flex flex-col gap-6">
-    <WeeklyClaim />
-    <CashRemerationTransactions />
-  </div>
+  <WeeklyClaim />
 </template>
 
 <script setup lang="ts">
 import WeeklyClaim from '@/components/sections/WeeklyClaimView/WeeklyClaim.vue'
-import CashRemerationTransactions from '@/components/sections/WeeklyClaimView/CashRemerationTransactions.vue'
 </script>
