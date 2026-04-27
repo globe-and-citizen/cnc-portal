b9777f5fe5ee70d22de89377d999f6409822777b
Achille Gley <achillegley@gmail.com>
2026-04-27 14:03:22 +0700

    feat: add ExpenseTransactions and CashRemunerationTransactions components to ExpenseAccountView and WeeklyClaimView

--- app/src/views/team/[id]/Accounts/WeeklyClaimView.vue
+++ app/src/views/team/[id]/Accounts/WeeklyClaimView.vue
@@ -1,7 +1,19 @@
 <template>
   <WeeklyClaim />
+  <CashRemunerationTransactions
+    v-if="cashRemunerationAddress"
+    :cash-remuneration-address="cashRemunerationAddress"
+  />
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useTeamStore } from '@/stores'
 import WeeklyClaim from '@/components/sections/WeeklyClaimView/WeeklyClaim.vue'
+import CashRemunerationTransactions from '@/components/sections/CashRemunerationView/CashRemunerationTransactions.vue'
+
+const teamStore = useTeamStore()
+const cashRemunerationAddress = computed(() =>
+  teamStore.getContractAddressByType('CashRemunerationEIP712')
+)
 </script>
