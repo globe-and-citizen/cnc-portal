55c93e538f9ad6e43dd9da3b4387d6c5633c9f2f
Hermann EHO <hermannleboss@gmail.com>
2026-05-27 11:29:37 +0000

    feat: add isHidden and isArchived columns to MemberTeamsData and Team tables

--- backend/prisma/migrations/20260527112240_archive_and_hide/migration.sql
+++ backend/prisma/migrations/20260527112240_archive_and_hide/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE "MemberTeamsData" ADD COLUMN     "isVisible" BOOLEAN NOT NULL DEFAULT true;
+ALTER TABLE "MemberTeamsData" ADD COLUMN     "isHidden" BOOLEAN NOT NULL DEFAULT false;
 
 -- AlterTable
 ALTER TABLE "Team" ADD COLUMN     "isArchived" BOOLEAN NOT NULL DEFAULT false;
