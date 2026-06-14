2b63c41ab2efc581556b0a3ad3f33fbc96a86784
Hermann EHO <hermannleboss@gmail.com>
2026-06-14 17:25:13 +0000

    style: 💄 prettier-format CompanyActionConfirm

--- app/src/components/sections/CompaniesView/CompanyActionConfirm.vue
+++ app/src/components/sections/CompaniesView/CompanyActionConfirm.vue
@@ -1,5 +1,10 @@
 <template>
-  <UModal :open="open" :title="title" :description="description" @update:open="emit('update:open', $event)">
+  <UModal
+    :open="open"
+    :title="title"
+    :description="description"
+    @update:open="emit('update:open', $event)"
+  >
     <template #body>
       <UAlert
         v-if="errorMessage"
@@ -10,8 +15,7 @@
         data-test="company-action-error"
       />
       <p class="text-default text-sm">
-        Are you sure you want to {{ verb }} the company
-        <span class="font-bold">{{ teamName }}</span
+        Are you sure you want to {{ verb }} the company <span class="font-bold">{{ teamName }}</span
         >?
       </p>
       <div class="mt-4 flex justify-center gap-2">
