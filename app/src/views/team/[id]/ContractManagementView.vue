16110f9f890380d5aadbbbccbc9303503802fc1d
Hermann EHO <hermannleboss@gmail.com>
2026-07-17 18:42:26 +0000

    feat: add OfficerContractsByVersion component and integrate into ContractManagementView

--- app/src/views/team/[id]/ContractManagementView.vue
+++ app/src/views/team/[id]/ContractManagementView.vue
@@ -1,8 +1,12 @@
 <template>
-  <MainContractSection />
-  <AdvertiseContractSection />
+  <div class="flex flex-col gap-6">
+    <MainContractSection />
+    <OfficerContractsByVersion />
+    <AdvertiseContractSection />
+  </div>
 </template>
 <script setup lang="ts">
 import MainContractSection from '@/components/sections/ContractManagementView/MainContractSection.vue'
+import OfficerContractsByVersion from '@/components/sections/ContractManagementView/OfficerContractsByVersion.vue'
 import AdvertiseContractSection from '@/components/sections/ContractManagementView/AdvertiseContractSection.vue'
 </script>
